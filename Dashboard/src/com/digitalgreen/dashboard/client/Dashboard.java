ac0e84592b15c8e8b879ea522c42b0f827e16bd7
Apurva Joshi <apurva@digitalgreen.org>
2010-01-29 08:23:06 +0000

    Adding media files (CSS, JS, images)

--- Dashboard/src/com/digitalgreen/dashboard/client/Dashboard.java
+++ Dashboard/src/com/digitalgreen/dashboard/client/Dashboard.java
@@ -21,15 +21,27 @@ public class Dashboard implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
+	
+	Element e_form = RootPanel.get("region_form").getElement();
+	FormPanel f = FormPanel.wrap(e_form);
+	
 	public void onModuleLoad() {
 		
+		f.setAction("http://174.129.13.106:8000/feeds/animators/49/");
+
+	    // Because we're going to add a FileUpload widget, we'll need to set the
+	    // form to use the POST method, and multipart MIME encoding.
+	    f.setEncoding(FormPanel.ENCODING_MULTIPART);
+	    f.setMethod(FormPanel.METHOD_POST);
 		
 	    Element e = RootPanel.get("save_button").getElement();
 	    Button b = Button.wrap(e);
 	    
+	    	    
 	    b.addClickHandler(new ClickHandler() {
 		      public void onClick(ClickEvent event) {
 			        Window.alert("Hi");
+			        f.submit();
 			      }});
 	    
 	    	    
