1baa5d4dead7ef57eaaf95983161ab2921038953
Apurva Joshi <apurva@digitalgreen.org>
2010-02-25 07:10:32 +0000

    Logic for creating the SQLite database

--- gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/servlets/Index.java
+++ gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/servlets/Index.java
@@ -1,21 +1,41 @@
 package com.digitalgreen.dashboardgwt.client.servlets;
 
+import java.util.HashMap;
+
+import com.digitalgreen.dashboardgwt.client.common.RequestContext;
+import com.digitalgreen.dashboardgwt.client.data.IndexData;
 import com.digitalgreen.dashboardgwt.client.servlets.BaseServlet;
 import com.digitalgreen.dashboardgwt.client.templates.IndexTemplate;
+import com.google.gwt.user.client.Window;
 
 public class Index extends BaseServlet {	
 	public Index(){
 		super();
 	}
 	
+	public Index(RequestContext requestContext){
+		super(requestContext);
+	}
+	
 	@Override
 	public void response () {
 		super.response();
-
+		String method = this.getMethodTypeCtx();
+		
 		if (!this.isLoggedIn()) {
 			super.redirectTo(new Login());
-		} else {
-			this.fillTemplate(new IndexTemplate(this.requestContext));
+		} 
+		else {
+			if(method == RequestContext.METHOD_POST) {
+				HashMap queryArgs = (HashMap)this.requestContext.getArgs();
+				String queryArg = (String)queryArgs.get("action");
+				if(queryArg == "gooffline"){
+					IndexData.createTables();
+				}
+			}
+			else{
+				this.fillTemplate(new IndexTemplate(this.requestContext));
+			}
 		}
 	}
 }
