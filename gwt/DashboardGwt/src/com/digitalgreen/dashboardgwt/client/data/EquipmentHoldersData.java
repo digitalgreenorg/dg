1fa07e034632b7f9ace76830a773f75fd5742fd0
Sreenivas Reddy <sreenivasula@digitalgreen.org>
2010-04-02 18:10:25 +0000

    --Added data class for equipment holder

--- gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/EquipmentHoldersData.java
+++ gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/EquipmentHoldersData.java
@@ -1,23 +1,151 @@
 package com.digitalgreen.dashboardgwt.client.data;
 
-public class EquipmentHoldersData extends BaseData {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.digitalgreen.dashboardgwt.client.common.Form;
+import com.digitalgreen.dashboardgwt.client.common.OnlineOfflineCallbacks;
+import com.digitalgreen.dashboardgwt.client.data.EquipmentHoldersData.Data;
+import com.digitalgreen.dashboardgwt.client.data.EquipmentHoldersData.Type;
+import com.google.gwt.core.client.JsArray;
+
+public class EquipmentHoldersData extends BaseData {
+	
+	public static class Type extends BaseData.Type{
+		protected Type() {}
+		public final native String getContentType() /*-{ return this.fields.content_type; }-*/;
+		public final native String getObjectId() /*-{ return this.fields.object_id; }-*/;
+	}
+	
+	public class Data extends BaseData.Data {
+		
+		final private static String COLLECTION_PREFIX = "equipmentholder";
+			
+		private String content_type;
+		private String object_id;
+		
+		public Data() {
+			super();
+		}
+		
+		public Data(int id, String content_type, String object_id){
+			super();
+			this.id = id;
+			this.content_type = content_type;
+			this.object_id = object_id;
+		}
+		
+		public Data(int id ) {
+			super();
+			this.id = id;
+		}
+		
+		public String getContentType(){
+			return this.content_type;
+		}
+		
+		public String getObjectId(){
+			return this.object_id;
+		}
+		
+		public BaseData.Data clone() {
+			Data obj = new Data();
+			obj.id = this.id;
+			obj.content_type = this.content_type;
+			obj.object_id = this.object_id;
+			return obj;
+		}
+		
+		@Override
+		public String getPrefixName() {
+			return Data.COLLECTION_PREFIX;
+		}
+		
+		@Override
+		public void setObjValueFromString(String key, Object val) {
+			if(key.equals("id")) {
+				this.id = Integer.parseInt((String)val);
+			} else if(key.equals("content_type")) {
+				this.content_type = (String)val;
+			} else if(key.equals("object_id")) {
+				this.object_id = (String)val;
+			}			
+		}
+		
+		@Override
+		public void save() {
+			EquipmentHoldersData equipmentholdersDataDbApis = new EquipmentHoldersData();
+			this.id = equipmentholdersDataDbApis.autoInsert( this.content_type, this.object_id);
+		}
+	}
+	
+	
 	protected static String tableID = "2";
 	protected static String createTable = "CREATE TABLE IF NOT EXISTS `equipment_holder` " +
 												"(id INTEGER PRIMARY KEY NOT NULL ," +
 												"content_type_id INT NOT NULL DEFAULT 0," +
 												"object_id INT NOT NULL DEFAULT 0);"; 
 	
+	protected static String selectEquipmentHolders = "SELECT id FROM equipment_holder ORDER BY(-id)";
+	protected static String listEquipmentHolders = "SELECT * FROM equipment_holder ORDER BY(-id)";
+	protected static String saveEquipmentHolderOnlineURL = "/dashboard/saveequipmentholderonline/";
+	protected static String getEquipmentHolderOnlineURL = "/dashboard/getequipmentholdersonline/";
 	protected static String saveEquipmentHolderOfflineURL = "/dashboard/saveequipmentholderoffline/";
+	protected String table_name = "equipment_holder";
+	protected String[] fields = {"id", "content_type", "object_id"};
+	
 	
 	public EquipmentHoldersData() {
 		super();
 	}
 	
-	@Override
-	protected String getTableId() {
-		return EquipmentHoldersData.tableID;
+	public EquipmentHoldersData(OnlineOfflineCallbacks callbacks) {
+		super(callbacks);
+	}
+	
+	public EquipmentHoldersData(OnlineOfflineCallbacks callbacks, Form form, String queryString) {
+		super(callbacks, form, queryString);
 	}
 
+	@Override
+	public Data getNewData() {
+		return new Data();
+	}
 	
+	@Override
+	protected  String getTableId() {
+		return EquipmentHoldersData.tableID;
+	}
+	
+	protected String getTableName() {
+		return this.table_name;
+	}
+	
+	protected String[] getFields() {
+		return this.fields;
+	}
+	
+	protected String getSaveOfflineURL(){
+		return EquipmentHoldersData.saveEquipmentHolderOfflineURL;
+	}
+
+	public final native JsArray<Type> asArrayOfData(String json) /*-{
+		return eval(json);
+	}-*/;
+	
+	public List serialize(JsArray<Type> equipmentholderObjects){
+		List equipmentholders = new ArrayList();
+		for(int i = 0; i < equipmentholderObjects.length(); i++){
+			Data equipmentholder = new Data(Integer.parseInt(equipmentholderObjects.get(i).getPk()), 
+					equipmentholderObjects.get(i).getContentType(), 
+					equipmentholderObjects.get(i).getObjectId());
+			equipmentholders.add(equipmentholder);
+		}
+		return equipmentholders;
+	}
+	
+	public List getListingOnline(String json){
+		return this.serialize(this.asArrayOfData(json));		
+	}	
+		
 }
