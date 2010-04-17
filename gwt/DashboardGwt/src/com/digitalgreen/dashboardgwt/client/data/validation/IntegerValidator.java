5f77dcf60a71d12702b09e2304910da73f7b173c
Sreenivas Reddy <sreenivasula@digitalgreen.org>
2010-04-17 14:56:03 +0000

    --Modified to handle null case.

--- gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/validation/IntegerValidator.java
+++ gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/validation/IntegerValidator.java
@@ -1,39 +1,43 @@
 package com.digitalgreen.dashboardgwt.client.data.validation;
 
 public class IntegerValidator extends BaseValidator {
-	
+
 	private Integer minValue = null;
 	private Integer maxValue = null;
 	private int valueInt;
-	
+
 	public IntegerValidator(String value) {
 		super(value);
 	}
-	
+
 	public IntegerValidator(String value, boolean nullable, boolean blank) {
 		super(value, nullable, blank);
 	}
-	
+
 	public IntegerValidator(String value, boolean nullable, boolean blank,
 			int minValue, int maxValue) {
 		super(value, nullable, blank);
 	}
-	
+
 	public int getValueInt() {
 		return this.valueInt;
 	}
-	
+
 	@Override
 	public boolean validate() {
-		try {
-			this.valueInt = Integer.parseInt(this.getValue());
-		} catch(NumberFormatException e) {
+		if (!super.validate()) {
 			return false;
+		} else if (this.getValue() == null) {
+			return true;
+		} else {
+			try {
+				this.valueInt = Integer.parseInt(this.getValue());
+			} catch (NumberFormatException e) {
+				return false;
+			}
+
+			return (this.minValue != null ? this.getValue().length() >= this.minValue.toString().length(): true 
+					&& this.maxValue != null ? this.getValue().length() <= this.maxValue.toString().length(): true);
 		}
-		
-		return (super.validate() && 
-				this.minValue != null ? this.getValue().length() >= this.minValue.toString().length() : true && 
-				this.maxValue != null ? this.getValue().length() <= this.maxValue.toString().length() : true);
 	}
-	
 }
