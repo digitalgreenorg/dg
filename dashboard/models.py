ec008024bccb5cdd937f0f571216d589910872a5
Rahul Agrawal <rahul@digitalgreen.org>
2010-06-10 07:07:52 +0000

    - District & Month_district wasn't unique together. Syntax problem. Rectified this.

--- dashboard/models.py
+++ dashboard/models.py
@@ -497,5 +497,6 @@ class Target(models.Model):
     what_not_went_well =  models.TextField("What did NOT go well and why?", blank=True)
     challenges =  models.TextField(blank=True)
     support_requested =  models.TextField(blank=True)
-
-    unique_together = ("district","month_year")
+    
+    class Meta:
+        unique_together = ("district","month_year")
