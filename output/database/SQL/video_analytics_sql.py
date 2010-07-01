6ffa10f18759b78392ae10a2e327514e7be682cd
Rahul Agrawal <rahul@digitalgreen.org>
2010-07-01 18:27:24 +0000

    -Adding partner filtering to 'Distinct Videos Screened' field

--- output/database/SQL/video_analytics_sql.py
+++ output/database/SQL/video_analytics_sql.py
@@ -76,7 +76,7 @@ def video_tot_scr(request, geog, id):
     sql_ds = get_init_sql_ds();
     sql_ds['select'].append("COUNT(DISTINCT SCR.video_id) AS count")
     sql_ds['from'].append("SCREENING_videoes_screened SCR");
-    if(geog.upper()!="COUNTRY" or (to_date and from_date)):
+    if(geog.upper()!="COUNTRY" or (to_date and from_date) or partners):
         sql_ds['join'].append(["SCREENING SC", "SC.id = SCR.screening_id"])
         filter_partner_geog_date(sql_ds,'SC','SC.DATE',geog,id,from_date,to_date,partners)
 
