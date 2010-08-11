1add72827e5d86dde78a957dd6f6d6cb021dd191
Apurva Joshi <apurva@digitalgreen.org>
2010-08-11 10:48:04 +0000

    Fixed the bug in animator sql statement

--- gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/AnimatorsData.java
+++ gwt/DashboardGwt/src/com/digitalgreen/dashboardgwt/client/data/AnimatorsData.java
@@ -373,7 +373,7 @@ public class AnimatorsData extends BaseData {
 											"FOREIGN KEY(partner_id) REFERENCES partners(id), " +
 											"FOREIGN KEY(village_id) REFERENCES village(id));";
 	protected static String dropTable = "DROP TABLE IF EXISTS `animator`;";
-	protected static String selectAnimators = "SELECT animator.id, animator.name FROM animator ORDER BY (animator.name);";
+	protected static String selectAnimators = "SELECT animator.id, animator.name FROM animator ORDER BY (animator.NAME);";
 	protected static String selectAnimatorsWithVillage = "SELECT animator.id, animator.NAME, village.id, village.VILLAGE_NAME " +
 			"FROM animator JOIN village ON animator.village_id = village.id ORDER BY (animator.NAME);";
 	protected static String listAnimators = "SELECT a.id, a.name,p.id,p.partner_name,vil.id,vil.village_name "
