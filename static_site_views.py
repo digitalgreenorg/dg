32cef03c9575e22f85d5ada9678d3758c0195e59
Praveen Shekhar <praveen@digitalgreen.org>
2011-08-09 06:57:03 +0000

    added new urls for annual reports

--- static_site_views.py
+++ static_site_views.py
@@ -10,7 +10,11 @@ def home(request):
     
 def wondervillage(request):
    
-    return render_to_response('wondervillage.html')    
+    return render_to_response('wondervillage.html') 
+
+def annualreport(request):
+   
+    return render_to_response('annualreports.html')    
 
 def featuredfarmer(request):
    
@@ -32,29 +36,39 @@ def aboutus(request):
     
     return render_to_response('base_aboutusoverview.html')
 
-def annualreport(request):
+def annualreport09(request):
    
-    return render_to_response('annualreport.html')
+    return render_to_response('annualreport09-10.html')
 
-def annualletter(request):
-   
+def annualreport10(request):   
+    return render_to_response('annualreport10-11.html')
+
+def annualletter(request):   
     return render_to_response('annualletter.html')
 
-def projectprogress(request):
-   
+def annualletter10(request):   
+    return render_to_response('annualletter10-11.html')
+
+def projectprogress(request):   
     return render_to_response('projectprogress.html')
 
-def budgetprogress(request):
-   
+def projectprogress10(request):   
+    return render_to_response('projectprogress10-11.html')
+
+def budgetprogress(request):   
     return render_to_response('budgetprogress.html')
 
-def scalability(request):
-   
+def financial10(request):   
+    return render_to_response('financial10-11.html')
+
+def scalability(request):   
     return render_to_response('scalability.html')
 
-def lessonlearned(request):
-   
+def lessonlearned(request):   
     return render_to_response('lessonlearned.html')
+
+def challenge10(request):   
+    return render_to_response('challenge10.html')
 
 def keyprinciple(request):
     
@@ -64,76 +78,60 @@ def corevalue(request):
     
     return render_to_response('base_overviewcorevalue.html')
 
-def sop(request):
-    
+def sop(request):    
     return render_to_response('workprocedure.html')
 
-def overviewfarmer(request):
-    
+def overviewfarmer(request):    
     return render_to_response('base_overviewfarmer.html')
 
-def overviewdatabase(request):
-    
+def overviewdatabase(request):    
     return render_to_response('base_overviewdatabase.html')
 
-def overviewproduction(request):
-    
+def overviewproduction(request):    
     return render_to_response('base_overviewproduction.html')
 
-def overviewsequence(request):
-    
+def overviewsequence(request):    
     return render_to_response('base_overviewsequence.html')
 
-def overviewdiffusion(request):
-    
+def overviewdiffusion(request):    
     return render_to_response('base_overviewdiffusion.html')
 
-def overviewscalability(request):
-    
+def overviewscalability(request):    
     return render_to_response('base_overviewscalability.html')
 
-def overviewdistribution(request):
-    
+def overviewdistribution(request):    
     return render_to_response('base_overviewdistribution.html')
 
-def career(request):
-    
+def career(request):    
     return render_to_response('base_career.html')
 
-def careers(request):
-    
+def careers(request):    
     return render_to_response('base_career.html')
 
-def contact(request):
-    
+def contact(request):    
     return render_to_response('base_contact.html')
 
-def team(request):
-    
+def team(request):    
     return render_to_response('base_team.html')
 
-def teamboard(request):
-    
+
+def teamboard(request):    
     return render_to_response('base_team_board.html')
 
-def teamadviser(request):
-    
+
+def teamadviser(request):    
     return render_to_response('base_team_adviser.html')
 
-def teamacclaw(request):
-    
+def teamacclaw(request):    
     return render_to_response('base_team_acclaw.html')
 
-def teamintern(request):
-    
+def teamintern(request):    
     return render_to_response('base_team_intern.html')
 
-def teamalumni(request):
-    
+def teamalumni(request):    
     return render_to_response('base_team_alumni.html')
 
-def teammember(request):
-    
+def teammember(request):    
     return render_to_response('base_team_members.html')
 
 def press(request):
@@ -201,10 +199,10 @@ def tech(request):
     return render_to_response('technology.html')
 
 def update(request):
-    return HttpResponseRedirect('http://sites.digitalgreen.org/inside-digital-green/updates')
+    return HttpResponseRedirect('https://sites.google.com/a/digitalgreen.org/inside-digital-green/updates')
 
 def latestupdate(request):
-    return HttpResponseRedirect('http://sites.digitalgreen.org/inside-digital-green/updates/june292010transition')
+    return HttpResponseRedirect('https://sites.google.com/a/digitalgreen.org/inside-digital-green/updates/june292010transition')
 
 def nexus(request):
-    return HttpResponseRedirect('http://sites.digitalgreen.org/inside-digital-green/nexus')
+    return HttpResponseRedirect('https://sites.google.com/a/digitalgreen.org/inside-digital-green/nexus')
