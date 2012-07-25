9c08bbb01d912198c398888a88f6f71651db33c9
yashseth <yash@digitalgreen.org>
2012-07-25 14:47:39 +0530

    farmerbook settings update

--- base_settings.py
+++ base_settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+   # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
@@ -79,9 +79,16 @@ INSTALLED_APPS = (
     #'django.contrib.sites',
     'django.contrib.admindocs',
     'dashboard',
+<<<<<<< Updated upstream
     'debug_toolbar',
     'django.contrib.humanize',
     'south',
     'farmerbook'
    )
+=======
+   # 'debug_toolbar',
+    'south',
+    'video_practice_map',
+)
+>>>>>>> Stashed changes
 
