02d9e7ba8d899bc7ef8377930a98fc45b965530c
Rahul <rahul@digitalgreen.org>
2012-08-01 22:20:42 +0530

    Correcting bugs in targets.

--- output/views/targets.py
+++ output/views/targets.py
@@ -38,15 +38,12 @@ def target_table(request):
     achieved_vals['refresher_crp_traing'] = run_query(get_crp_tot_training(geog, id, from_date, to_date, partners))[0]['count'] - achieved_vals['new_crp_training']
     achieved_vals['vil_identified'] = run_query(get_village_identified(geog, id, from_date, to_date, partners))[0]['count']
     achieved_vals['geog_name'] = get_parent_geog_id(geog, id)[0]
-    achieved_vals['video_production'] = run_query(video_analytics_sql.video_tot_video(geog, id, from_date, to_date, partners))[0]['count']
-    
-    tot_val = run_query(screening_analytics_sql.totAttendees_totScreening_datediff(geog, id, from_date, to_date, partners))[0];
+    tot_val = run_query(shared_sql.get_totals(geog, id, from_date, to_date, partners, ['tot_vid', 'tot_ado', 'tot_att', 'tot_scr']))[0]
+    achieved_vals['video_production'] = tot_val['tot_vid']
     achieved_vals['disseminations'] = tot_val['tot_scr']
-    
-    tot_ado = run_query(adoption_analytics_sql.adoption_tot_ado(geog, id, from_date, to_date, partners))[0]['tot_ado']
     if(tot_val['tot_scr']):
-        achieved_vals['att_per_diss'] = float(tot_val['tot_per'])/tot_val['tot_scr']
-        achieved_vals['avg_adoption'] = float(tot_ado)/tot_val['tot_scr']
+        achieved_vals['att_per_diss'] = tot_val['tot_att']/tot_val['tot_scr']
+        achieved_vals['avg_adoption'] = tot_val['tot_ado']/tot_val['tot_scr']
     else:
         achieved_vals['att_per_diss'] = 0
         achieved_vals['avg_adoption'] = 0
