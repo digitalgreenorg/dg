8f4788c7a9879ab01c51c4435a843ed62870470d
shreyjairath <shrey@digitalgreen.org>
2012-08-01 13:46:22 +0530

    Schema and data migrations related to Practice Hierarchy and code changes in video_practice_map app to use dashboard.practice model instead of its own practicecombination model(now deleted)
    1.Added new practice hierarchy fields to dashboard.practice model and removed the video_practice_map.practicecombination model. Also migrated the data.
    2.Removed all references to practice table except from Video table
    2.Made changes to video_practice_map app to use dashboard.practice table and renamed the hierarchy models
    3.Added the new models to the admin

--- video_practice_map/models.py
+++ video_practice_map/models.py
@@ -1,25 +1,14 @@
 from dashboard.fields import *
-from dashboard.models import PracticeMain, PracticeSub, PracticeSector, \
+from dashboard.models import Practices, PracticeTopic, PracticeSubtopic, PracticeSector, \
     PracticeSubSector, PracticeSubject, Video
 from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 
-class PracticeCombination(models.Model):
-    id = BigAutoField(primary_key = True)
-    top_practice = BigForeignKey(PracticeSector) #Didn't rename variable to 'sector' to avoid unnecessary labor. This going to removed anyway.
-    sub_practice = BigForeignKey(PracticeSubSector, null=True)
-    utility = BigForeignKey(PracticeMain, null=True)
-    type = BigForeignKey(PracticeSub, null=True)
-    subject = BigForeignKey(PracticeSubject, null=True)
-    
-    class Meta:
-        unique_together = ("top_practice", "sub_practice", "utility", "type", "subject")
-    
 class VideoPractice(models.Model):
     id = BigAutoField(primary_key = True)
     video = BigForeignKey(Video)
-    practice = BigForeignKey(PracticeCombination)
+    practice = BigForeignKey(Practices)
     user = models.ForeignKey(User, null=True)
     review_user = models.ForeignKey(User, null=True, related_name='reviewed_practices')
     review_approved = models.NullBooleanField(null=True)
--- video_practice_map/views.py
+++ video_practice_map/views.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
-from dashboard.models import PracticeMain, PracticeSub, PracticeSector, \
-    PracticeSubSector, PracticeSubject, Video
+import json
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import Template, Context
-from video_practice_map.models import *
-import json
+
+from dashboard.models import Practices, PracticeTopic, PracticeSubtopic, PracticeSector, PracticeSubSector, PracticeSubject, Video
+from video_practice_map.models import VideoPractice,SkippedVideo
+
 
 @login_required(login_url='/videotask/login/')
 def home(request):
@@ -98,11 +100,11 @@ def logout_view(request):
 def form_submit(request):
     if request.POST:
         if 'yes' in request.POST:  #Submitted a new practice
-            field_arr = ['top_practice', 'sub_practice', 'utility', 'type', 'subject']
+            field_arr = ['practice_sector', 'practice_subsector', 'practice_topic', 'practice_subtopic', 'practice_subject']
             selected_vals = []
             for field in field_arr:
                 selected_vals.append(int(request.POST[field]) if request.POST[field] != "None" else None)
-            pr = PracticeCombination.objects.get(*zip(field_arr, selected_vals))
+            pr = Practices.objects.get(*zip(field_arr, selected_vals))
             vid = Video.objects.get(id=int(request.POST['vid_id']))
             if request.POST['action'] == 'review':
                 vid_pr = VideoPractice.objects.get(video=vid, review_user=None)
@@ -117,18 +119,21 @@ def form_submit(request):
                                         video=Video.objects.get(pk=int(request.POST['vid_id'])))
         
         elif 'accept' in request.POST:  #Accepted for review
-            vid_pr = VideoPractice.objects.get(video=Video.objects.get(id=int(request.POST['vid_id'])), review_user=None)
+            video=Video.objects.get(id=int(request.POST['vid_id']))
+            vid_pr = VideoPractice.objects.get(video=video, review_user=None)
             vid_pr.review_user = request.user
             vid_pr.review_approved = True
             vid_pr.save()
+            video.related_practice = vid_pr.practice
+            video.save()
             
     return HttpResponseRedirect('/videotask/home/')
 
 
 def practice_filter_options(request):
-    pr = PracticeCombination.objects.all()
-    field_arr = ['top_practice', 'sub_practice', 'utility', 'type', 'subject']
-    model_arr = [PracticeSector, PracticeSubSector, PracticeMain, PracticeSub, PracticeSubject]
+    pr = Practices.objects.all()
+    field_arr = ['practice_sector', 'practice_subsector', 'practice_topic', 'practice_subtopic', 'practice_subject']
+    model_arr = [PracticeSector, PracticeSubSector, PracticeTopic, PracticeSubtopic, PracticeSubject]
     output_arr = []
     for model, field in zip(model_arr, field_arr):
         if request.GET[field]:
@@ -160,7 +165,7 @@ def practice_filter_options(request):
     return HttpResponse(json.dumps(output_arr))
 
 def all_practice_options(request=None):
-    model_arr = [PracticeSector, PracticeSubSector, PracticeMain, PracticeSub, PracticeSubject]
+    model_arr = [PracticeSector, PracticeSubSector, PracticeTopic, PracticeSubtopic, PracticeSubject]
     value_arr = [model.objects.values_list('id', 'name').order_by('name') for model in model_arr]
     if request:
         output_arr = ["<option value=''>Select Sector</option>",
@@ -198,9 +203,7 @@ def add_new(request):
                         for main_practice in main_practices:
                             for sub_practice in sub_practices:
                                 for subject in subjects:
-                                    obj, created = PracticeCombination.objects.get_or_create(top_practice_id=sector, sub_practice_id=sub_sector,
-                                                                                             utility_id=main_practice, type_id=sub_practice,
-                                                                                             subject_id=subject)
+                                    obj, created = Practices.objects.get_or_create(practice_sector_id=sector, practice_subsector_id=sub_sector,practice_topic_id=main_practice, practice_subtopic_id=sub_practice,practice_subject_id=subject)
                                     if created:
                                         count = count + 1
                                     
@@ -209,8 +212,8 @@ def add_new(request):
             if not request.POST['name']:
                 msg = "Please enter name for %s" % (request.POST['form_type'].title())
             else:
-                model_class = dict(sector=PracticeSector, subsector=PracticeSubSector, practice=PracticeMain,
-                                   subpractice=PracticeSub, subject=PracticeSubject)
+                model_class = dict(sector=PracticeSector, subsector=PracticeSubSector, practice=PracticeTopic,
+                                   subpractice=PracticeSubtopic, subject=PracticeSubject)
                 obj, created = model_class[request.POST['form_type']].objects.get_or_create(name=request.POST['name'])
                 if created:
                     msg = "%s - %s created." % (request.POST['form_type'].title(), request.POST['name'])
@@ -219,11 +222,11 @@ def add_new(request):
 
     all_sectors = PracticeSector.objects.values_list('id', 'name').order_by('name')
     all_subsectors = PracticeSubSector.objects.values_list('id', 'name').order_by('name')
-    all_main = PracticeMain.objects.values_list('id', 'name').order_by('name')
-    all_subpr = PracticeSub.objects.values_list('id', 'name').order_by('name')
-    sector_subject_tups = PracticeCombination.objects.exclude(subject=None). values_list('top_practice__name', 
-                                                                                 'subject__id', 
-                                                                                 'subject__name').distinct().order_by('subject__name')
+    all_main = PracticeTopic.objects.values_list('id', 'name').order_by('name')
+    all_subpr = PracticeSubtopic.objects.values_list('id', 'name').order_by('name')
+    sector_subject_tups = Practices.objects.exclude(practice_subject=None). values_list('practice_sector__name', 
+                                                                                 'practice_subject__id', 
+                                                                                 'practice_subject__name').distinct().order_by('practice_subject__name')
     all_subjects = defaultdict(list)
     for sector_sub_tup in sector_subject_tups:
         all_subjects[sector_sub_tup[0]].append(list(sector_sub_tup)[1:])
