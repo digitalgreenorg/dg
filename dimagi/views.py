2524d775433ef6a14f6fdbf1f6460ed622aeda6d
Nandini Bhardwaj <nandini@digitalgreen.org>
2012-08-23 13:32:51 +0530

    Bugfix in view. XML data is available in request.raw_post_data not request.body.

--- dimagi/views.py
+++ dimagi/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from models import XMLSubmission
 
 def save_submission(request):
     submission = XMLSubmission()
-    submission.xml_data = request.body
+    submission.xml_data = request.raw_post_data
     submission.save() 
     return HttpResponse(status=201)
