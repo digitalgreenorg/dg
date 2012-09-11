be0b61f78311a31bddcd33b7d6e005c96d94c6a4
tverma <tanmay.verma1607@gmail.com>
2012-09-11 16:24:48 +0530

    Fix Script Path

--- scripts/update_village_precalculation.py
+++ scripts/update_village_precalculation.py
@@ -1,6 +1,6 @@
 import site, sys
 sys.path.append('/home/ubuntu/code/dg_git')
-site.addsitedir('/home/ubuntu/.virtualenv/dg_production/lib/python2.6/site-packages/')
+site.addsitedir('/home/ubuntu/.virtualenv/dg_production/lib/python2.7/site-packages/')
 
 from django.core.management import setup_environ
 import settings
