2845c89f74b2625bc2252da092d7027ad74ef720
tverma <tanmay.verma1607@gmail.com>
2012-10-03 12:26:06 +0530

    Adding SQL script to delete all myisam tables

---
+++ scripts/analytics_sync/delete_myisam_tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `screening_myisam`;
+DROP TABLE IF EXISTS `video_myisam`;
+DROP TABLE IF EXISTS `person_meeting_attendance_myisam`;
+DROP TABLE IF EXISTS `person_adopt_practice_myisam`;
+DROP TABLE IF EXISTS `village_precalculation_copy`;
