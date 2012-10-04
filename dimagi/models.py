7a6a4c1cef1dfc138c091ee5eb5b6fee54ed311d
yash <yash@digitalgreen.org>
2012-10-04 15:24:10 +0530

    updated script for saving mobile data
    added exception handling and error codes

--- dimagi/models.py
+++ dimagi/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+Error_list = {}
+Error_list [-1] = 'SCREEENING_SAVE_ERROR'
+Error_list [-2] = 'SCREEENING_REAS_ERROR'                                # Leave blank if you want the error string to be the Exception that was caught
+Error_list [-3] = 'ADOPTION_SAVE_ERROR'
+Error_list [-4] = 'ADOPTION_READ_ERROR'
+Error_list [-5] = 'PMA_SAVE_ERROR'
 
 class XMLSubmission(models.Model):
     submission_time = models.DateTimeField(auto_now=True)
     modification_time = models.DateTimeField(auto_now_add=True)
     xml_data = models.TextField()
+    error_code = models.IntegerField()
+    error_message = models.TextField()
