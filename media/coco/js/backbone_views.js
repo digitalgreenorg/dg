7764d7b2644863a03a8063e7a2e08b3df570dd1a
shreyjairath <shrey@digitalgreen.org>
2012-11-08 11:30:41 +0530

    +app cache - update ready event handler + app cache - network access for download

--- media/coco/js/backbone_views.js
+++ media/coco/js/backbone_views.js
@@ -1,6 +1,22 @@
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 $(document)
     .ready(function() {
-
     var village_list_view_configs = {
         'page_header': 'Villages',
         'backbone_collection': village_offline_collection,
