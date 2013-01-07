b4c29415f306f0400673dc80b8eaa1d22b05c8aa
shreyjairath <shreyjairath@gmail.com>
2013-01-07 14:20:59 +0530

    + bug fix  - adding message middleware - admin new user creation was crashing without it

--- base_settings.py
+++ base_settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware'
     #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
