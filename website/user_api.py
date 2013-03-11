fd7069249f520be27a68f207932cefbdb7d1f379
yash <yash@digitalgreen.org>
2013-03-11 10:53:56 +0530

    user_models and user_api

---
+++ website/user_api.py
@@ -0,0 +1,55 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+from website.user_models import CommentLike, TimeWatched, VideoLike, UserCollectionHistory
+from django.contrib.auth.models import User
+from tastypie.resources import ALL_WITH_RELATIONS, ALL
+from website.api import CollectionResource, VideoResource, CommentResource
+from tastypie.authorization import DjangoAuthorization, Authorization
+from tastypie.authentication import BasicAuthentication, Authentication
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        
+        
+class SignInResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'signIn'
+        filtering = {
+           "username": ALL,
+           "password": ALL,
+        }
+        
+        
+class UserCollectionHistoryResource(ModelResource):
+    completed = fields.ManyToManyField(CollectionResource, 'completed')
+    viewed = fields.ManyToManyField(CollectionResource, 'recentlyViewed')
+    liked = fields.ManyToManyField(CollectionResource, 'likedCollections')
+    class Meta:
+        queryset = UserCollectionHistory.objects.all()
+        resource_name = 'userCollectionHistory'
+        filtering = {
+           "userUID": ALL,
+        }
+        
+class VideoLikeResource(ModelResource):
+    video = fields.ForeignKey(VideoResource, 'videoUID')
+    class Meta:
+        queryset = VideoLike.objects.all()
+        resource_name = 'updateVideoLike'
+
+class CommentLikeResource(ModelResource):
+    comment = fields.ForeignKey(CommentResource, 'commentUID')
+    user = fields.ForeignKey(UserResource,'userUID')
+    class Meta:
+        queryset = CommentLike.objects.all()
+        resource_name = 'updateCommentLike'
+        authentication = Authentication()
+        authorization = Authorization()
+#class TimeWatchedResource(ModelResource):
+#    class Meta:
+#        queryset = TimeWatched.objects.all()
+#        resource_name = 'updateTimeWatched'
---
+++ website/user_models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from website.models import Collection, Comment, Video, User
+from django.db import models
+    
+class UserCollectionHistory(models.Model):
+    userUID = models.ForeignKey(User)
+    completed = models.ManyToManyField('Collection', related_name = 'completed')
+    recentlyViewed = models.ManyToManyField('Collection', related_name = 'viewed')
+    likedCollections = models.ManyToManyField('Collection', related_name = 'liked')
+    
+class VideoLike(models.Model):
+    videoUID = models.ForeignKey('Video')
+    userUID = models.ForeignKey(User)
+    # not putting liked (boolean field)
+    
+class CommentLike(models.Model):
+    commentUID = models.ForeignKey('Comment')
+    userUID = models.ForeignKey(User)
+    # not putting liked (boolean field)
+
+class TimeWatched(models.Model):
+    videoWatchRecord = models.ForeignKey('VideoWatchRecord')
+    
+    
+    
+
+    
