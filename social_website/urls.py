5999146f19d6883ed48bf9d98928f5516b25a55c
Nandini Bhardwaj <nandini@digitalgreen.org>
2013-04-09 11:13:03 +0530

    Basic page move to django complete. Integrating the django templating engine and API remain.

--- social_website/urls.py
+++ social_website/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import patterns
 from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('',
+urlpatterns = patterns('',   
     (r'^main.js$', direct_to_template, {'template': 'main.js', 'mimetype':'text/javascript'}),
+    (r'^$', direct_to_template, {'template': 'home.html', 'extra_context': {'header': {'jsController':'Home', 'loggedIn':False}}}),
+    (r'^discover/$', direct_to_template, {'template': 'collections.html', 'extra_context': {'header': {'jsController':'Collections', 'currentPage':'Discover', 'loggedIn':False}}}),
+    (r'^collections/$', direct_to_template, {'template': 'collections-view.html', 'extra_context': {'header': {'jsController':'ViewCollections', 'loggedIn':False}}}),
     (r'^about/$', direct_to_template, {'template': 'about.html', 'extra_context': {'header': {'jsController':'About', 'currentPage':'About', 'loggedIn':False}}}),
-    (r'^connect/$', direct_to_template, {'template': 'about.html', 'extra_context': {'header': {'jsController':'About', 'currentPage':'Connect', 'loggedIn':False}}}),
+    (r'^connect/$', direct_to_template, {'template': 'profile.html', 'extra_context': {'header': {'jsController':'Profile', 'currentPage':'Connect', 'loggedIn':False}}}),
 )
