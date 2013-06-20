0bd68a1a9f1f35bf3de6f3d3f17af447fe90802f
Nandini Bhardwaj <nandini@digitalgreen.org>
2013-06-20 14:04:48 +0530

    Removing raven and sentry.

--- base_settings.py
+++ base_settings.py
@@ -95,7 +95,6 @@ INSTALLED_APPS = (
     'dashboard',
     #'debug_toolbar',
     'output',
-    'raven.contrib.django',
     'django.contrib.humanize',
     'south',
     'farmerbook',
@@ -103,45 +102,3 @@ INSTALLED_APPS = (
     'path',
     'fbconnect'
 )
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'WARNING',
-        'handlers': ['sentry'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'sentry': {
-            'level': 'ERROR',
-            'class': 'raven.contrib.django.handlers.SentryHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-    },
-}
