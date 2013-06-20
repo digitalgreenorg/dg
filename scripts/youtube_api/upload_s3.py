bc17dd3d41a0934e26fa9abeb17ff75b50f9bd0c
Aadish Gupta <aadish@digitalgreen.org>
2013-06-20 11:50:34 +0530

    added new html pages overlay corrected youtube id changed

--- scripts/youtube_api/upload_s3.py
+++ scripts/youtube_api/upload_s3.py
@@ -3,25 +3,35 @@ import os
 from boto.s3.key import Key
 
 
-ACCESS_KEY = '01GE4NJEXRFQTBCFG782'
-SECRET_KEY = 'bK8gt4siHBryH/cRagSMtcDPwNbfB0l2E/KXVhYy'
+ACCESS_KEY = 'AKIAIKTVJKVVHA7Y4HNA'
+SECRET_KEY = '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'
 BUCKET_NAME = 'video_thumbnail'
-
+ 
 con = S3Connection(ACCESS_KEY, SECRET_KEY)
-
-bucket = con.get_bucket(BUCKET_NAME)
+ 
+bucket = con.create_bucket(BUCKET_NAME)
 bucket.set_acl('public-read')
 
 files_list = os.listdir(r'C:\Users\Aadish\Desktop\image_upload_video_id')
- 
-for file in files_list:
-     if not bucket.get_key(file):
-         print file
-         print type(file)
-         k = Key(bucket)
-         k.key = file
-         k.set_contents_from_filename(file)
-         k.make_public()
+for root, dirs, files in os.walk(r'C:\Users\Aadish\Desktop\image_upload_video_id'):
+    print (len(files))
+    for file in files:
+        print (file)
+        key = file
+        if (root[-1] == 'd'):
+            key = 'raw/' + file
+        if (root[-1] == '6'):
+            key = '16by9/' + file
+        if (root[-1] == '4'):
+            key = '4by3/' + file
+        if not bucket.get_key(key):
+            print root
+            print file
+            k = Key(bucket)
+            k.key = key
+            k.set_contents_from_filename(root+'\\'+file)
+            k.make_public()
+
  
 
   
