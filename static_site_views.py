2920ef9d1a6b65a1480bb46b0f7bbde9d53286b5
koneti chandu <koneti@digitalgreen.org>
2013-07-03 14:56:27 +0530

    split into queries

--- static_site_views.py
+++ static_site_views.py
@@ -186,7 +186,9 @@ def partnerexecutive(request):
 
 def partnerspring(request):
     ids = [6000017889,6000017713,6000017896,6000016589,6000008464,6000007313]
-    vids = list(Video.objects.filter(id__in = ids).select_related().annotate(adoptions=Count('personadoptpractice')))
+    vids = list(Video.objects.filter(id__in = ids).select_related())
+    for vid in vids:    
+        vid.adoptions = PersonAdoptPractice.objects.filter(video=vid.id).count()
     return render_to_response('base_partner_spring.html', dict(vids = vids))
 
 def partnerresearch(request):
