f65f87968f9bcbd467b119dc1acae9333cb2159e
yash <yash@digitalgreen.org>
2013-07-11 21:11:20 +0530

    correcting adoption

--- dimagi/views.py
+++ dimagi/views.py
@@ -23,7 +23,7 @@ def save_in_db(submission):
     data = xml_parse.getElementsByTagName('data')
     if data[0].attributes["name"].value.lower() == 'screening form' :
         status, msg = save_mobile_data.save_screening_data(xml_parse)
-    elif data[0].attributes["name"].value.lower() == 'adoption form' :
+    elif data[0].attributes["name"].value.lower() == 'adoption' :
         status, msg = save_mobile_data.save_adoption_data(xml_parse)
     else :
         status = -1
