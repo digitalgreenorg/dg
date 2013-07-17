4943cd219cf5f8481dca5d6c858b39c91568440b
yash <yash@digitalgreen.org>
2013-07-17 13:31:37 +0530

    collection view page + trial on show more on partner page

--- media/social_website/scripts/app/controllers/ProfileController.js
+++ media/social_website/scripts/app/controllers/ProfileController.js
@@ -28,9 +28,11 @@ define(function(require) {
         constructor: function(bootstrapConfig, globalHelpers) {
             this.base(bootstrapConfig, globalHelpers);
             var references = this._references;
+            var currentPage = 0;
+            var collectionsPerPage = 4;
 
             references.collectionViewController
-                .setCollectionsPerPage(12)
+                .setCollectionsPerPage(4)
                 .setCollectionsPerRow(4)
                 .setVideosPerDrawer(5);
 
@@ -39,7 +41,7 @@ define(function(require) {
             var partner_uid = $(".js-partner-farmers-pages-container").attr('data-partnerID') ;
             this._references.collectionViewController._references.dataFeed.addInputParam('uid', true, 0, true);
             this._references.collectionViewController._references.dataFeed.setInputParam('uid', partner_uid, true);
-            this._getCollections();
+            this._getCollections(0,4);
 
             return this;
         },
@@ -58,6 +60,8 @@ define(function(require) {
             
             var $partnerFarmersCarouselContainer = jQuery(".js-partner-farmers-carousel-container");
             references.partnerFarmersViewController = new PartnerFarmersViewController($partnerFarmersCarouselContainer);
+            references.$showMoreButton = jQuery(".js-show-more");
+            references.$hideCollectionsButton = jQuery(".js-hide-collections");
         },
         
         _initEvents: function() {
@@ -68,14 +72,87 @@ define(function(require) {
 
             boundFunctions.onOrderChanged = this._onOrderChanged.bind(this);
             references.collectionMostFiltersViewController.on('orderChanged', boundFunctions.onOrderChanged);
+            
+            // show more
+            boundFunctions.onShowMoreClick = this._onShowMoreClick.bind(this);
+            references.$showMoreButton.on('click', boundFunctions.onShowMoreClick);
+
+            boundFunctions.onHideCollectionsClick = this._onHideCollectionsClick.bind(this);
+            references.$hideCollectionsButton.on('click', boundFunctions.onHideCollectionsClick);
         },
+        
         
         _onOrderChanged: function(orderCriteria) {
             this._references.collectionViewController.setInputParam('order_by', orderCriteria);
         },
 
-        _getCollections: function() {
-            this._references.collectionViewController.getCollections();
+        _getCollections: function(page,count) {
+            return this._references.collectionViewController.getCollections(page,count);
+        },
+        
+        _onShowMoreClick: function(e) {
+            e.preventDefault();
+            this._references.collectionViewController._references.dataFeed.setInputParam('offset', 4, true);
+            var collectionData = this._getCollections(1,4);
+            this._renderPartialCollections
+            
+            //this._getCollectionsPage('next');
+        },
+        
+        _onHideCollectionsClick: function(e) {
+            e.preventDefault();
+
+            var references = this._references;
+
+            var $collectionsPages = references.$collectionsContainer.find('.js-collections-page');
+
+            if ($collectionsPages.length <= 1) {
+                return;
+            }
+
+            var $pageToRemove = $collectionsPages.eq($collectionsPages.length - 1);
+            var updateHideCollectionsButtonDisplayBind = this._updateHideCollectionsButtonDisplay.bind(this);
+            $pageToRemove
+                .animate({
+                    height: '0px'
+                }, function() {
+                    $pageToRemove.remove();
+                    updateHideCollectionsButtonDisplayBind();
+                });
+
+            this._setCollectionsPage('previous');
+
+            references.$showMoreButton.show();
+        },
+        
+        _renderPartialCollections: function(collectionHTML) {
+            var $div = jQuery('<div />');
+            $div.append(collectionHTML);
+            var $page = $div.find('.js-collections-page');
+            $page.css({
+                position: 'absolute',
+                visibility: 'hidden'
+            });
+
+            this._references.$collectionsContainer.append($page);
+
+            var height = $page.height();
+            $page.css({
+                height: '0px',
+                position: '',
+                visibility: ''
+            });
+
+            $page.animate({
+                height: height + 'px'
+            }, function() {
+                $page.css({
+                    height: ''
+                });
+            });
+            
+            //this._updateHideCollectionsButtonDisplay();
+            //this._initVideoCarousels();
         },
         /**
          * Controller destructor
