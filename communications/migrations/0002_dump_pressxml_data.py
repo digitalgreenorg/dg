6e8215fa5a821c86a8bb63402d3218f304ad13a0
koneti chandu <koneti@digitalgreen.org>
2013-07-26 14:44:44 +0530

    relative path given

--- communications/migrations/0002_dump_pressxml_data.py
+++ communications/migrations/0002_dump_pressxml_data.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import datetime
-import unicodedata
+import datetime, unicodedata, os
+from dg.base_settings import PROJECT_PATH
 from communications.models import Article
 from django.db import models
 from south.db import db
@@ -12,7 +12,7 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        xmldoc = minidom.parse('C:\Users\chandu\Documents\dg\dg\media\Output\press.xml')
+        xmldoc = minidom.parse(os.path.join(PROJECT_PATH, 'media\Output\press.xml'))
         itemlist = xmldoc.getElementsByTagName('TEAM')
         for item in itemlist:
             source = item.getElementsByTagName('TITLE')[0].childNodes[0].nodeValue
