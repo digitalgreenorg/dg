532931020dd72267c49e16f029a133bae19338f5
yash <yash@digitalgreen.org>
2013-07-27 19:16:00 +0530

    combined activity, partner, comment, videolike and userprofile schemamigrations into one migration

--- social_website/migrations/0003_auto__add_userprofile__add_videolike__add_milestone__add_field_comment.py
+++ social_website/migrations/0003_auto__add_userprofile__add_videolike__add_milestone__add_field_comment.py
@@ -8,9 +8,52 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'UserProfile'
+        db.create_table('social_website_userprofile', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=30)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal('social_website', ['UserProfile'])
+
+        # Adding model 'VideoLike'
+        db.create_table('social_website_videolike', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('video', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['social_website.Video'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['social_website.UserProfile'])),
+        ))
+        db.send_create_signal('social_website', ['VideoLike'])
+
+        # Adding model 'Milestone'
+        db.create_table('social_website_milestone', (
+            ('uid', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('partner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['social_website.Partner'], unique=True)),
+            ('videoNumber', self.gf('django.db.models.fields.IntegerField')()),
+            ('villageNumber', self.gf('django.db.models.fields.IntegerField')()),
+            ('screeningNumber', self.gf('django.db.models.fields.IntegerField')()),
+            ('viewerNumber', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal('social_website', ['Milestone'])
+
+        # Adding field 'Comment.user'
+        db.add_column('social_website_comment', 'user',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['social_website.UserProfile'], null=True, blank=True),
+                      keep_default=False)
+
+
+        # Changing field 'Comment.person'
+        db.alter_column('social_website_comment', 'person_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['social_website.Person'], null=True))
         # Adding field 'Partner.full_name'
         db.add_column('social_website_partner', 'full_name',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=250),
+                      self.gf('django.db.models.fields.CharField')(default=2, max_length=250),
                       keep_default=False)
 
         # Adding field 'Partner.location_image'
@@ -19,12 +62,49 @@ class Migration(SchemaMigration):
                       keep_default=False)
 
 
+        # Changing field 'Partner.logoURL'
+        db.alter_column('social_website_partner', 'logoURL', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True))
+        # Adding field 'Activity.type'
+        db.add_column('social_website_activity', 'type',
+                      self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=2),
+                      keep_default=False)
+
+        # Adding field 'Activity.titleURL'
+        db.add_column('social_website_activity', 'titleURL',
+                      self.gf('django.db.models.fields.URLField')(default=2, max_length=400),
+                      keep_default=False)
+
+
     def backwards(self, orm):
+        # Deleting model 'UserProfile'
+        db.delete_table('social_website_userprofile')
+
+        # Deleting model 'VideoLike'
+        db.delete_table('social_website_videolike')
+
+        # Deleting model 'Milestone'
+        db.delete_table('social_website_milestone')
+
+        # Deleting field 'Comment.user'
+        db.delete_column('social_website_comment', 'user_id')
+
+
+        # Changing field 'Comment.person'
+        db.alter_column('social_website_comment', 'person_id', self.gf('django.db.models.fields.related.ForeignKey')(default=2, to=orm['social_website.Person']))
         # Deleting field 'Partner.full_name'
         db.delete_column('social_website_partner', 'full_name')
 
         # Deleting field 'Partner.location_image'
         db.delete_column('social_website_partner', 'location_image')
+
+
+        # Changing field 'Partner.logoURL'
+        db.alter_column('social_website_partner', 'logoURL', self.gf('django.db.models.fields.files.ImageField')(default=2, max_length=100))
+        # Deleting field 'Activity.type'
+        db.delete_column('social_website_activity', 'type')
+
+        # Deleting field 'Activity.titleURL'
+        db.delete_column('social_website_activity', 'titleURL')
 
 
     models = {
@@ -65,11 +145,12 @@ class Migration(SchemaMigration):
         },
         'social_website.comment': {
             'Meta': {'object_name': 'Comment'},
-            'date': ('django.db.models.fields.DateField', [], {}),
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 7, 27, 0, 0)'}),
             'isOnline': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['social_website.Person']", 'null': 'True', 'blank': 'True'}),
             'text': ('django.db.models.fields.TextField', [], {}),
             'uid': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['social_website.UserProfile']", 'null': 'True', 'blank': 'True'}),
             'video': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['social_website.Video']"})
         },
         'social_website.featuredcollection': {
@@ -129,6 +210,20 @@ class Migration(SchemaMigration):
             'videoID': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
             'views': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'})
         },
+        'social_website.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
         'social_website.video': {
             'Meta': {'object_name': 'Video'},
             'adoptions': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
@@ -153,6 +248,12 @@ class Migration(SchemaMigration):
             'topic': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
             'uid': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'youtubeID': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'social_website.videolike': {
+            'Meta': {'object_name': 'VideoLike'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['social_website.UserProfile']"}),
+            'video': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['social_website.Video']"})
         }
     }
 
