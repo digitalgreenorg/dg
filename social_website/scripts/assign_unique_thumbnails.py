b1a4f12ac4851223055dddadf39da34865931e9b
yash <yash@digitalgreen.org>
2013-07-30 17:43:09 +0530

    assigning default value to collection thumbnail in case no video found which already hasnt been used

--- social_website/scripts/assign_unique_thumbnails.py
+++ social_website/scripts/assign_unique_thumbnails.py
@@ -6,11 +6,13 @@ from social_website.models import Collection
 used_vid_list = []
 done = 0
 for collection in Collection.objects.all():
-    for video in collection.videos.all():
+    vids = collection.videos.all()
+    collection.thumbnailURL = vids[0].thumbnailURL16by9
+    for video in vids:
         if video.uid not in used_vid_list:
             collection.thumbnailURL = video.thumbnailURL16by9
             used_vid_list.append(video.uid)
             done += 1
-            collection.save()
             break
+    collection.save()
 print '%s collection assigned unique thumbnails' % done
