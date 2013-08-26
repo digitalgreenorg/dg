41dd11f3702defbd82ff7e25fc03d31c3b0e8896
yash <yash@digitalgreen.org>
2013-08-26 17:56:47 +0530

    removed pap, added delete functionality + time update at the end of script

--- social_website/scripts/website_sync.py
+++ social_website/scripts/website_sync.py
@@ -4,51 +4,60 @@ setup_environ(dg.settings)
 
 import datetime
 from dashboard import models
-from social_website.models import CronTimestamp
-from social_website.migration_functions import delete_person, delete_video, populate_adoptions, populate_farmers, update_person_video_record, update_questions_asked, update_website_video
+from social_website.models import CronTimestamp, PersonVideoRecord
+from social_website.migration_functions import delete_person, delete_video, initial_personvideorecord, populate_farmers, update_questions_asked, update_website_video
 
 
 def process_log(objects, logfile):
-    pma_count = 0
     for object in objects:
         logfile.write(object.entry_table)
         if object.entry_table == 'Screening':
             screening_object = models.Screening.objects.get(id = object.model_id)
-            videos = screening_object.videoes_screened.all()
             for pma in screening_object.personmeetingattendance_set.all():
-                update_person_video_record(pma, videos)
                 update_questions_asked(pma)
                 logfile.write(' Added Pma ')
-                pma_count += 1
-        elif object.entry_table == "PersonAdoptPractice":
-            pap_object = models.PersonAdoptPractice.objects.get(id = object.model_id)
-            populate_adoptions(pap_object)
-            logfile.write(' Added Pap ')
+#        elif object.entry_table == "PersonAdoptPractice":
+#            pap_object = models.PersonAdoptPractice.objects.get(id = object.model_id)
+#            populate_adoptions(pap_object)
+#            logfile.write(' Added Pap ')
         elif object.entry_table == 'Video':
             video_object = models.Video.objects.get(id = object.model_id)
             if object.action == -1:
                 delete_video(video_object)
+                logfile.write(' Deleted Video ')
             update_website_video(video_object)
             logfile.write(' Added Video ')
         elif object.entry_table == 'Person':
             person_object = models.Person.objects.get(id = object.model_id)
             if object.action == -1:
                 delete_person(person_object)
+                logfile.write(' Deleted Person ')
             if person_object.image_exists :
                 populate_farmers(person_object)
                 logfile.write(' Added Person ')
         logfile.write('\n')
-        print pma_count
         
         
          
 
 script_name = 'website_sync'
+
+# Drop and re-create PersonVIdeoRecord
+#PersonVideoRecord.objects.all().delete()
+#initial_personvideorecord()
+#print "PVR done"
 file_name = script_name + str(datetime.datetime.now().date()) + ".txt"
 logfile = open(file_name, "w")
-timestamp = CronTimestamp.objects.get(name = script_name).last_time
+crontimestamp = CronTimestamp.objects.get(name = script_name)
+timestamp = crontimestamp.last_time
 serverlog_objects = models.ServerLog.objects.filter(timestamp__gte = timestamp)
-process_log(serverlog_objects[:100], logfile)
+
+# Update last_time but dont save
+crontimestamp.last_time = datetime.datetime.now()
+process_log(serverlog_objects[:200], logfile)
+crontimestamp.save()
+
+
 
     
 
