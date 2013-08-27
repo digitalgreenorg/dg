6513bc428aefbbde055fef3b9e5cc94552e35ebf
yash <yash@digitalgreen.org>
2013-08-27 17:14:16 +0530

    made 3 functions, intial, person, closing and now using them to make xml

--- dimagi/scripts/userfile_functions.py
+++ dimagi/scripts/userfile_functions.py
@@ -17,6 +17,36 @@ def read_userfile(file):
     data = json.loads(json_data)
     return data
 
+def write_opening_meta(file, num_people):
+    file.write('<?xml version="1.0" ?>\n')
+    file.write('<data uiVersion="1" version="8" name="New Form" xmlns:jrm="http://dev.commcarehq.org/jr/xforms" xmlns="http://openrosa.org/formdesigner/DB63E17D-B572-4F5B-926E-061583DAE9DA">\n')
+    file.write('<num_people>' + unicode(num_people) + '</num_people>\n')
+    
+def write_person_content(file, i, case_id, owner_id, person, videos_seen):
+    file.write('<people>\n')
+    file.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
+    file.write('<n'+unicode(i)+':create>\n')
+    file.write('<n'+unicode(i)+':case_type>person</n'+unicode(i)+':case_type>\n')
+    file.write('<n'+unicode(i)+':owner_id>'  + owner_id + '</n'+unicode(i)+':owner_id>\n')
+    file.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
+    file.write('</n'+unicode(i)+':create>\n')
+    file.write('<n'+unicode(i)+':update>\n')
+    file.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
+    file.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
+    file.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
+    file.write('<n'+unicode(i)+':videos_adopted>' + '' + '</n'+unicode(i)+':videos_adopted>\n')
+    file.write('</n'+unicode(i)+':update>\n')
+    file.write('</n'+unicode(i)+':case>\n')
+    file.write('</people>\n')
+    
+def write_closing_meta(file, owner_id, i):
+    file.write('<n'+unicode(i) + ':meta xmlns:n' + unicode(i) + '="http://openrosa.org/jr/xforms">\n')
+    file.write('<n'+unicode(i) + ':userID>'+unicode(owner_id) + '</n' + unicode(i) + ':userID>\n')
+    file.write('<n'+unicode(i) + ':instanceID>' + unicode(uuid.uuid4()) + '</n' + unicode(i) + ':instanceID>\n')
+    file.write('</n' + unicode(i) + ':meta>\n')
+    file.write('</data>')
+    
+
 def write_person_detail(person_id, filename): #used for updating case..
     # Retrieving the mapping from pickle files
     dir = os.path.dirname(__file__)
@@ -29,18 +59,14 @@ def write_person_detail(person_id, filename): #used for updating case..
     case_person_dict = pickle.load(fp)
     fp.close()
     
-    person = Person.objects.get(id = person_id)
-    owner_id = case_user_dict[person_id]
-    case_id = case_person_dict[person_id]
-    
-    f = codecs.open(filename, "w",'utf-8')
     Person = get_model('dashboard','Person')
     PersonMeetingAttendance = get_model('dashboard','PersonMeetingAttendance')
     PersonAdoptPractice = get_model('dashboard','PersonAdoptPractice')
-    f.write('<?xml version="1.0" ?>\n')
-    f.write('<data uiVersion="1" version="8" name="New Form" xmlns:jrm="http://dev.commcarehq.org/jr/xforms" xmlns="http://openrosa.org/formdesigner/DB63E17D-B572-4F5B-926E-061583DAE9DA">\n')
-    f.write('<num_people>' + unicode(1) + '</num_people>\n')
-    i = 0
+    person = Person.objects.get(id = person_id)
+    
+    owner_id = case_user_dict[person_id]
+    case_id = case_person_dict[person_id]
+    
     # Getting list of videos seen
     vids = PersonMeetingAttendance.objects.filter(person = person).values_list('screening__videoes_screened', flat = True)
     videos_seen = ''
@@ -51,40 +77,22 @@ def write_person_detail(person_id, filename): #used for updating case..
     videos_adopted = ''
     for vid in adopts:
         videos_adopted = videos_adopted + unicode(vid) + ' '
-        # Putting all the info in xml tags
-    f.write('<people>\n')
-    f.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
-    f.write('<n'+unicode(i)+':create>\n')
-    f.write('<n'+unicode(i)+':case_type>person</n'+unicode(i)+':case_type>\n')
-    f.write('<n'+unicode(i)+':owner_id>'  + owner_id + '</n'+unicode(i)+':owner_id>\n')
-    f.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
-    f.write('</n'+unicode(i)+':create>\n')
-    f.write('<n'+unicode(i)+':update>\n')
-    f.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
-    f.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
-    f.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
-    f.write('<n'+unicode(i)+':videos_adopted>' + '' + '</n'+unicode(i)+':videos_adopted>\n')
-    f.write('</n'+unicode(i)+':update>\n')
-    f.write('</n'+unicode(i)+':case>\n')
-    f.write('</people>\n')
+    # Write xml for a particular person
+    file = codecs.open(filename, "w",'utf-8')
+    write_opening_meta(file, 1)
+    i = 0
+    write_person_content(file, i, case_id, owner_id, person, videos_seen)
     i+= 1
-    # Writing closing meta info of the form        
-    f.write('<n'+unicode(i) + ':meta xmlns:n' + unicode(i) + '="http://openrosa.org/jr/xforms">\n')
-    f.write('<n'+unicode(i) + ':userID>2523fc995ccfd1d27c15111ec8987be6</n' + unicode(i) + ':userID>\n')
-    f.write('<n'+unicode(i) + ':instanceID>2729386f-7fd2-42cc-807f-786bf2dc952b</n' + unicode(i) + ':instanceID>\n')
-    f.write('</n' + unicode(i) + ':meta>\n')
-    f.write('</data>')
-    f.close()
-    return f
+    write_closing_meta(file, owner_id, i)    
+    file.close()
+    return file
 
 def write_full_case_list(person_list, filename, user_id): #for generating cases
-    f = codecs.open(filename, "w",'utf-8')
+    file = codecs.open(filename, "w",'utf-8')
     Person = get_model('dashboard','Person')
     PersonMeetingAttendance = get_model('dashboard','PersonMeetingAttendance')
     PersonAdoptPractice = get_model('dashboard','PersonAdoptPractice')
-    f.write('<?xml version="1.0" ?>\n')
-    f.write('<data uiVersion="1" version="8" name="New Form" xmlns:jrm="http://dev.commcarehq.org/jr/xforms" xmlns="http://openrosa.org/formdesigner/DB63E17D-B572-4F5B-926E-061583DAE9DA">\n')
-    f.write('<num_people>' + unicode(len(person_list)) + '</num_people>\n')
+    write_opening_meta(file, len(person_list))
     i = 0
     case_user_dict = {}
     case_person_dict = {}
@@ -105,33 +113,14 @@ def write_full_case_list(person_list, filename, user_id): #for generating cases
         for vid in adopts:
             videos_adopted = videos_adopted + unicode(vid) + ' '
         # Putting all the info in xml tags
-        f.write('<people>\n')
-        f.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
-        f.write('<n'+unicode(i)+':create>\n')
-        f.write('<n'+unicode(i)+':case_type>person</n'+unicode(i)+':case_type>\n')
-        f.write('<n'+unicode(i)+':owner_id>'  + owner_id + '</n'+unicode(i)+':owner_id>\n')
-        f.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
-        f.write('</n'+unicode(i)+':create>\n')
-        f.write('<n'+unicode(i)+':update>\n')
-        f.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
-        f.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
-        f.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
-        f.write('<n'+unicode(i)+':videos_adopted>' + '' + '</n'+unicode(i)+':videos_adopted>\n')
-        f.write('</n'+unicode(i)+':update>\n')
-        f.write('</n'+unicode(i)+':case>\n')
-        f.write('</people>\n')
-        # Writing closing meta info of the form
+        write_person_content(file, i, case_id, owner_id, person, videos_seen)
         i += 1
-        
-    f.write('<n'+unicode(i) + ':meta xmlns:n' + unicode(i) + '="http://openrosa.org/jr/xforms">\n')
-    f.write('<n'+unicode(i) + ':userID>' + owner_id  + '</n' + unicode(i) + ':userID>\n')
-    f.write('<n'+unicode(i) + ':instanceID>2729386f-7fd2-42cc-807f-786bf2dc952b</n' + unicode(i) + ':instanceID>\n')
-    f.write('</n' + unicode(i) + ':meta>\n')
-    f.write('</data>')
-    f.close()
+    
+    write_closing_meta(file, owner_id, i)    
+    file.close()
     pickle.dump(case_user_dict, open('case_user','w')) #mapping of case with user
     pickle.dump(case_person_dict, open('case_person','w')) #mapping of case with person(farmer)
-    return f
+    return file
 
 def make_upload_file(villages, filename, user_id):
     Person = get_model('dashboard','Person')
