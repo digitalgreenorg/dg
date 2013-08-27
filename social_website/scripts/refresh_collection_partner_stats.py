97f751ca0c5682e567368da89973f196c1b42a9a
yash <yash@digitalgreen.org>
2013-08-27 21:28:19 +0530

    added video bit also

--- social_website/scripts/refresh_collection_partner_stats.py
+++ social_website/scripts/refresh_collection_partner_stats.py
@@ -1,8 +1,15 @@
 from django.core.management import setup_environ
 import dg.settings
 setup_environ(dg.settings)
-from social_website.migration_functions import populate_collection_stats, populate_partner_stats
-from social_website.models import Collection, Partner
+from social_website.migration_functions import get_offline_stats, populate_collection_stats, populate_partner_stats
+from social_website.models import Collection, Partner, Video
+
+for video in Video.objects.all():
+    stats = get_offline_stats(video.coco_id)
+    video.offlineLikes = stats['like__sum']
+    video.offlineViews = stats['views__sum']
+    video.adoptions = stats['adopted__sum']
+    video.save()
 
 for collection in Collection.objects.all():
     populate_collection_stats(collection)
