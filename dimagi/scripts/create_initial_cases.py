481d1c513f85fe3b1963949240f35270ef17cd15
koneti chandu <koneti@digitalgreen.org>
2013-08-27 16:50:05 +0530

    changes in creating cases

--- dimagi/scripts/create_initial_cases.py
+++ dimagi/scripts/create_initial_cases.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-import settings
+import os
+import dg.settings
 from django.core.management import setup_environ
-setup_environ(settings)
+setup_environ(dg.settings)
 from userfile_functions import read_userfile, make_upload_file, upload_file
 
-data = read_userfile('userfile.json')
+data = read_userfile(os.path.dirname(__file__)+'\cases.json')
 for entry in data:
     print entry['username']
     villages = entry['villages']
     filename = entry['username'] + '.xml'
-    file_to_upload = make_upload_file(villages, filename)
+    file_to_upload = make_upload_file(villages, filename, entry['user_id'])
     break
     
     
--- dimagi/scripts/userfile_functions.py
+++ dimagi/scripts/userfile_functions.py
@@ -1,12 +1,11 @@
-import settings
+import dg.settings
 from django.core.management import setup_environ
-setup_environ(settings)
+setup_environ(dg.settings)
 import csv,datetime, json, urllib2, uuid, pickle,os
 from django.db.models import get_model
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
-
-
+import codecs
 
 def write_userfile(file, data):
     f = open(file,'w')
@@ -14,17 +13,26 @@ def write_userfile(file, data):
         
 def read_userfile(file):
     json_data=open(file).read()
+    print json_data
     data = json.loads(json_data)
     return data
 
-def write_person_detail(person_id, filename, i, case_id):
+def write_person_detail(person_id, filename): #used for updating case..
+    # Retrieving the mapping from pickle files
     dir = os.path.dirname(__file__)
     filepath = os.path.join(dir,'case_user')
     fp = open(filepath,'rb')
-    loaded = pickle.load(fp)
+    case_user_dict = pickle.load(fp)
     fp.close()
-    case_user_dict = loaded['case_user_dict']
-    import codecs
+    filepath = os.path.join(dir,'case_person')
+    fp = open(filepath,'rb')
+    case_person_dict = pickle.load(fp)
+    fp.close()
+    
+    person = Person.objects.get(id = person_id)
+    owner_id = case_user_dict[person_id]
+    case_id = case_person_dict[person_id]
+    
     f = codecs.open(filename, "w",'utf-8')
     Person = get_model('dashboard','Person')
     PersonMeetingAttendance = get_model('dashboard','PersonMeetingAttendance')
@@ -32,40 +40,35 @@ def write_person_detail(person_id, filename, i, case_id):
     f.write('<?xml version="1.0" ?>\n')
     f.write('<data uiVersion="1" version="8" name="New Form" xmlns:jrm="http://dev.commcarehq.org/jr/xforms" xmlns="http://openrosa.org/formdesigner/DB63E17D-B572-4F5B-926E-061583DAE9DA">\n')
     f.write('<num_people>' + unicode(1) + '</num_people>\n')
-    for n in range(0,1):
-        owner_id = case_user_dict[case_id]
-        person = Person.objects.get(id = person_id)
-    #    f = open(filename,'w')
-    #    owner_id = 'f097d1d213ad3a08fee6643133465660'
-        # Getting list of videos seen
-        vids = PersonMeetingAttendance.objects.filter(person = person).values_list('screening__videoes_screened', flat = True)
-        videos_seen = ''
-        for vid in vids:
-            videos_seen = videos_seen + unicode(vid) + ' '
-        # Getting list of videos adopted
-        adopts = PersonAdoptPractice.objects.filter(person = person).values_list('video', flat = True)
-        videos_adopted = ''
-        for vid in adopts:
-            videos_adopted = videos_adopted + unicode(vid) + ' '
+    i = 0
+    # Getting list of videos seen
+    vids = PersonMeetingAttendance.objects.filter(person = person).values_list('screening__videoes_screened', flat = True)
+    videos_seen = ''
+    for vid in vids:
+        videos_seen = videos_seen + unicode(vid) + ' '
+    # Getting list of videos adopted
+    adopts = PersonAdoptPractice.objects.filter(person = person).values_list('video', flat = True)
+    videos_adopted = ''
+    for vid in adopts:
+        videos_adopted = videos_adopted + unicode(vid) + ' '
         # Putting all the info in xml tags
-        f.write('<people>\n')
-        f.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
-        f.write('<n'+unicode(i)+':create>\n')
-        f.write('<n'+unicode(i)+':case_type>person</n'+unicode(i)+':case_type>\n')
-        f.write('<n'+unicode(i)+':owner_id>'  + owner_id + '</n'+unicode(i)+':owner_id>\n')
-        f.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
-        f.write('</n'+unicode(i)+':create>\n')
-        f.write('<n'+unicode(i)+':update>\n')
-        f.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
-        f.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
-        f.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
-        f.write('<n'+unicode(i)+':videos_adopted>' + '' + '</n'+unicode(i)+':videos_adopted>\n')
-        f.write('</n'+unicode(i)+':update>\n')
-        f.write('</n'+unicode(i)+':case>\n')
-        f.write('</people>\n')
-        # Writing closing meta info of the form
-        i += 1
-        
+    f.write('<people>\n')
+    f.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
+    f.write('<n'+unicode(i)+':create>\n')
+    f.write('<n'+unicode(i)+':case_type>person</n'+unicode(i)+':case_type>\n')
+    f.write('<n'+unicode(i)+':owner_id>'  + owner_id + '</n'+unicode(i)+':owner_id>\n')
+    f.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
+    f.write('</n'+unicode(i)+':create>\n')
+    f.write('<n'+unicode(i)+':update>\n')
+    f.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
+    f.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
+    f.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
+    f.write('<n'+unicode(i)+':videos_adopted>' + '' + '</n'+unicode(i)+':videos_adopted>\n')
+    f.write('</n'+unicode(i)+':update>\n')
+    f.write('</n'+unicode(i)+':case>\n')
+    f.write('</people>\n')
+    i+= 1
+    # Writing closing meta info of the form        
     f.write('<n'+unicode(i) + ':meta xmlns:n' + unicode(i) + '="http://openrosa.org/jr/xforms">\n')
     f.write('<n'+unicode(i) + ':userID>2523fc995ccfd1d27c15111ec8987be6</n' + unicode(i) + ':userID>\n')
     f.write('<n'+unicode(i) + ':instanceID>2729386f-7fd2-42cc-807f-786bf2dc952b</n' + unicode(i) + ':instanceID>\n')
@@ -74,8 +77,7 @@ def write_person_detail(person_id, filename, i, case_id):
     f.close()
     return f
 
-def write_full_case_list(person_list, filename, user_id):
-    import codecs
+def write_full_case_list(person_list, filename, user_id): #for generating cases
     f = codecs.open(filename, "w",'utf-8')
     Person = get_model('dashboard','Person')
     PersonMeetingAttendance = get_model('dashboard','PersonMeetingAttendance')
@@ -84,10 +86,14 @@ def write_full_case_list(person_list, filename, user_id):
     f.write('<data uiVersion="1" version="8" name="New Form" xmlns:jrm="http://dev.commcarehq.org/jr/xforms" xmlns="http://openrosa.org/formdesigner/DB63E17D-B572-4F5B-926E-061583DAE9DA">\n')
     f.write('<num_people>' + unicode(len(person_list)) + '</num_people>\n')
     i = 0
+    case_user_dict = {}
+    case_person_dict = {}
     for person_id in person_list:
         owner_id = user_id
         person = Person.objects.get(id = person_id)
         case_id = uuid.uuid4()
+        case_person_dict[person.id] = case_id
+        case_user_dict[person.id] = user_id
         # Getting list of videos seen
         vids = PersonMeetingAttendance.objects.filter(person = person).values_list('screening__videoes_screened', flat = True)
         videos_seen = ''
@@ -123,12 +129,14 @@ def write_full_case_list(person_list, filename, user_id):
     f.write('</n' + unicode(i) + ':meta>\n')
     f.write('</data>')
     f.close()
+    pickle.dump(case_user_dict, open('case_user','w')) #mapping of case with user
+    pickle.dump(case_person_dict, open('case_person','w')) #mapping of case with person(farmer)
     return f
 
-def make_upload_file(villages, filename):
+def make_upload_file(villages, filename, user_id):
     Person = get_model('dashboard','Person')
     person_ids = Person.objects.filter(village__in = villages).values_list('id',flat=True)
-    f = write_person_detail(person_ids, filename)
+    f = write_full_case_list(person_ids, filename, user_id)
 #    response = upload_file(filename)
 #    print response
     
