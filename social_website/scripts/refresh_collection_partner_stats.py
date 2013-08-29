7995cf7976c9aae977a021681c2568f0ae1300e9
yash <yash@digitalgreen.org>
2013-08-29 11:47:28 +0530

    optimizing stats updation of videos

--- social_website/scripts/refresh_collection_partner_stats.py
+++ social_website/scripts/refresh_collection_partner_stats.py
@@ -1,15 +1,23 @@
 from django.core.management import setup_environ
+from django.db.models import Sum
 import dg.settings
 setup_environ(dg.settings)
 from social_website.migration_functions import get_offline_stats, populate_collection_stats, populate_partner_stats
-from social_website.models import Collection, Partner, Video
+from social_website.models import Collection, Partner, PersonVideoRecord, Video
 
-for video in Video.objects.all():
-    stats = get_offline_stats(video.coco_id)
-    video.offlineLikes = stats['like__sum']
-    video.offlineViews = stats['views__sum']
-    video.adoptions = stats['adopted__sum']
-    video.save()
+vid_id_list = Video.objects.all().values_list('coco_id', flat=True)
+stats = PersonVideoRecord.objects.all().values('videoID').annotate(views = Sum('views'), likes = Sum('like'), adoptions = Sum('adopted'))
+for row in stats:
+    try:
+        video = Video.objects.get(uid = row['videoID'])
+        video.offlineLikes = row['like']
+        video.offlineViwes = row['views']
+        video.adoptions = row['adoptions']
+        video.save()
+    except Video.DoesNotExist:
+        # Video not yet in website DB
+        pass
+
 
 for collection in Collection.objects.all():
     populate_collection_stats(collection)
