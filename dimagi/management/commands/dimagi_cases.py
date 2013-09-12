3442e40182eee9bf1e4ce3e36075376e5c0d6301
koneti chandu <koneti@digitalgreen.org>
2013-09-12 10:50:05 +0530

    Solved issues encountered while creating xml file

--- dimagi/management/commands/dimagi_cases.py
+++ dimagi/management/commands/dimagi_cases.py
@@ -3,8 +3,8 @@ import os
 from django.core.management.base import BaseCommand, CommandError
 from dg.settings import MEDIA_ROOT
 
-from dimagi.models import CommCareProject, CommCareUser
-from diamgi.userfile_functions import make_upload_file, upload_file
+from dimagi.models import CommCareProject, CommCareUser, CommCareUserVillage
+from dimagi.scripts.userfile_functions import make_upload_file
 
 
 class Command(BaseCommand):
@@ -23,9 +23,9 @@ class Command(BaseCommand):
                 commcare_project = CommCareProject.objects.get(name=commcare_project_name)
             except CommCareProject.DoesNotExist:
                 raise CommandError('CommCare Project "%s" not yet entered in the Database.' % commcare_project_name)
-            commcare_users = CommCareUsers.objects.filter(project=commcare_project).all()
+            commcare_users = CommCareUser.objects.filter(project=commcare_project).all()
             for user in commcare_users:
-                villages = [village.id for village in user.villages]
+                villages = CommCareUserVillage.objects.filter(user=user).values_list('village__id', flat =True)
                 filename = os.path.join(MEDIA_ROOT, "dimagi", "%s.xml" % (user.username))
                 case_user_dict = {}
                 case_person_dict = {}
@@ -38,4 +38,6 @@ class Command(BaseCommand):
                 )
                 response = commcare_project.upload_case_file(filename)
                 if response == 201 or response == 200:
-                    self.stdout.write('Successfully closed poll "%s"' % poll_id) "" +entry['username']
+                    self.stdout.write('Successfully uploaded cases for "%s"' % entry['username'])
+                else:
+                    self.stdout.write('Not uploaded but file ("%s") has been created in MEDIA_ROOT/dimagi' % entry['username'])
