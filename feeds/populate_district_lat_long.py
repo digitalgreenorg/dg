427e8a1f73903403fe070b3234c91424e1ec7114
Aadish Gupta <aadish@digitalgreen.org>
2013-10-03 14:56:23 +0530

    BMGF Feed

---
+++ feeds/populate_district_lat_long.py
@@ -0,0 +1,20 @@
+from django.core.management import setup_environ
+import dg.settings
+setup_environ(dg.settings)
+
+from dashboard.models import District
+from libs.geocoder import Geocoder
+
+geocoder = Geocoder()
+for d in District.objects.all():
+    address = u"%s,%s,%s" % (d.district_name, d.state.state_name, d.state.country.country_name)
+    if (geocoder.convert(address)):
+        try:
+            (d.latitude, d.longitude) = geocoder.getLatLng()
+            d.save()
+        except:
+            print "Geocodes not found for %s, %s" % (d.district_name, d.state.state_name)
+
+
+
+
