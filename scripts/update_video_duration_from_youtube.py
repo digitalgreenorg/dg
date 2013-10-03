7d6655ae9a3cc1be6fd3ff5d1bfbd0e0e3261d82
Aadish Gupta <aadish@digitalgreen.org>
2013-10-03 09:48:32 +0530

    Adding Call function for you tube updates Adding Optional parameter complete for running script on all videos

--- scripts/update_video_duration_from_youtube.py
+++ scripts/update_video_duration_from_youtube.py
@@ -61,62 +61,63 @@ def crop(img, new_width, new_height):
     img_resized = img_crop.resize((new_width, new_height), Image.ANTIALIAS)
     return img_resized
 
-yt_service = gdata.youtube.service.YouTubeService()
 
-#Developer key of rahul@digitalgreen.org
-yt_service.developer_key = 'AI39si74a5fwzrBsgSxjgImSsImXHfGgt8IpozLxty9oGP7CH0ky4Hf1eetV10IBi2KlgcgkAX-vmtmG86fdAX2PaG2CQPtkpA'
-# The YouTube API does not currently support HTTPS/SSL access.
-yt_service.ssl = False
+def call_youtube_update(complete=False):
+    yt_service = gdata.youtube.service.YouTubeService()
 
-error_ids = {}
-filter_all = (date.today().day) % 7 == 0  #Check all videos every 7 days. Daily check for new videos only.
-if filter_all:
-    vids = Video.objects.exclude(youtubeid='')
-else:
-    vids = Video.objects.exclude(youtubeid='').filter(duration=None)
+    #Developer key of rahul@digitalgreen.org
+    yt_service.developer_key = 'AI39si74a5fwzrBsgSxjgImSsImXHfGgt8IpozLxty9oGP7CH0ky4Hf1eetV10IBi2KlgcgkAX-vmtmG86fdAX2PaG2CQPtkpA'
+    # The YouTube API does not currently support HTTPS/SSL access.
+    yt_service.ssl = False
 
-for vid in vids:
-    try:
-        #Fetch the video entry from Youtube
-        video_id = vid.youtubeid
-        video_id = video_id.strip()
-        if '/' in video_id:
-            video_id_ar = video_id.split('/')
-            video_id = video_id_ar[-1]
-            vid.youtubeid = video_id
-            vid.save()
-        entry = yt_service.GetYouTubeVideoEntry(video_id=video_id)
-    except gdata.service.RequestError, inst:
-        error_ids[vid.id] = inst
+    error_ids = {}
+    filter_all = (date.today().day) % 7 == 0  #Check all videos every 7 days. Daily check for new videos only.
+    if (filter_all or complete):
+        vids = Video.objects.exclude(youtubeid='')
     else:
-        print vid.id
-        key = "".join(['raw/', str(vid.id), '.jpg'])
-        if not bucket.get_key(key):
-            img = get_thumbnail(entry)
-            img_border = remove_border(img)
-            img_border.save(os.path.join(file_save_dir, ("raw.jpg")))
-            k = Key(bucket)
-            k.key = key
-            k.set_contents_from_filename(os.path.join(file_save_dir, ("raw.jpg")))
-            k.make_public()
-            img_cropped = crop(img_border, 217, 124)
-            img_cropped.save(os.path.join(file_save_dir, ("16.jpg")))
-            k = Key(bucket)
-            k.key = "".join(['16by9/', str(vid.id), '.jpg'])
-            k.set_contents_from_filename(os.path.join(file_save_dir, ("16.jpg")))
-            k.make_public()
-            print "uploaded to s3"
-        duration = timedelta(seconds = int(entry.media.duration.seconds))
-        if vid.duration != str(duration):
-            vid.duration = str(duration)
-            vid.save()
+        vids = Video.objects.exclude(youtubeid='').filter(duration=None)
 
+    for vid in vids:
+        try:
+            #Fetch the video entry from Youtube
+            video_id = vid.youtubeid
+            video_id = video_id.strip()
+            if '/' in video_id:
+                video_id_ar = video_id.split('/')
+                video_id = video_id_ar[-1]
+                vid.youtubeid = video_id
+                vid.save()
+            entry = yt_service.GetYouTubeVideoEntry(video_id=video_id)
+        except gdata.service.RequestError, inst:
+            error_ids[vid.id] = inst
+        else:
+            print vid.id
+            key = "".join(['raw/', str(vid.id), '.jpg'])
+            if not bucket.get_key(key):
+                img = get_thumbnail(entry)
+                img_border = remove_border(img)
+                img_border.save(os.path.join(file_save_dir, ("raw.jpg")))
+                k = Key(bucket)
+                k.key = key
+                k.set_contents_from_filename(os.path.join(file_save_dir, ("raw.jpg")))
+                k.make_public()
+                img_cropped = crop(img_border, 217, 124)
+                img_cropped.save(os.path.join(file_save_dir, ("16.jpg")))
+                k = Key(bucket)
+                k.key = "".join(['16by9/', str(vid.id), '.jpg'])
+                k.set_contents_from_filename(os.path.join(file_save_dir, ("16.jpg")))
+                k.make_public()
+                print "uploaded to s3"
+            duration = timedelta(seconds = int(entry.media.duration.seconds))
+            if vid.duration != str(duration):
+                vid.duration = str(duration)
+                vid.save()
 
-if(len(error_ids) > 0):
-    log = open(dg.settings.MEDIA_ROOT + "log/youtube_log.txt", "a")
-    log.write('\n' + str(datetime.utcnow()) + '\n')
-    text = ["Following Videos (ID & Error given) have problem with youtube id."]
-    for k, v in error_ids.iteritems():
-        text.append(str(k) + "\t" + str(v))
-    log.write('\n'.join(text))
-    log.close()
+    if(len(error_ids) > 0):
+        log = open(dg.settings.MEDIA_ROOT + "log/youtube_log.txt", "a")
+        log.write('\n' + str(datetime.utcnow()) + '\n')
+        text = ["Following Videos (ID & Error given) have problem with youtube id."]
+        for k, v in error_ids.iteritems():
+            text.append(str(k) + "\t" + str(v))
+        log.write('\n'.join(text))
+        log.close()
