0eb3828d04da997983568da360aa251463bedba2
Nandini Bhardwaj <nandini@digitalgreen.org>
2013-10-04 13:55:11 +0530

    Code review: Video thumbnail and duration updates, Footer update, renamed partner activities script.

--- scripts/update_video_duration_from_youtube.py
+++ scripts/update_video_duration_from_youtube.py
@@ -1,25 +1,15 @@
-from django.core.management import setup_environ
-import dg.settings
-setup_environ(dg.settings)
-
-import gdata.youtube.service
 import os
 import urllib
-
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 from datetime import timedelta, date, datetime
 from math import ceil, floor
 from PIL import Image
 
+import gdata.youtube.service
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+from dg.settings import ACCESS_KEY, SECRET_KEY, MEDIA_ROOT, YOUTUBE_SIMPLE_ACCESS
 from dashboard.models import Video
-
-BUCKET_NAME = 'video_thumbnail'
-con = S3Connection(dg.settings.ACCESS_KEY, dg.settings.SECRET_KEY)
-bucket = con.create_bucket(BUCKET_NAME)
-bucket.set_acl('public-read')
-
-file_save_dir = os.path.join(dg.settings.MEDIA_ROOT, 'youtube')
 
 
 def get_thumbnail(entry):
@@ -27,7 +17,6 @@ def get_thumbnail(entry):
         urllib.urlretrieve(thumbnail.url, os.path.join(file_save_dir, thumbnail.url.split("/")[-1]))
         img = Image.open(os.path.join(file_save_dir, thumbnail.url.split("/")[-1]))
         return img
-
 
 def remove_border(image):
     sz = image.size
@@ -38,7 +27,6 @@ def remove_border(image):
     box = img1.getbbox()
     im_border_removed = image.crop(box)
     return im_border_removed
-
 
 def crop(img, new_width, new_height):
     ratio = new_width * 1.0 / new_height
@@ -61,60 +49,81 @@ def crop(img, new_width, new_height):
     img_resized = img_crop.resize((new_width, new_height), Image.ANTIALIAS)
     return img_resized
 
+def cleanup_youtubeid(video_id):
+    # Strip spaces at the beginning and end
+    video_id = video_id.strip()
+    # There shouldn't be any slashes.
+    if '/' in video_id:
+        # When there is a slash, we have found that the anything before the slash must be removed.
+        video_id = video_id.split('/').pop()
+    return video_id
+
+def add_to_s3(key, filepath):
+    k = Key(bucket)
+    k.key = key
+    k.set_contents_from_filename(filepath)
+    k.make_public()
 
 def call_youtube_update(complete=False):
-    yt_service = gdata.youtube.service.YouTubeService()
+    
+    # Create S3 Connection
+    bucket_name = 'video_thumbnail'
+    bucket = S3Connection(ACCESS_KEY, SECRET_KEY).create_bucket(bucket_name)
+    bucket.set_acl('public-read')
+    location_raw_images = 'raw/'
+    location_16by9_images = '16by9/'
 
-    #Developer key of rahul@digitalgreen.org
-    yt_service.developer_key = 'AI39si74a5fwzrBsgSxjgImSsImXHfGgt8IpozLxty9oGP7CH0ky4Hf1eetV10IBi2KlgcgkAX-vmtmG86fdAX2PaG2CQPtkpA'
+    file_save_dir = os.path.join(MEDIA_ROOT, 'youtube')
+
+    yt_service = gdata.youtube.service.YouTubeService()
+    yt_service.developer_key = YOUTUBE_SIMPLE_ACCESS
     # The YouTube API does not currently support HTTPS/SSL access.
     yt_service.ssl = False
 
     error_ids = {}
-    filter_all = (date.today().day) % 7 == 0  #Check all videos every 7 days. Daily check for new videos only.
-    if (filter_all or complete):
-        vids = Video.objects.exclude(youtubeid='')
-    else:
-        vids = Video.objects.exclude(youtubeid='').filter(duration=None)
+    
+    #Check all videos every 7 days. Daily check for new videos only.
+    filter_all = (datetime.datetime.utcnow().weekday()) % 7 == 0  
+    
+    vids = Video.objects.exclude(youtubeid='')
+    if not (filter_all or complete):
+        # Duration is computed using the YouTube API. If this field is not populated, this video record has been newly linked through youtubeid.
+        vids = vids.filter(duration=None)
 
     for vid in vids:
         try:
-            #Fetch the video entry from Youtube
-            video_id = vid.youtubeid
-            video_id = video_id.strip()
-            if '/' in video_id:
-                video_id_ar = video_id.split('/')
-                video_id = video_id_ar[-1]
-                vid.youtubeid = video_id
+            cleaned_id = cleanup_youtubeid(vid.youtubeid)
+            if cleaned_id == vid.youtubeid:
+                vid.youtubeid = cleaned_id
                 vid.save()
-            entry = yt_service.GetYouTubeVideoEntry(video_id=video_id)
+            #Fetch the video entry from Youtube
+            entry = yt_service.GetYouTubeVideoEntry(video_id=cleaned_id)
         except gdata.service.RequestError, inst:
             error_ids[vid.id] = inst
         else:
-            print vid.id
-            key = "".join(['raw/', str(vid.id), '.jpg'])
+            # Update thumbnails on s3
+            key = "".join([location_raw_images, str(vid.id), '.jpg']])
             if not bucket.get_key(key):
                 img = get_thumbnail(entry)
+                
                 img_border = remove_border(img)
-                img_border.save(os.path.join(file_save_dir, ("raw.jpg")))
-                k = Key(bucket)
-                k.key = key
-                k.set_contents_from_filename(os.path.join(file_save_dir, ("raw.jpg")))
-                k.make_public()
+                filepath_borderless = os.path.join(file_save_dir, ("raw.jpg"))
+                img_border.save(filepath_borderless)
+                add_to_s3(key, filepath_borderless)
+                
                 img_cropped = crop(img_border, 217, 124)
-                img_cropped.save(os.path.join(file_save_dir, ("16.jpg")))
-                k = Key(bucket)
-                k.key = "".join(['16by9/', str(vid.id), '.jpg'])
-                k.set_contents_from_filename(os.path.join(file_save_dir, ("16.jpg")))
-                k.make_public()
-                print "uploaded to s3"
+                filepath_16by9 = os.path.join(file_save_dir, ("16.jpg"))
+                img_cropped.save(filepath_16by9)                
+                key = "".join([location_16by9_images, str(vid.id), '.jpg']])
+                add_to_s3(key, filepath_16by9)
+            # Update duration
             duration = timedelta(seconds = int(entry.media.duration.seconds))
             if vid.duration != str(duration):
                 vid.duration = str(duration)
                 vid.save()
 
     if(len(error_ids) > 0):
-        log = open(dg.settings.MEDIA_ROOT + "log/youtube_log.txt", "a")
+        log = open(MEDIA_ROOT + "log/youtube_log.txt", "a")
         log.write('\n' + str(datetime.utcnow()) + '\n')
         text = ["Following Videos (ID & Error given) have problem with youtube id."]
         for k, v in error_ids.iteritems():
--- social_website/management/commands/social_media.py
+++ social_website/management/commands/social_media.py
@@ -1,8 +1,8 @@
+import logging
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
-from social_website.scripts.write_footer_social import call_footer_stats
+from social_website.utils.website_footer_social import WebsiteFooter
 from social_website.scripts.activity_facebook_data import get_facebook_feed
-
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -10,22 +10,44 @@ class Command(BaseCommand):
                     action='store_true',
                     default=False,
                     help='Update news feed and footer statistics'),
-        make_option('-f', '--footer_statistics',
+        make_option('-f', '--footer',
                     action='store_true',
                     default=False,
                     help='Update footer statistics'),
-        make_option('-n', '--news_feed',
+        make_option('-n', '--newsfeed',
                     action='store_true',
                     default=False,
-                    help='Update news feed from facebook feed'),
+                    help='Update news feed from Facebook'),
         )
 
     def handle(self, *args, **options):
         if (options['all']):
-            call_footer_stats()
+            self.update_footer_stats()
             get_facebook_feed()
-        if (options['footer_statistics']):
-            call_footer_stats()
-        if (options['news_feed']):
+        if (options['footer']):
+            self.update_footer_stats()
+        if (options['newsfeed']):
             get_facebook_feed()
 
+    
+    def update_footer_stats(self):
+        logger = logging.getLogger('social_website')    
+        footer = WebsiteFooter()
+        try:
+            footer.fetch_facebook_likes()
+        except (httplib.HTTPException, urllib2.HTTPError, urllib2.URLError):
+            logger.error("error in updating facebook likes")
+        try:
+            footer.fetch_twitter_followers()
+        except exceptions:
+            logger.error("error in updating twitter followers")
+        try:
+            footer.fetch_youtube_videos()
+        except (httplib.HTTPException, urllib2.HTTPError, urllib2.URLError):
+            logger.error("error in updating youtube videos")
+        try:
+            footer.fetch_linkedin_subscribers()
+        except Exception:
+            logger.error("error in updating linkedin subscribers")
+        footer.write()
+        logger.info("Updated footer stats")
