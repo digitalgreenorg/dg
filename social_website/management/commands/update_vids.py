ccd1d8d83b553e369afe03b7f751b1f263563dcb
Aadish Gupta <aadish@digitalgreen.org>
2013-10-08 15:03:52 +0530

    converted update_vids to django command management

--- social_website/management/commands/update_vids.py
+++ social_website/management/commands/update_vids.py
@@ -1,12 +1,15 @@
-from django.core.management import setup_environ
-import dg.settings
-setup_environ(dg.settings)
+from django.core.management.base import BaseCommand, CommandError
 
 from dashboard.models import Video
 from social_website.models import Video as Website_video
 from social_website.migration_functions import update_website_video
 
-existing_vids = Website_video.objects.all().values_list('coco_id', flat=True)
-print len(Video.objects.exclude(id__in = existing_vids))
-for video in Video.objects.exclude(id__in = existing_vids):
-    update_website_video(video)
+
+class Command(BaseCommand):
+    help = '''Run this command when some video from dashboard is not in website tables'''
+
+    def handle(self, *args, **options):
+        existing_vids = Website_video.objects.all().values_list('coco_id', flat=True)
+        print len(Video.objects.exclude(id__in = existing_vids))
+        for video in Video.objects.exclude(id__in = existing_vids):
+            update_website_video(video)
