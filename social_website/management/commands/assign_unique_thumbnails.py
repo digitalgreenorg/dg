efc109997858f2a859436651065809a53eac355f
Aadish Gupta <aadish@digitalgreen.org>
2013-10-08 15:03:09 +0530

    converted assign_unique_thumbnails to django command

--- social_website/management/commands/assign_unique_thumbnails.py
+++ social_website/management/commands/assign_unique_thumbnails.py
@@ -1,18 +1,21 @@
-from django.core.management import setup_environ
-import dg.settings
-setup_environ(dg.settings)
+from django.core.management.base import BaseCommand, CommandError
+
 from social_website.models import Collection
 
-used_vid_list = []
-done = 0
-for collection in Collection.objects.all():
-    vids = collection.videos.all()
-    collection.thumbnailURL = vids[0].thumbnailURL16by9
-    for video in vids:
-        if video.uid not in used_vid_list:
-            collection.thumbnailURL = video.thumbnailURL16by9
-            used_vid_list.append(video.uid)
-            done += 1
-            break
-    collection.save()
-print '%s collection assigned unique thumbnails' % done
+
+class Command(BaseCommand):
+    help = '''This command will assign the unique thubnails to the collection'''
+
+    def handle(self, *args, **options):
+        used_vid_list = []
+        done = 0
+        for collection in Collection.objects.all():
+            vids = collection.videos.all()
+            collection.thumbnailURL = vids[0].thumbnailURL16by9
+            for video in vids:
+                if video.uid not in used_vid_list:
+                    collection.thumbnailURL = video.thumbnailURL16by9
+                    used_vid_list.append(video.uid)
+                    done += 1
+                    break
+            collection.save()
