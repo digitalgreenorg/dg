e5dbfc01ad27f36d74140075d4ec052db6333c93
Aadish Gupta <aadish@digitalgreen.org>
2013-10-17 15:48:05 +0530

    converting Capital K to small k for thousands

--- dg/media/social_website/scripts/libs/framework/Util.js
+++ dg/media/social_website/scripts/libs/framework/Util.js
@@ -165,7 +165,7 @@ define(function(require) {
 		   return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
 	   }
 	   if (num >= 1000) {
-		   return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+		   return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
 	   }
 	   return num;
    };
