7a415f045f7df15c7a86955f18ffe538f9ac3fee
koneti chandu <koneti@digitalgreen.org>
2013-11-01 18:26:02 +0530

    device report

--- dimagi/views.py
+++ dimagi/views.py
@@ -26,12 +26,13 @@ def save_submission(request):
 def save_in_db(submission):
     xml_string = submission.xml_data
     xml_parse = minidom.parseString(xml_string)
-    data = xml_parse.getElementsByTagName('data')
-    if data[0].attributes["name"].value.lower() == 'screening form' :
-        status, msg = save_mobile_data.save_screening_data(xml_parse)
-    elif data[0].attributes["name"].value.lower() == 'adoption' :
-        status, msg = save_mobile_data.save_adoption_data(xml_parse)
-    else :
+    if xml_parse.getElementsByTagName('data'):
+        data = xml_parse.getElementsByTagName('data')
+        if data[0].attributes["name"].value.lower() == 'screening form' :
+            status, msg = save_mobile_data.save_screening_data(xml_parse)
+        elif data[0].attributes["name"].value.lower() == 'adoption' :
+            status, msg = save_mobile_data.save_adoption_data(xml_parse)
+    else:
         status = -1
         msg = 'error in form'
     return status, msg
