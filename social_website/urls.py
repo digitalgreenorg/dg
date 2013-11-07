ceffe92604a0036dd260bbe57bf923fdd0286cf2
Aadish Gupta <aadish@digitalgreen.org>
2013-11-07 12:09:06 +0530

    video-view page url

--- social_website/urls.py
+++ social_website/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 
 from communications.views import media_view
 from human_resources.views import job_view, member_view
-from views import social_home, collection_view, logout_view, partner_view, search_view
+from views import social_home, collection_view, logout_view, partner_view, search_view, video_view
 
 urlpatterns = patterns('',
     url(r'^$', social_home, name="home"),    
@@ -36,4 +36,5 @@ urlpatterns = patterns('',
     url(r'^team/$', member_view, name='team'),
     url(r'^resources/$', direct_to_template, {'template': 'resources.html'}, name='resources'),
     url(r'^tools/$', direct_to_template, {'template': 'tools.html', 'extra_context': {'header': {'currentPage':'Tools'}}}, name='tools'),
+    url(r'^video-view/(?P<partner>.+)/(?P<state>.+)/(?P<language>.+)/(?P<title>.+)/$', video_view, name="video_page"),
 )
