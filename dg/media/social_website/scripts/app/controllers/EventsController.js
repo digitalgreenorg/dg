5a58f209980d5197cbe77157bbff26536d95409f
Gaurav Aggarwal <gaurav@digitalgreen.org>
2014-02-05 14:11:17 +0530

    Events Controller created to get the video on the main carousel to play.

---
+++ dg/media/social_website/scripts/app/controllers/EventsController.js
@@ -0,0 +1,128 @@
+/**
+ * AboutController Class File
+ *
+ * @author rdeluca
+ * @version $Id$
+ * @requires require.js
+ * @requires jQuery
+ */
+
+define(function(require) {
+    'use strict';
+
+    var DigitalGreenPageController = require('app/controllers/DigitalGreenPageController');
+    var jQuery = require('jquery');
+
+    var NCarousel = require('libs/NCarousel/NCarousel');
+    
+    require('libs/external/swfobject/swfobject');
+
+    var EventsController = DigitalGreenPageController.extend({
+
+        /**
+         * Controller constructor
+         * @return {EventsController} this
+         */
+        constructor: function(bootstrapConfig, globalHelpers) {
+            this.base(bootstrapConfig, globalHelpers);
+
+            
+
+
+            return this;
+        },
+
+        _initReferences: function($referenceBase, params) {
+            this.base(params);
+
+            var references = this._references;
+
+            // dom elements
+            references.$mainCarouselWrapper = jQuery('#main-carousel');
+
+            references.mainCarousel = new NCarousel(references.$mainCarouselWrapper, {
+                transition: 'slide',
+                autoPlay: true,
+                autoPlayDelay: 8000
+            });
+            
+            // play button 
+            references.$playButton = jQuery('.play-button');
+        },
+        
+        _initEvents: function() {
+            this.base();
+            
+            var references = this._references;
+            var boundFunctions = this._boundFunctions;
+            
+            boundFunctions.onPlayButtonClick = this._onVideoPlayButtonClick.bind(this);
+            references.$playButton.on('click', boundFunctions.onPlayButtonClick);
+        },
+        
+        _initVideoPlayer: function() {
+
+            var videoId = 'CR8zRC3wd-s';
+
+            var params = { allowScriptAccess: "always" };
+            var atts = { id: "player", 
+       			 		 class: 'main-carousel-video-player'
+		    			};
+            
+            swfobject.embedSWF(
+                'http://www.youtube.com/v/' + videoId + '?enablejsapi=1&playerapiid=ytplayer&version=3',
+                'player',
+                '1024',
+                '424',
+                '8',
+                null,
+                null,
+                params,
+                atts
+            );
+
+            window.onYouTubePlayerReady = this._onYouTubePlayerReady.bind(this);
+            $("#video-img > div").not("#player").each(function(index, ele){$(ele).hide();});
+            $("#player").show();
+        },
+
+        _onYouTubePlayerReady: function() {
+            // clean up the window
+            window.onYouTubePlayerReady = undefined;
+
+            var videoPlayer = jQuery('#player').get(0);
+            this._references.videoPlayer = videoPlayer;
+
+            // youtube seems to force us to put functions in the window
+            // we'll do our best to handle that elegantly
+            window.onYouTubePlayerStateChange = this._onYouTubePlayerStateChange.bind(this);
+
+            videoPlayer.addEventListener('onStateChange', 'onYouTubePlayerStateChange');
+            
+            videoPlayer.playVideo();
+            
+        },
+
+        _onYouTubePlayerStateChange: function(newState) {
+            if (newState == 0){
+            	$("#player").hide();          
+            	$("#video-img > div").not("#player").each(function(index, ele){$(ele).show();});
+            }
+        	
+        },
+        
+        _onVideoPlayButtonClick: function(e) {
+        	this._initVideoPlayer();
+        },
+
+        /**
+         * Controller destructor
+         * @return {void}
+         */
+        destroy: function() {
+            this.base();
+        }
+    });
+
+    return EventsController;
+});
