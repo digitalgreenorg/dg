f6c594b55f39df82b3dc8b37d5d5ad15f88deb6a
Aadish Gupta <aadish@digitalgreen.org>
2014-03-20 07:01:28 +0530

    programmes app and data migration

---
+++ programmes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from dashboard.base_models import CocoModel
+
+
+class Partner(CocoModel):
+    id = models.AutoField(primary_key=True)
+    old_coco_id = models.BigIntegerField(db_index=True)
+    partner_name = models.CharField(max_length=100)
+    date_of_association = models.DateField(null=True, blank=True)
+    phone_no = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __unicode__(self):
+        return self.partner_name
