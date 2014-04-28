90d6d438799548ef61353563a223a96cb6ceb6f1
Karan Gupta <karan10037@iiitd.ac.in>
2014-04-28 15:39:45 +0530

    Working model added.

--- deoanalytics/views.py
+++ deoanalytics/views.py
@@ -12,19 +12,12 @@ from dashboard.models import District
 from dashboard.models import ServerLog
 from dashboard.models import CocoUser
 from dashboard.models import PersonAdoptPractice
+from dashboard.models import Person
 
 
 def index(request):
-    noofscreenings = Screening.objects.all()[0]
-    '''output = ', '.join([p.question_text for p in latest_question_list])'''
     template = loader.get_template('deoanalytics/index.html')
-    
-    context = RequestContext(request, {
-        'noofscreenings': noofscreenings,
-    })
-    
-    return HttpResponse(template.render(context))    
-    '''return HttpResponse("Hello, world. You're at the DEO index.")'''
+    return HttpResponse()
 
 def mainpage(request):
     '''ASA partner_id = 10000000000008, BRLPS partner_id = 10000000000013'''
@@ -38,18 +31,11 @@ def mainpage(request):
             deodetails = CocoUser.objects.get(user__username=deo)
             state = deodetails.villages.values_list('block__district__state__state_name', flat=True).distinct()
             if state[0] == "Bihar":
-                deos_working_in_Bihar.append(deo) 
-
-    
-    screenings = Screening.objects.filter(user_created_id=126).count()   
-    adoptions = PersonAdoptPractice.objects.filter(user_created_id=126).count()                                                                                                            
-    
+                deos_working_in_Bihar.append(deo)   
     context = {
         'partners': partners,
         'districts': districts,
         'deos': deos_working_in_Bihar,
-        'screenings':screenings,
-        'adoptions':adoptions,
     }
     
     return render_to_response('deoanalytics/mainpage.html' , context, context_instance = RequestContext(request))
@@ -83,28 +69,17 @@ def deosetter(request):
 def deodatasetter(request):
     selecteddeo = request.GET.get('deo',None)
     sdate = request.GET.get ('sdate',None)
-    print sdate
     edate = request.GET.get ('edate',None)
-    print edate
-    mode = request.GET.get ('mode',None)
     
     start_date = datetime.datetime.strptime(sdate, "%Y-%m-%d")
-    print "start_date as in view: "
-    print start_date
     end_date = datetime.datetime.strptime(edate, "%Y-%m-%d")
-    
-    if mode == 1:       
-        screenings = Screening.objects.filter(user_created__username=selecteddeo, time_created=start_date).count()
-        adoptions = PersonAdoptPractice.objects.filter(user_created__username=selecteddeo, time_created=start_date).count()
-
-    else:
-        '''mydate = datetime.datetime.strptime(selecteddate, "%Y-%m-%d")
-        start_date = mydate - datetime.timedelta(days=7)'''       
-        screenings = Screening.objects.filter(user_created__username=selecteddeo, time_created__range=[start_date, end_date]).count()
-        adoptions = PersonAdoptPractice.objects.filter(user_created__username=selecteddeo, time_created__range=[start_date, end_date]).count()
-        '''video,person''' 
+         
+    screenings = Screening.objects.filter(user_created__username=selecteddeo, time_created__range=[start_date, end_date]).count()
+    adoptions = PersonAdoptPractice.objects.filter(user_created__username=selecteddeo, time_created__range=[start_date, end_date]).count()
+    persons = Person.objects.filter(user_created__username=selecteddeo, time_created__range=[start_date, end_date]).count()
     
     return HttpResponse(json.dumps({
         "screenings": screenings, 
-        "adoptions": adoptions}),
+        "adoptions": adoptions,
+        "persons": persons}),
     content_type="application/json")
