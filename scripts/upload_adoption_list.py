b92f3813d16ebed2812d75f0ed16adb092166625
Abhishek Chandran <abhishekchandran91@gmail.com>
2014-06-19 15:17:00 +0530

    Python script to upload adoption list (.csv file) containing person_id, date of adoption and video_id to PersonAdoptPractice table based on person_id in pma and video seen in Screeing.

--- scripts/upload_adoption_list.py
+++ scripts/upload_adoption_list.py
@@ -1,44 +1,40 @@
-import csv
+import csv, datetime
 
 from django.core.management import setup_environ
 
 import dg.settings
 setup_environ(dg.settings)
 
-from activities.models import  PersonAdoptPractice, PersonMeetingAttendance
+from activities.models import  PersonAdoptPractice
 from people.models import Person
 
 csvfile = open("Adoption.csv", "rb")
 csv_data = csv.reader(csvfile)
 
-csvfle= open('op.csv', 'wb') 
-a = csv.writer(csvfle)
+csv_file = open('op.csv', 'wb') 
+a = csv.writer(csv_file)
 a.writerow(["Did not attend the screening"])
 
 csv_data.next()
 videos = []
 for row in csv_data:
-
 	if row[0]:
 		for pma in Person.objects.get(id = row[0]).personmeetingattendance_set.all():
 			for vid in pma.screening.videoes_screened.all():
-				videos.append(vid.id)
+				videos.append(str(vid.id))
+	row[1] = datetime.datetime.strptime(row[1], "%d-%b-%y").strftime("20%y-%m-%d")
 	try:
-
 		if(row[2] in videos):
-			
-			c=PersonAdoptPractice(person_id=int(row[0]), video_id=int(row[2]), date_of_adoption=row[1])
+			c=PersonAdoptPractice(person_id=int(row[0]), video_id=int(row[2]), date_of_adoption=row[1], partner_id="13", user_created_id="127")
 			c.save()
 		else:
 			a.writerow(row)
-			
 	except Exception, err:
-		a.writerow(str(err))
+		a.writerow([str(err)])
 	
 csvfile.close()
-csvfle.close()
-
-    	
+csv_file.close()
+	
 
 	
 	
