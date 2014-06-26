88ef3b18a21f086548b83168bb0a69b95dd29856
sreenug <sreenivasula@digitalgreen.org>
2014-06-26 11:53:38 +0530

    --added one more try catch to avoid 500

--- dimagi/views.py
+++ dimagi/views.py
@@ -38,12 +38,13 @@ def save_in_db(submission):
     xml_parse = minidom.parseString(xml_string)
     if xml_parse.getElementsByTagName('data'):
         data = xml_parse.getElementsByTagName('data')
-        name = data[0].get('name', None)
-        if name:
+        try:
             if data[0].attributes["name"].value.lower() == 'screening form' :
                 status, msg = save_mobile_data.save_screening_data(xml_parse)
             elif data[0].attributes["name"].value.lower() == 'adoption' :
                 status, msg = save_mobile_data.save_adoption_data(xml_parse)
+        except Exception as ex:
+            error = ex
     elif devicereport:
         status = error_list['DEVICE_REPORT']
         msg = 'device_report'
