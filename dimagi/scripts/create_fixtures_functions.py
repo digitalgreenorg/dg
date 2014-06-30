c593ecae27f349748e93d9710c43e3b25465bc2c
sreenug <sreenivasula@digitalgreen.org>
2014-06-30 11:17:23 +0530

    --changing fixtures to display videos screened in the villages assigned

--- dimagi/scripts/create_fixtures_functions.py
+++ dimagi/scripts/create_fixtures_functions.py
@@ -304,8 +304,10 @@ def create_fixture(users, project_name, want_seasonal_behavior):
         video_schedule_list = []
         for id in video_list:
             video_schedule_list.append(id)
-        video_schedule_list_of_dict = [] 
-        for id in video_schedule_list[:5]: #when we create a project, we should atleast see some data. That can serve as example. And seasonal data must be added and deleted as and when they needed.
+        video_schedule_list_of_dict = []
+        ##Get videos screened in Chittor for this project
+        video_list_screened_in_villages = Screening.objects.filter(village__in = villages).values_list('videoes_screened', flat=True) 
+        for id in video_list_screened_in_villages[:10]: #when we create a project, we should atleast see some data. That can serve as example. And seasonal data must be added and deleted as and when they needed.
             video_schedule_list_of_dict.append({'id': id,
                                         'low_val': '2013-01-01', 
                                         'high_val': '2020-01-01' })
