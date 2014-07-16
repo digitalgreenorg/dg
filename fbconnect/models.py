31098fe11d58769b2c397648e1048ee047494175
Aadish Gupta <aadish@digitalgreen.org>
2014-07-16 14:13:55 +0530

    removing imports crashing in django 1.6

--- fbconnect/models.py
+++ fbconnect/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-from people.models import Person
-from dashboard.fields import BigAutoField, BigForeignKey, PositiveBigIntegerField
-# Create your models here.
+
 
 class FBUser(models.Model):
     fuid = models.CharField(max_length=64)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
-    
+
     class Meta:
         db_table = 'fb_user'
+
 
 class FBFollowers(models.Model):
     fbuser = models.CharField(max_length=64, null=True)
