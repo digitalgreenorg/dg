abf8e6e9d13ab843371628e48ae714b72819e628
sreenug <sreenivasula@digitalgreen.org>
2014-07-18 14:24:14 +0530

    --DB model clean up..

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,7 +5,7 @@ from django.forms.extras.widgets import *
 
 from activities.models import PersonAdoptPractice, PersonMeetingAttendance, Screening
 from coco.base_models import CocoModel
-from geographies.models import Village, Block, Region, District, State, Country
+from geographies.models import Village, Block, District, State, Country
 from people.models import Animator, AnimatorAssignedVillage, Person, PersonGroup
 from programs.models import Partner
 from videos.models import Language, Practice, Video
@@ -60,10 +60,6 @@ class CountryForm(CocoModelForm):
     class Meta:
         model = Country
         
-class RegionForm(CocoModelForm):
-    class Meta:
-        model = Region
-
 class StateForm(CocoModelForm):
     class Meta:
         model = State
