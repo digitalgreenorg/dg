0ee3c7929f836139b064d05d185eb3529e2acdf1
Aadish Gupta <aadish@digitalgreen.org>
2014-11-13 12:18:09 +0530

    selective cases update

--- dimagi/scripts/update_cases_functions.py
+++ dimagi/scripts/update_cases_functions.py
@@ -2,6 +2,10 @@ import uuid
 import codecs
 from django.db.models import get_model
 from django.core.exceptions import ValidationError
+import json
+import requests
+from requests.auth import HTTPDigestAuth
+
 from write_xml_content import write_close_person_content, write_closing_meta, write_opening_meta, write_person_content, write_person_update_content 
 
 
@@ -19,16 +23,18 @@ def close_case(cases, filename):
     file.close()
 
 
-def update_case(cases, filename):
+def update_case(cases, filename, project_name):
     file = codecs.open(filename, "w",'utf-8')
-    write_opening_meta(file, len(cases))
     PersonMeetingAttendance = get_model('activities','PersonMeetingAttendance')
     PersonAdoptPractice = get_model('activities','PersonAdoptPractice')
     Person = get_model('people','Person')
-    for i, case in enumerate(cases):
+    count = 0
+    content_array = []
+    for case in cases:
         case_id = case.guid
         owner_id = case.user.guid
 
+        url = "".join(["https://www.commcarehq.org/a/", project_name, "/api/v0.5/case/", case_id, "/?type=json"])
         person = Person.objects.get(id=case.person_id)
         vids = PersonMeetingAttendance.objects.filter(person=person).values_list('screening__videoes_screened', flat = True).distinct()
 
@@ -38,9 +44,25 @@ def update_case(cases, filename):
         adopts = PersonAdoptPractice.objects.filter(person=person).values_list('video', flat = True).distinct()
         videos_adopted = " ".join([unicode(a) for a in adopts])
 
+        #check for changes in the cases in dimagi
+
+        r = requests.get(url, auth=HTTPDigestAuth('nandinibhardwaj@gmail.com','digitalgreen'))
+        data = json.loads(r.content)
+        video_seen_in_case = sorted(data['properties']['videos_seen'].split())
+        video_seen_in_db = sorted(videos_seen.split())
+        print cmp(video_seen_in_case, video_seen_in_db)
+        video_adopt_in_case = sorted(data['properties']['videos_adopted'].split())
+        video_adopt_in_db = sorted(videos_adopted.split())
+        print cmp(video_adopt_in_case, video_adopt_in_db)
+
         # Write xml for a particular person
-        write_person_update_content(file, i, case_id, owner_id, person, videos_seen, videos_adopted)
-    write_closing_meta(file, owner_id, i + 1)
+        if (cmp(video_seen_in_case, video_seen_in_db) != 0 or cmp(video_adopt_in_case, video_adopt_in_db) != 0):
+            write_person_update_content(content_array, count, case_id, owner_id, person, videos_seen, videos_adopted)
+            count += 1
+    write_opening_meta(file, count)
+    for content in content_array:
+        file.write(content)
+    write_closing_meta(file, owner_id, count)
     file.close()
 
 
--- dimagi/scripts/write_xml_content.py
+++ dimagi/scripts/write_xml_content.py
@@ -47,19 +47,19 @@ def write_close_person_content(file, i, case_id, owner_id):
     file.write('</people>\n')
 
 
-def write_person_update_content(file, i, case_id, owner_id, person, videos_seen, videos_adopted):
-    file.write('<people>\n')
-    file.write('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
-    file.write('<n'+unicode(i)+':update>\n')
-    file.write('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
-    file.write('<n'+unicode(i)+':owner_id>' + unicode(owner_id) + '</n'+unicode(i)+':owner_id>\n')
-    file.write('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
+def write_person_update_content(content_array, i, case_id, owner_id, person, videos_seen, videos_adopted):
+    content_array.append('<people>\n')
+    content_array.append('<n'+unicode(i)+':case case_id="' + unicode(case_id)+ '" date_modified="'+ unicode(datetime.datetime.now().date()) + '" user_id="' + owner_id + '" xmlns:n'+unicode(i)+'="http://commcarehq.org/case/transaction/v2">\n')
+    content_array.append('<n'+unicode(i)+':update>\n')
+    content_array.append('<n'+unicode(i)+':case_name>' + unicode(person.person_name) + '</n'+unicode(i)+':case_name>\n')
+    content_array.append('<n'+unicode(i)+':owner_id>' + unicode(owner_id) + '</n'+unicode(i)+':owner_id>\n')
+    content_array.append('<n'+unicode(i)+':id>' + unicode(person.id) + '</n'+unicode(i)+':id>\n')
     if person.group:
-        file.write('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
+        content_array.append('<n'+unicode(i)+':group_id>' + unicode(person.group.id)+ '</n'+unicode(i)+':group_id>\n')
     else:
-        file.write('<n'+unicode(i)+':group_id>None</n'+unicode(i)+':group_id>\n')
-    file.write('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
-    file.write('<n'+unicode(i)+':videos_adopted>' + videos_adopted + '</n'+unicode(i)+':videos_adopted>\n')
-    file.write('</n'+unicode(i)+':update>\n')
-    file.write('</n'+unicode(i)+':case>\n')
-    file.write('</people>\n')
+        content_array.append('<n'+unicode(i)+':group_id>None</n'+unicode(i)+':group_id>\n')
+    content_array.append('<n'+unicode(i)+':videos_seen>' + videos_seen + '</n'+unicode(i)+':videos_seen>\n')
+    content_array.append('<n'+unicode(i)+':videos_adopted>' + videos_adopted + '</n'+unicode(i)+':videos_adopted>\n')
+    content_array.append('</n'+unicode(i)+':update>\n')
+    content_array.append('</n'+unicode(i)+':case>\n')
+    content_array.append('</people>\n')
