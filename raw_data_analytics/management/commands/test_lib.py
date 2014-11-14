8393e1884a6efb87228eda22d259838548f61c1b
Lokesh Garg <lokeshgarg92@gmail.com>
2014-11-14 13:34:23 +0530

    working on command line

--- raw_data_analytics/management/commands/test_lib.py
+++ raw_data_analytics/management/commands/test_lib.py
@@ -18,6 +18,11 @@ class Command(BaseCommand):
                     default=False,
                     dest='partner',
                     help='Takes partner name as input'),
+    make_option('-s', '--state',
+                    action='store',
+                    default=False,
+                    dest='state',
+                    help='Takes state name as input'),
     make_option('-d', '--district',
                     action='store',
                     default=False,
@@ -37,35 +42,77 @@ class Command(BaseCommand):
         whereClause = " 1=1 "
         groupbyClause = ' group by S.state_name'
         gflag = 0
+        sflag = 0
+        selectClause = ''
+
         if(options['partner']!=False):
-            whereClause += "and P.partner_name=\'"+options['partner']+"\'"
-            groupbyClause = ' group by P.partner_name'
-            gflag = 1
+            if gflag == 1:
+                groupbyClause += ',P.partner_name'
+            else:
+                gflag = 1
+                groupbyClause = ' group by P.partner_name'
+            if sflag == 1:
+                selectClause += ',P.partner_name '
+            else:
+                sflag = 1
+                selectClause = ' P.partner_name'
+
+            if (options['partner']!='null'):
+                whereClause += "and P.partner_name=\'"+options['partner']+"\'"
+
+        if(options['state']!=False):
+            if gflag == 1:
+                groupbyClause += ',S.state_name'
+            else:
+                gflag = 1
+                groupbyClause = ' group by S.state_name'
+            if sflag == 1:
+                selectClause += ',S.state_name '
+            else:
+                sflag = 1
+                selectClause = ' S.state_name'
+
+            if (options['state']!='null'):
+                whereClause += "and S.state_name=\'"+options['state']+"\'"
+
         if(options['district']!=False):
-            whereClause += "and D.district_name=\'"+options['district']+"\'"
             if gflag==1:
+                groupbyClause += ',D.district_name'
+            else:
+                gflag = 1
+                groupbyClause = ' group by D.district_name'
+            if sflag == 1:
+                selectClause += ',D.district_name '
+            else:
+                sflag = 1
+                selectClause = ' D.district_name'
+
+            if (options['district']!='null'):
+                whereClause += "and D.district_name=\'"+options['district']+"\'"
+
+        if(options['block']!=False):
+            if gflag == 1:
                 groupbyClause += ',B.block_name'
             else:
+                gflag = 1
                 groupbyClause = ' group by B.block_name'
-        if(options['block']!=False):
-            whereClause += "and B.block_name=\'"+options['block']+"\'"
-            groupbyClause = ' group by V.village_name'
-            if gflag == 1:
-                groupbyClause += ',V.village_name'
+            if sflag == 1:
+                selectClause += ',B.block_name '
             else:
-                groupbyClause = ' group by V.village_name'
+                sflag = 1
+                selectClause = ' B.block_name'
 
-        self.home(whereClause,groupbyClause)
+            if (options['block']!='null'):
+                whereClause += "and B.block_name=\'"+options['block']+"\'"
 
-    def home(self,whereClause, groupbyClause):
+        self.home(selectClause, whereClause, groupbyClause)
+
+    def home(self, selectClause, whereClause, groupbyClause):
         mysql_cn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='root', db='digitalgreen')
-        df_mysql = psql.read_sql(
-            ('select S.state_name, D.district_name, B.block_name, V.village_name, count(SC.id) as nScreenings from activities_screening SC join programs_partner P on P.id=SC.partner_id join geographies_village V on SC.village_id=V.id join geographies_block B on V.block_id=B.id join geographies_district D on B.district_id=D.id join geographies_state S on D.state_id=S.id where'+whereClause+groupbyClause+';'),
-            con=mysql_cn)
+        query = 'select'+ selectClause + ',count(SC.id) as nScreenings from activities_screening SC join programs_partner P on P.id=SC.partner_id join geographies_village V on SC.village_id=V.id join geographies_block B on V.block_id=B.id join geographies_district D on B.district_id=D.id join geographies_state S on D.state_id=S.id where'+whereClause+groupbyClause+';'
+        df_mysql = psql.read_sql(query,con=mysql_cn)
         print 'loaded dataframe from MySQL. records:', len(df_mysql)
+        print query
         print "################################"
         print df_mysql
-
-# 'select V.village_name, B.block_name, D.district_name, S.state_name, count(SC.id) from activities_screening SC join programs_partner P on P.id=SC.partner_id join geographies_village V on SC.village_id=V.id join geographies_block B on V.block_id=B.id join geographies_
-# district D on B.district_id=D.id join geographies_state S on D.state_id=S.id where %s %s'
         mysql_cn.close()
