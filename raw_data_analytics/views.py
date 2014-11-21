7ace1d57bb37ac399ca41f11823653498b41e806
Abhishek Chandran <abhishekchandran91@gmail.com>
2014-11-21 17:27:04 +0530

    if conditions sorted, changed to false for no entries made

--- raw_data_analytics/views.py
+++ raw_data_analytics/views.py
@@ -41,31 +41,43 @@ def execute(request):
         partner = True 
     elif (len(partner)>0 and partner_chk[0]==None) or (len(partner)>0 and partner_chk[0]!=None):
         partner = partner[0]
+    elif(partner[0]=='' and partner_chk[0]==None):
+        partner = False
     
     if(country[0]=='' and country_chk[0]!=None):
         country = True
     elif (len(country)>0 and country_chk[0]==None) or (len(country)>0 and country_chk[0]!=None):
         country = country[0]
+    elif(country[0]=='' and country_chk[0]==None):
+        country = False
         
     if(state[0]=='' and state_chk[0]!=None):
         state = True
     elif (len(state)>0 and state_chk[0]==None) or (len(state)>0 and state_chk[0]!=None):
         state = state[0]
+    elif(state[0]=='' and state_chk[0]==None):
+        state = False
         
     if(district[0]=='' and district_chk[0]!=None):
         district = True
     elif (len(district)>0 and district_chk[0]==None) or (len(district)>0 and district_chk[0]!=None):
         district = district[0]
+    elif(district[0]=='' and district_chk[0]==None):
+        district = False
 
     if(block[0]=='' and block_chk[0]!=None):
         block = True
     elif (len(block)>0 and block_chk[0]==None) or (len(block)>0 and block_chk[0]!=None):
         block = block[0]
+    elif(block[0]=='' and block_chk[0]==None):
+        block = False
         
     if(village[0]=='' and village_chk[0]!=None):
         village = True
     elif (len(village)>0 and village_chk[0]==None) or (len(village)>0 and village_chk[0]!=None):
         village = village[0]
+    elif(village[0]=='' and village_chk[0]==None):
+        village = False
 
     if(screening_chk[0]!=None):
         screening = True
