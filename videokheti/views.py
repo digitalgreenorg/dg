0009420f58b048adb7808c6a3a540c9724d018f1
Aadish Gupta <aadish@digitalgreen.org>
2015-01-16 11:17:26 +0530

    level 4 done

--- videokheti/views.py
+++ videokheti/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from videokheti.models import ActionType, Crop, TimeYear
+from videokheti.models import ActionType, Crop, Method, TimeYear
 
 
 def home(request):
@@ -56,3 +56,39 @@ def level(request):
                   'crop': list_dict,
                   }
         return render_to_response('videokheti.html', context, context_instance=RequestContext(request))
+    if level == '3':
+        crop_id = request.GET.get('crop', None)
+        time_id = request.GET.get('time', None)
+        action_id = request.GET.get('action', None)
+        
+        action_object = ActionType.objects.get(id=action_id)
+        if action_object.name in ('seed_treatment', 'nutrient_management', 'disease_and_pest_control'):
+            method_objects = Method.objects.all()[:3]
+            list_dict = []
+            for obj in method_objects:
+                dic_obj = {'name': obj.name.replace('_', ' '),
+                           'image': obj.image_file,
+                           'audio': obj.sound_file,
+                           'id': obj.id,
+                           'link': ''.join(['?crop=', crop_id, '&time=', str(time_id), '&action=', action_id, '&method=', str(obj.id), '&level=4'])
+                           }
+                list_dict.append(dic_obj)
+            context = {
+                        'crop': list_dict,
+                      }
+            return render_to_response('videokheti.html', context, context_instance=RequestContext(request))
+        elif action_object.name == 'interculture':
+            method_objects = Method.objects.all()[3:]
+            list_dict = []
+            for obj in method_objects:
+                dic_obj = {'name': obj.name.replace('_', ' '),
+                           'image': obj.image_file,
+                           'audio': obj.sound_file,
+                           'id': obj.id,
+                           'link': ''.join(['?crop=', crop_id, '&time=', str(time_id), '&action=', action_id, '&method=', str(obj.id), '&level=4'])
+                           }
+                list_dict.append(dic_obj)
+            context = {
+                        'crop': list_dict,
+                      }
+            return render_to_response('videokheti.html', context, context_instance=RequestContext(request))
