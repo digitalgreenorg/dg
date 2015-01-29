f1c33a23365c6156f24425f7519aa4317c9f47a9
Abhishek Chandran <abhishekchandran91@gmail.com>
2015-01-29 16:03:35 +0530

    ID of filters passed as string to library

--- raw_data_analytics/views.py
+++ raw_data_analytics/views.py
@@ -4,7 +4,11 @@ from django.core.management.base import BaseCommand, CommandError
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from geographies.models import District, Block
+
+from geographies.models import Country, State, District, Block, Village
+from programs.models import Partner
+from people.models import Animator
+
 import pandas as pd
 import MySQLdb
 import pandas.io.sql as psql
@@ -26,7 +30,8 @@ def execute(request):
     state = [request.POST.get("state")]
     district = [request.POST.get("district")]
     block = [request.POST.get("block")]
-    village = [request.POST.get("village")]  
+    village = [request.POST.get("village")]
+    #animator =[request.POST.get("animator")]  
     
     partner_chk = [request.POST.get("partner_chk")]
     country_chk = [request.POST.get("country_chk")]
@@ -34,7 +39,8 @@ def execute(request):
     district_chk = [request.POST.get("district_chk")]
     block_chk = [request.POST.get("block_chk")]
     village_chk = [request.POST.get("village_chk")]
-
+    #animator_chk = [request.POST.get("animator_chk")]
+    
     screening_chk = [request.POST.get("screening_chk")]
     adoption_chk = [request.POST.get("adoption_chk")]
 
@@ -44,44 +50,54 @@ def execute(request):
     if(partner[0]=='' and partner_chk[0]!=None):
         partner = True 
     elif (partner[0]!='' and partner_chk[0]==None) or (partner[0]!='' and partner_chk[0]!=None):
-        partner = partner[0]
+        partner = str(Partner.objects.get(partner_name=partner[0]).id)
     elif(partner[0]=='' and partner_chk[0]==None):
         partner = False
     
     if(country[0]=='' and country_chk[0]!=None):
         country = True
     elif (country[0]!='' and country_chk[0]==None) or (country[0]!='' and country_chk[0]!=None):
-        country = country[0]
+        country = str(Country.objects.get(country_name=country[0]).id)
     elif(country[0]=='' and country_chk[0]==None):
         country = False
         
     if(state[0]=='' and state_chk[0]!=None):
         state = True
     elif (state[0]!='' and state_chk[0]==None) or (state[0]!='' and state_chk[0]!=None):
-        state = state[0]
+        state = str(State.objects.get(state_name=state[0]).id)
     elif(state[0]=='' and state_chk[0]==None):
         state = False
         
     if(district[0]=='' and district_chk[0]!=None):
         district = True
     elif (district[0]!='' and district_chk[0]==None) or (district[0]!='' and district_chk[0]!=None):
-        district = district[0]
+        district = str(District.objects.get(district_name=district[0]).id)
     elif(district[0]=='' and district_chk[0]==None):
         district = False
 
     if(block[0]=='' and block_chk[0]!=None):
         block = True
     elif (block[0]!='' and block_chk[0]==None) or (block[0]!='' and block_chk[0]!=None):
-        block = block[0]
+        block = str(Block.objects.get(block_name=block[0]).id)
     elif(block[0]=='' and block_chk[0]==None):
         block = False
         
     if(village[0]=='' and village_chk[0]!=None):
         village = True
     elif (village[0]!='' and village_chk[0]==None) or (village[0]!='' and village_chk[0]!=None):
-        village = village[0]
+        village = str(Village.objects.get(village_name=village[0]).id)
     elif(village[0]=='' and village_chk[0]==None):
         village = False
+
+    
+    '''if(animator[0]=='' and animator_chk[0]!=None):
+        animator = True 
+    elif (animator[0]!='' and animator_chk[0]==None) or (animator[0]!='' and animator_chk[0]!=None):
+        animator = str(Animator.objects.get(name=animator[0]).id)
+    elif(animator[0]=='' and animator_chk[0]==None):
+        animator = False'''
+
+
 
     if(screening_chk[0]!=None):
         screening = True
@@ -104,7 +120,7 @@ def execute(request):
         now = datetime.datetime.now()
         to_date = '%s-%s-%s' %(now.year, now.month, now.day)
        
-    partition={'partner':partner, 'country':country, 'state':state, 'district':district, 'block':block, 'village':village}
+    partition={'partner':partner, 'country':country, 'state':state, 'district':district, 'block':block, 'village':village} #,'animator':animator}
     value = {'numScreening':screening, 'numAdoption':adoption}
     print "in views-------------------"
     print partition
