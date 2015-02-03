c0d5edbbc967579b84ebea65c837e658aefd8ac6
Abhishek Chandran <abhishekchandran91@gmail.com>
2015-02-03 11:38:22 +0530

    deopdown menu for filters implemented

--- raw_data_analytics/urls.py
+++ raw_data_analytics/urls.py
@@ -5,6 +5,9 @@ from raw_data_analytics import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='raw_data_analytics'),
     url(r'^execute/$', views.execute, name='execute'),
-    #url(r'^download/$', views.download, name='download'),
-
+    url(r'^dropdown1/$', views.dropdown1, name='dropdown1'),
+    url(r'^dropdown2/$', views.dropdown2, name='dropdown2'),
+    url(r'^dropdown3/$', views.dropdown3, name='dropdown3'),
+    url(r'^dropdown4/$', views.dropdown4, name='dropdown4'),
+    
        )
--- raw_data_analytics/views.py
+++ raw_data_analytics/views.py
@@ -31,19 +31,86 @@ def home(request):
    #for child in root:
       #  print (child.tag, root[1].tag)'''
 
+    countries = Country.objects.all()
+   
+    partners = Partner.objects.all()
 
-    return render_to_response('raw_data_analytics/output.html', context_instance=RequestContext(request))
+    return render_to_response('raw_data_analytics/output.html', {'countries' : countries, 'partners':partners}, context_instance=RequestContext(request))
 
     
+def dropdown1(request):
+    
+    #country_id = Country.objects.get(country_name=[request.POST.get("partner")][0]).id
+    #state = State.objects.filter(country=country_id)
+
+    country_selected = request.GET.get('selected', None)
+    
+
+    states = State.objects.filter(country__country_name = country_selected).values_list('state_name', flat=True)
+    
+    
+
+    resp = json.dumps([str(i) for i in states])
+    return HttpResponse(resp)
+
+def dropdown2(request):
+    
+    #country_id = Country.objects.get(country_name=[request.POST.get("partner")][0]).id
+    #state = State.objects.filter(country=country_id)
+
+    state_selected = request.GET.get('selected', None)
+    
+
+    districts = District.objects.filter(state__state_name = state_selected).values_list('district_name', flat=True)
+    
+    
+
+    resp = json.dumps([str(i) for i in districts])
+    return HttpResponse(resp)
+
+def dropdown3(request):
+    
+    #country_id = Country.objects.get(country_name=[request.POST.get("partner")][0]).id
+    #state = State.objects.filter(country=country_id)
+
+    district_selected = request.GET.get('selected', None)
+    
+
+    blocks = Block.objects.filter(district__district_name = district_selected).values_list('block_name', flat=True)
+    
+    
+
+    resp = json.dumps([str(i) for i in blocks])
+    return HttpResponse(resp)
+def dropdown4(request):
+    
+    #country_id = Country.objects.get(country_name=[request.POST.get("partner")][0]).id
+    #state = State.objects.filter(country=country_id)
+
+    block_selected = request.GET.get('selected', None)
+    
+
+    villages = Village.objects.filter(block__block_name = block_selected).values_list('village_name', flat=True)
+    
+    
+
+    resp = json.dumps([str(i) for i in villages])
+    return HttpResponse(resp)
+
+
 def execute(request):
 
     partner = [request.POST.get("partner")]
+    print partner[0]
     country = [request.POST.get("country")]
+    print country[0]
     state = [request.POST.get("state")]
+    
     district = [request.POST.get("district")]
+    
     block = [request.POST.get("block")]
     village = [request.POST.get("village")]
-    animator =[request.POST.get("animator")]
+    #animator =[request.POST.get("animator")]
     
     partner_chk = [request.POST.get("partner_chk")]
     country_chk = [request.POST.get("country_chk")]
@@ -59,55 +126,61 @@ def execute(request):
     from_date = [request.POST.get("from_date")]
     to_date = [request.POST.get("to_date")]
 
-    if(partner[0]=='' and partner_chk[0]!=None):
+    if(partner[0]== '-1' and partner_chk[0]!=None):
         partner = True 
-    elif (partner[0]!='' and partner_chk[0]==None) or (partner[0]!='' and partner_chk[0]!=None):
+    elif (partner[0]!= '-1' and partner_chk[0]==None) or (partner[0]!= '-1' and partner_chk[0]!=None):
         partner = str(Partner.objects.get(partner_name=partner[0]).id)
-    elif(partner[0]=='' and partner_chk[0]==None):
+    elif(partner[0]== '-1' and partner_chk[0]==None):
         partner = False
     
-    if(country[0]=='' and country_chk[0]!=None):
+    if(country[0]=='-1' and country_chk[0]!=None):
         country = True
-    elif (country[0]!='' and country_chk[0]==None) or (country[0]!='' and country_chk[0]!=None):
+    elif (country[0]!='-1' and country_chk[0]==None) or (country[0]!='-1' and country_chk[0]!=None):
         country = str(Country.objects.get(country_name=country[0]).id)
-    elif(country[0]=='' and country_chk[0]==None):
+    elif(country[0]== '-1' and country_chk[0]==None):
         country = False
         
-    if(state[0]=='' and state_chk[0]!=None):
+    if(state[0]=='-1' and state_chk[0]!=None):
         state = True
-    elif (state[0]!='' and state_chk[0]==None) or (state[0]!='' and state_chk[0]!=None):
+    elif (state[0]!= '-1' and state_chk[0]==None) or (state[0]!= '-1' and state_chk[0]!=None):
+        print state[0]
         state = str(State.objects.get(state_name=state[0]).id)
-    elif(state[0]=='' and state_chk[0]==None):
+    elif(state[0]=='-1' and state_chk[0]==None):
         state = False
         
-    if(district[0]=='' and district_chk[0]!=None):
+    if(district[0]=='-1' and district_chk[0]!=None):
         district = True
-    elif (district[0]!='' and district_chk[0]==None) or (district[0]!='' and district_chk[0]!=None):
+    elif (district[0]!='-1' and district_chk[0]==None) or (district[0]!='-1' and district_chk[0]!=None):
         district = str(District.objects.get(district_name=district[0]).id)
-    elif(district[0]=='' and district_chk[0]==None):
+    elif(district[0]=='-1' and district_chk[0]==None):
         district = False
 
-    if(block[0]=='' and block_chk[0]!=None):
+    if(block[0]=='-1' and block_chk[0]!=None):
         block = True
-    elif (block[0]!='' and block_chk[0]==None) or (block[0]!='' and block_chk[0]!=None):
+    elif (block[0]!='-1' and block_chk[0]==None) or (block[0]!='-1' and block_chk[0]!=None):
         block = str(Block.objects.get(block_name=block[0]).id)
-    elif(block[0]=='' and block_chk[0]==None):
+    elif(block[0]=='-1' and block_chk[0]==None):
         block = False
         
-    if(village[0]=='' and village_chk[0]!=None):
+    if(village[0]=='-1' and village_chk[0]!=None):
         village = True
-    elif (village[0]!='' and village_chk[0]==None) or (village[0]!='' and village_chk[0]!=None):
+    elif (village[0]!='-1' and village_chk[0]==None) or (village[0]!='-1' and village_chk[0]!=None):
         village = str(Village.objects.get(village_name=village[0]).id)
-    elif(village[0]=='' and village_chk[0]==None):
+    elif(village[0]=='-1' and village_chk[0]==None):
         village = False
 
     
-    if(animator[0]=='' and animator_chk[0]!=None):
+    '''if(animator[0]=='' and animator_chk[0]!=None):
         animator = True 
     elif (animator[0]!='' and animator_chk[0]==None) or (animator[0]!='' and animator_chk[0]!=None):
         animator = str(Animator.objects.get(name=animator[0]).id)
     elif(animator[0]=='' and animator_chk[0]==None):
+        animator = False'''
+
+    if (animator_chk[0]==None):
         animator = False
+    elif (animator_chk[0] != None):
+        animator = True
 
 
 
@@ -132,7 +205,7 @@ def execute(request):
         now = datetime.datetime.now()
         to_date = '%s-%s-%s' %(now.year, now.month, now.day)
        
-    partition={'partner':partner, 'country':country, 'state':state, 'district':district, 'block':block, 'village':village}#,'animator':animator}
+    partition={'partner':partner, 'country':country, 'state':state, 'district':district, 'block':block, 'village':village,'animator':animator}
     value = {'numScreening':screening, 'numAdoption':adoption}
     print "in views-------------------"
     print partition
