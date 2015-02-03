d08216b8647158fcc14bcfed01796905d20a19b8
Lokesh Garg <lokeshgarg92@gmail.com>
2015-02-03 17:10:05 +0530

    password made dynamic

--- raw_data_analytics/management/commands/partition_library.py
+++ raw_data_analytics/management/commands/partition_library.py
@@ -10,7 +10,6 @@ import pandas.io.sql as psql
 import csv
 import dg.settings
 
-
 class Command(BaseCommand):
     Dict = {}
     lookup_matrix = {}
@@ -276,7 +275,7 @@ class Command(BaseCommand):
     # Function to accept query as a string to execute and make dataframe corresponding to that particular query and return that dataframe
     def runQuery(self, query):
         # Make connection with the database
-        mysql_cn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='root', db='digitalgreen')
+        mysql_cn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd=dg.settings.PASSWORD , db='digitalgreen')
         # Making dataframe
         temp_df = psql.read_sql(query, con=mysql_cn)
         mysql_cn.close()
