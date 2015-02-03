eeb5d8554f28d802926bc82080fe752f5870b53f
Lokesh Garg <lokeshgarg92@gmail.com>
2015-02-03 17:44:43 +0530

    database name changes to sandbox_test

--- raw_data_analytics/management/commands/partition_library.py
+++ raw_data_analytics/management/commands/partition_library.py
@@ -275,7 +275,7 @@ class Command(BaseCommand):
     # Function to accept query as a string to execute and make dataframe corresponding to that particular query and return that dataframe
     def runQuery(self, query):
         # Make connection with the database
-        mysql_cn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd=dg.settings.PASSWORD , db='digitalgreen')
+        mysql_cn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd=dg.settings.PASSWORD , db='sandbox_test')
         # Making dataframe
         temp_df = psql.read_sql(query, con=mysql_cn)
         mysql_cn.close()
