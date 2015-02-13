5c92d6e1194db5ce42ba2fbccc9a70eb03dceead
Lokesh Garg <lokeshgarg92@gmail.com>
2015-02-13 14:43:30 +0530

    dataframe merge change

--- raw_data_analytics/utils/data_library.py
+++ raw_data_analytics/utils/data_library.py
@@ -50,7 +50,11 @@ class data_lib():
             if final_df.empty:
                 final_df = df
             else:
-                final_df = pd.merge(final_df, df, how='outer')
+                if len(final_df)>len(df):
+                    how = 'left'
+                else:
+                    how='right'
+                final_df = pd.merge(final_df, df, how=how)
             print df
             # /home/ubuntu/code/dg_coco_test/dg/
         return final_df
