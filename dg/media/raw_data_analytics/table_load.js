ea506301f010908ace50471d6e8911bf7e2f1779
Abhishek Chandran <abhishekchandran91@gmail.com>
2015-04-24 17:24:58 +0530

    datables implemented with classes included

--- dg/media/raw_data_analytics/table_load.js
+++ dg/media/raw_data_analytics/table_load.js
@@ -4,29 +4,22 @@ function table_load()
 {               
 	
 	var json = $("#tablex").data("val-json");
-	console.log("hello", json);
-//	var parsed = JSON.parse(json);
+	
 	var formated_json = [];
 	var full_data = [];
+	
 	var keyset = Object.keys(json);
-	console.log("yoyo", keyset);
-	console.log("yoyo2", keyset);
+	var interkeys = Object.keys(json[keyset[0]]);
 
-	var interkeys = Object.keys(json[keyset[0]])
 	for (var i=0; i<interkeys.length; i++){
 		var obj=[];
-		console.log('i',i);
-		console.log('heo',Object.keys(json[keyset[0]]));
 		for (var j=0; j<keyset.length;j++){
-			console.log('j',keyset[j]);
-			console.log('json',json[keyset[j]]);
 			obj[j] = json[keyset[j]][interkeys[i]];
 		}
 		formated_json.push(obj);
 	}
 	full_data.push(formated_json);
-	console.log('yipee', full_data);
-
+	
 	var column_arr = [];
 	for(var i=0; i<keyset.length; i++)
 	{
@@ -36,17 +29,26 @@ function table_load()
 		column_arr.push(column_obj);
 
 	}
-	console.log("aocolumn", column_arr);
-
-	jQuery('#example').dataTable( {
+	
+	$('#example').dataTable( {
 		"sDom":'T<"clear">lfrtip',
 		"bAutoWidth":false,
         "aaData": formated_json,
         "aoColumns": column_arr,
         "oTableTools":{
 
-            "sSwfPath": "/media/social_website/scripts/libs/tabletools_media/swf/copy_csv_xls_pdf.swf"
-			}
+            "sSwfPath": "/media/social_website/scripts/libs/tabletools_media/swf/copy_csv_xls.swf",
+			"aButtons": [
+	                           {
+	                               "sExtends": "copy",
+	                               "sButtonText": "Copy to Clipboard"
+	                           },
+	                           {
+	                               "sExtends": "xls",
+	                               "sButtonText": "Download in Excel"
+	                           }
+	                       ]
+                    }
     } );
 
 }
