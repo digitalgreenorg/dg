0a7f1c83b6e13bb8eb8e2fa333a033aa6e599a54
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2015-05-08 14:00:48 +0530

    Data parsing and calling

---
+++ ivr_data_collection/management/commands/ivr_call_initiate.py
@@ -0,0 +1,43 @@
+from xml.dom import minidom
+from django.core.management.base import BaseCommand
+
+import csv, os, logging, json
+
+sid = "digitalgreen2"
+token = "421c11b1235067ca30ca87590c80c31eadc46af0"
+        # agent_no="9718935868",
+        #customerNo="9718935868",
+customerNo="9910338592"
+callerid="01130018178"
+url='http://my.exotel.in/exoml/start/27037'
+timelimit="500"  # This is optional
+timeout="500" # This is also optional
+calltype="trans" # Can be "trans" for transactional and "promo" for promotional content
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        csvfile = open(os.path.join(__location__, 'test.csv'), 'rU')
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+        	print row['Mobile_Number'], row['Video_ID'], row['Person_ID']
+
+    def call_exotel(mobile_number,video_id,person_id):
+        r = requests.post('https://twilix.exotel.in/v1/Accounts/{sid}/Calls/connect.json'.format(sid=sid),
+            auth=(sid, token),
+            data={
+                'From': int(mobile_number),
+                'CallerId': callerid,
+                'TimeLimit': timelimit,
+                'Url': url,
+                'TimeOut': timeout,
+                'CallType': calltype,
+                'CustomField': video_id
+            })
+        json_data = json.loads(r.text)
+        log_string = "".join(["Call begins : Call id : ", json_data['Call']['Sid'],", Person id : ",person_id, " , videoId : ", videoId])
+        logger.debug(log_string)
+        return True
