80b49cd5968b73d4663706ec05d3e7b3b8087c99
Tanmay Goel <goeltanmay@gmail.com>
2015-05-11 19:25:10 +0530

    test to main

--- ivr_data_collection/management/commands/ivr_call_initiate.py
+++ ivr_data_collection/management/commands/ivr_call_initiate.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        csvfile = open(os.path.join(__location__, 'test.csv'), 'rU')
+        csvfile = open(os.path.join(__location__, 'farmer_details.csv'), 'rU')
         reader = csv.DictReader(csvfile)
         count = 0
         for row in reader:
