509f77b685fd3dea58c25da842fa2440d74b9508
Tanmay Goel <goeltanmay@gmail.com>
2015-05-18 15:28:23 +0530

    cleanup 5

--- ivr_data_collection/urls.py
+++ ivr_data_collection/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
-from views import call_exotel, greeting_view, custom_field_update, screening_question, screening_answer, adoption_question, adoption_answer, nonnegotiable_question, nonnegotiable_answer, thanks_view
+from views import greeting_view, screening_question, screening_answer, adoption_question, adoption_answer, nonnegotiable_question, nonnegotiable_answer, thanks_view
 
 urlpatterns = patterns('',
-    (r'^call/', call_exotel),
     (r'^greeting/',greeting_view),
     (r'^updatecustomfield/',custom_field_update), 
     (r'^video/screening/question/',screening_question),
