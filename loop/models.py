5d0ca4af033f81f7c4347ba76474d11bce97b5f9
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2015-12-21 13:59:42 +0530

    added type in upload added migration for this

--- loop/models.py
+++ loop/models.py
@@ -4,6 +4,7 @@ class Upload(models.Model):
 	id = models.AutoField(primary_key=True)
 	mediator = models.CharField(max_length=50)
 	data = models.CharField(max_length=5000)
+	type = models.CharField(max_length=50, null=True, default=None)
 
 	def __unicode__(self):
 		return self.mediator
