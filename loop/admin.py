2515c81c74aa0033d4a9050b0ac1535c33c5c763
Tanmay Goel <goeltanmay@gmail.com>
2016-01-04 16:30:29 +0530

    upload model removed..

--- loop/admin.py
+++ loop/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
 
-from models import Upload
-
 class LoopAdmin(AdminSite):
 	def has_permission(self, request):
 		return request.user.is_active
 
 loop_admin = LoopAdmin(name='loop_admin')
-loop_admin.register(Upload)
