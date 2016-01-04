7843bb62edde775be70932cca9795d703c2b9c8a
Tanmay Goel <goeltanmay@gmail.com>
2016-01-04 16:25:39 +0530

    farmer and village resource added

--- loop/urls.py
+++ loop/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, url
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from api import UploadResource
+from api import FarmerResource, VillageResource
 from loop import views
 
 api = Api(api_name = "v1")
-api.register(UploadResource())
+api.register(FarmerResource)
+api.register(VillageResource)
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='loop'),
