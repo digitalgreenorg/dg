58bcaac68eb53e0b59dd0d4830636c199b464f55
Tanmay Goel <goeltanmay@gmail.com>
2016-01-04 17:18:54 +0530

    register village model in admin

--- loop/admin.py
+++ loop/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
 
+from models import Village
+
 class LoopAdmin(AdminSite):
 	def has_permission(self, request):
 		return request.user.is_active
 
 loop_admin = LoopAdmin(name='loop_admin')
+loop_admin.register(Village)
