7f2156486752c14ae84d1499c3939d98229984a8
Tanmay Goel <goeltanmay@gmail.com>
2016-01-04 14:15:56 +0530

    loop models and api

--- loop/api.py
+++ loop/api.py
@@ -1,7 +1,53 @@
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
+from django.forms.models import model_to_dict
 from models import Upload
+
+def foreign_key_to_id(bundle, field_name,sub_field_names):
+    field = getattr(bundle.obj, field_name)
+    if(field == None):
+        dict = {}
+        for sub_field in sub_field_names:
+            dict[sub_field] = None 
+    else:
+        dict = model_to_dict(field, fields=sub_field_names, exclude=[])
+    return dict
+
+def dict_to_foreign_uri(bundle, field_name, resource_name=None):
+    field_dict = bundle.data.get(field_name)
+    if field_dict.get('id'):
+        bundle.data[field_name] = "/loop/api/v1/%s/%s/"%(resource_name if resource_name else field_name, 
+                                                    str(field_dict.get('id')))
+    else:
+        bundle.data[field_name] = None
+    return bundle
+
+def dict_to_foreign_uri_m2m(bundle, field_name, resource_name):
+    m2m_list = bundle.data.get(field_name)
+    resource_uri_list = []
+    for item in m2m_list:
+        try:
+            resource_uri_list.append("/loop/api/v1/%s/%s/"%(resource_name, str(item.get('id'))))
+        except:
+            return bundle
+    bundle.data[field_name] = resource_uri_list
+    return bundle
+
+class UserResource(ModelResource):
+   # We need to store raw password in a virtual field because hydrate method
+   # is called multiple times depending on if it's a POST/PUT/PATCH request
+
+   class Meta:
+       authorization = Authorization()
+       allowed_methods = ['get', 'patch', 'put', 'post']
+       always_return_data = True
+       queryset = User.objects.all()
+       resource_name='user'
+       excludes = ['is_active', 'is_staff', 'is_superuser', 'date_joined',
+                   'last_login']
+       filtering = {'username':ALL,
+                    }
 
 class UploadResource(ModelResource):
 	class Meta:
@@ -9,3 +55,59 @@ class UploadResource(ModelResource):
 		resource_name = 'upload'
 		authentication = Authentication()
 		authorization = Authorization()
+
+class CountryResource(ModelResource):
+	class Meta:
+		queryset = Country.objects.all()
+		resource_name = 'country'
+		fields = ["country_name"]
+		authorization = Authorization()
+
+class StateResource(ModelResource):
+	country = fields.ForeignKey(CountryResource, attribute='country', full=True)
+	class Meta:
+		queryset = State.objects.all()
+		resource_name = 'state'
+		fields = ["state_name"]
+		authorization = Authorization()
+
+class DistrictResource(ModelResource):
+	state = fields.ForeignKey(StateResource, 'state', full=True)
+	class Meta:
+		queryset = District.objects.all()
+		resource_name = 'district'
+		fields = ["district_name"]
+		authorization = Authorization()
+
+class BlockResource(ModelResource):
+	district = fields.ForeignKey(DistrictResource, 'district', full=True)
+	class Meta:
+		queryset = Block.objects.all()
+		resource_name = 'block'
+		fields = ["block_name"]
+		authorization = Authorization()
+
+class VillageResource(ModelResource):
+	block = fields.ForeignKey(BlockResource, 'block', full=True)
+	class Meta:
+		queryset = Village.objects.all()
+		resource_name = 'village'
+		authorization = Authorization()
+
+class FarmerResource(ModelResource):
+	village = fields.ForeignKey(VillageNameResource, 'village', full=True)
+	class Meta:
+		queryset = Farmer.objects.all()
+		resource_name = 'farmer'
+		authorization = Authorization()
+
+class LoopUserResource(ModelResource):
+	user = fields.ForeignKey(UserResource, 'user')
+	assigned_villages = fields.ListField()
+		class Meta:
+		queryset = LoopUser.objects.prefetch_related('assigned_villages','user')
+		resource_name = 'loopUser'
+		authorization = Authorization()
+	hydrate_user = partial(dict_to_foreign_uri, field_name = 'user')
+	hydrate_assigned_villages = partial(dict_to_foreign_uri_m2m, field_name='assigned_villages', resource_name = 'village')
+	dehydrate_user = partial(foreign_key_to_id,, field_name='user', sub_field_names=['id','username'])
--- loop/models.py
+++ loop/models.py
@@ -1,9 +1,118 @@
-from django.db import models
 
-class Upload(models.Model):
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+RoleChoice = ((1, "Admin"), (2, "Aggregator"))
+
+class LoopModel(models.Model):
+    user_created = models.ForeignKey(User, related_name ="%(app_label)s_%(class)s_created", editable = False, null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    user_modified = models.ForeignKey(User, related_name ="%(app_label)s_%(class)s_related_modified",editable = False, null=True, blank=True)
+    time_modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+class LoopUser(LoopModel):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, related_name="loop_user")
+    role = models.IntegerField(choices = RoleChoice)
+    assigned_villages = models.ManyToManyField(Village, related_name="assigned_villages")
+
+    def __unicode__(self):
+    	return self.user.username
+
+class Country(LoopModel):
 	id = models.AutoField(primary_key=True)
-	mediator = models.CharField(max_length=50)
+	country_name = models.CharField(max_length=50)
+	
+	def __unicode__(self):
+		return self.country_name
+
+class State(LoopModel):
+	id = models.AutoField(primary_key=True)
+	state_name = models.CharField(max_length=50)
+	country = models.ForeignKey(Country)
+
+	def __unicode__(self):
+		return self.state_name
+
+class District(LoopModel):
+	id = models.AutoField(primary_key=True)
+	district_name = models.CharField(max_length=50)
+	state = models.ForeignKey(State)
+		
+	def __unicode__(self):
+		return self.district_name
+
+class Block(LoopModel):
+	id = models.AutoField(primary_key=True)
+	block_name = models.CharField(max_length=50)
+	district = models.ForeignKey(District)
+
+	def __unicode__(self):
+		return self.block_name
+
+class Village(LoopModel):
+	id = models.AutoField(primary_key=True)
+	village_name = models.CharField(max_length=50)
+	latitude = models.FloatField(null=True)
+	longitude = models.FloatField(null=True)
+	block = models.ForeignKey(Block)
+	
+	def __unicode__(self):
+		return self.village_name
+
+class Farmer(LoopModel):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+#	gender = models.CharField(max_length=1)		# M/F
+	phone = models.CharField(max_length=13)
+	image_path = models.CharField(max_length=100)
+	village = models.ForeignKey(Village)
+
+	def __unicode__(self):
+		return self.name;
+
+class Crop(LoopModel):
+	id = models.AutoField(primary_key=True)
+	crop_name = models.CharField(max_length=30)
+	measuring_unit = models.CharField(max_length=20)
+	image_path = models.CharField(max_length=100)
+		
+	def __unicode__(self):
+		return self.crop_name
+
+class Mandi(LoopModel):
+	id = models.AutoField(primary_key=True)
+	mandi_name = models.CharField(max_length=90)
+	latitude = models.FloatField(null=True)
+	longitude =  models.FloatField(null=True)
+	district = models.ForeignKey(District)
+		
+	def __unicode__(self):
+		return self.mandi_name
+
+class CombinedTransaction(LoopModel):
+ 	id = models.AutoField(primary_key=True)
+ 	date = models.DateField(auto_now=False)
+	aggregator = models.ForeignKey(LoopUser)
+	farmer = models.ForeignKey(Farmer)
+	crop = models.ForeignKey(Crop)
+	mandi = models.ForeignKey(Mandi)
+	crop_amount = models.FloatField()
+	crop_price = models.FloatField()
+	pay_status = models.CharField(max_length=1)
+	pay_amount = models.FloatField()
+
+	class Meta:
+		db_table = 'combined_transaction'
+
+class log(LoopModel):
+	id = models.AutoField(primary_key=True)
+	user = models.CharField(max_length=50)
 	data = models.CharField(max_length=5000)
+	timestamp = models.DateTimeField(auto_now=False)
 	type = models.CharField(max_length=50, null=True, default=None)
 
 	def __unicode__(self):
