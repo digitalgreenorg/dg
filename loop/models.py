80e35704bf16aecad12a39f704681383cbdc25b3
Tanmay Goel <goeltanmay@gmail.com>
2016-01-06 10:20:07 +0530

    tastypie to receive files..

--- loop/models.py
+++ loop/models.py
@@ -83,7 +83,7 @@ class Farmer(LoopModel):
 	name = models.CharField(max_length=100)
 #	gender = models.CharField(max_length=1)		# M/F
 	phone = models.CharField(max_length=13)
-	image_path = models.CharField(max_length=100)
+	image = models.ImageField(upload_to=STATIC_URL, null=True, blank=True)
 	village = models.ForeignKey(Village)
 
 	def __unicode__(self):
