7c6183208c648e4da0247626777dd441889d6d46
Tanmay Goel <goeltanmay@gmail.com>
2016-01-06 15:53:57 +0530

    try

--- loop/api.py
+++ loop/api.py
@@ -152,7 +152,7 @@ class FarmerResource(MultipartResource, ModelResource):
         print bundle.data['village'].split("/")[-2]
         result = {}
         if attempt.count() < 1:
-            bundle.obj = Farmer(name = bundle.data['name'], phone = bundle.data['phone'], village = Village.objects.get(id = int((bundle.data['village'].split("/"))[-2])))
+            bundle.obj = Farmer(name = bundle.data['name'], phone = bundle.data['phone'], village = Village.objects.get(id = int(bundle.data['village'].split("/")[-2])))
         else:
             raise ImmediateHttpResponse(response = HttpResponse("Duplicate : " + str(attempt[0].id), status=501))
 
