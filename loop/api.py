db59aca2106940f2dbd630820f2da0ebe5b2d1d0
Tanmay Goel <goeltanmay@gmail.com>
2016-01-06 12:10:37 +0530

    try3

--- loop/api.py
+++ loop/api.py
@@ -143,13 +143,13 @@ class FarmerResource(MultipartResource, ModelResource):
     def obj_create(self, bundle, request=None, **kwargs):
         bundle = self.full_hydrate(bundle)
         attempt = Farmer.objects.filter(**kwargs)
-        result = {}
         if attempt.count() < 1:
-            result = Farmer(*kwargs)
+            bundle.obj = Farmer(*kwargs)
         else:
-            result['id'] = attempt[0].id
-            result['error'] = 'Duplicate Entry'
-        return result
+            bundle.obj['id'] = attempt[0].pk
+            bundle.obj['error'] = 'Duplicate Entry'
+        return bundle
+
 class LoopUserResource(ModelResource):
 	user = fields.ForeignKey(UserResource, 'user')
 	assigned_villages = fields.ListField()
