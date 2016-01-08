5bdc2f2976a1373cc73e602a8a10fd97aaee1841
Tanmay Goel <goeltanmay@gmail.com>
2016-01-08 11:11:17 +0530

    try2

--- loop/api.py
+++ loop/api.py
@@ -163,8 +163,7 @@ class FarmerResource(BaseResource):
             error_dict = {}
             error_dict['id'] = attempt[0].id
             error_dict['message'] = 'Duplicate'
-            error = json.dumps(error_dict)
-            raise FarmerNotSaved(error)
+            raise FarmerNotSaved(error_dict)
         return bundle
     def dehydrate(self, bundle):
         bundle.data['onlineId'] = bundle.data['id']
