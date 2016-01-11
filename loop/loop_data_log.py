5b973ab935e3ec9e7f56eb31449d47e2d603397c
Tanmay Goel <goeltanmay@gmail.com>
2016-01-11 15:04:22 +0530

    try 3

--- loop/loop_data_log.py
+++ loop/loop_data_log.py
@@ -19,7 +19,7 @@ class DatetimeEncoder(json.JSONEncoder):
    def default(self, obj):
        if isinstance(obj, datetime.datetime):
            return obj.strftime('%Y-%m-%dT%H:%M:%SZ')
-       elif isinstance(obj, date):
+       elif isinstance(obj, datetime.date):
            return obj.strftime('%Y-%m-%d')
        # Let the base class default method raise the TypeError
        return json.JSONEncoder.default(self, obj)
