93a2392b48c3a29a7394defdddfa74814da981fc
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2016-01-23 03:07:59 +0530

    testing

--- activities/management/commands/delete_adoptions.py
+++ activities/management/commands/delete_adoptions.py
@@ -5,6 +5,5 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		obj = PersonAdoptPractice.objects.filter(person__village__block__district_id = 71, video_id=3157, date_of_adoption__range = ('2015-10-01','2016-01-21'))
 		print len(obj)
-		for i in obj:
-			i.delete()
+		obj.delete()
 		print len(obj)
