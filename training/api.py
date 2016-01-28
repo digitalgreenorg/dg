fb9f88572745e3acc92263cab7dc3a9b3796e41f
Tanmay Goel <goeltanmay@gmail.com>
2016-01-28 10:18:54 +0530

    dehydrate will always send online id

--- training/api.py
+++ training/api.py
@@ -32,6 +32,7 @@ def foreign_key_to_id(bundle, field_name,sub_field_names):
             dict[sub_field] = None 
     else:
         dict = model_to_dict(field, fields=sub_field_names, exclude=[])
+        dict["online_id"] = dict["id"]
     return dict
 
 def dict_to_foreign_uri(bundle, field_name, resource_name=None):
