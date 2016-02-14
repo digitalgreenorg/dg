9d25b500d20ed3aa7b6c70e629116d77810ebe46
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2016-02-14 23:01:28 +0530

    unintentional comment

--- loop/urls.py
+++ loop/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+
 from tastypie.api import Api
 from api import FarmerResource, VillageResource, LoopUserResource, CropResource, MandiResource, CombinedTransactionResource
+
 from loop.views import dashboard, login, home, village_wise_data, mediator_wise_data, crop_wise_data
 from loop_data_log import send_updated_log
 
 api = Api(api_name = "v1")
-# api.register(VillageResource())
-# api.register(LoopUserResource())
-# api.register(FarmerResource())
-# api.register(CropResource())
-# api.register(MandiResource())
-# api.register(CombinedTransactionResource())
+api.register(VillageResource())
+api.register(LoopUserResource())
+api.register(FarmerResource())
+api.register(CropResource())
+api.register(MandiResource())
+api.register(CombinedTransactionResource())
 
 urlpatterns = patterns('',
     url(r'^$', home, name='loop'),
