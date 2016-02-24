c1c6928fc25e5de8596a1a893aae43e0f9dfc183
Abhishek Chandran <abhishekchandran91@gmail.com>
2016-02-24 17:34:09 +0530

    bug fix

--- activities/management/commands/jslps_screening_pma.py
+++ activities/management/commands/jslps_screening_pma.py
@@ -108,7 +108,7 @@ class Command(BaseCommand):
 		xml_file.write(contents)
 		xml_file.close()
 
-		csv_file = open('/home/ubuntu/code/dg_test/dg/activities/management/pma_error.csv', 'wb')
+		csv_file = open('/home/ubuntu/code/dg_test/activities/management/pma_error.csv', 'wb')
 		#csv_file = open('C:\Users\Abhishek\Desktop\pma_error.csv', 'wb')
 		wtrr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 		tree = ET.parse('C:\Users\Abhishek\Desktop\\pma.xml')
