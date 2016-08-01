276a76a7853446156275edba2d0e682ef5445354
sourabhsingh29 <sourabh@digitalgreen.org>
2016-08-01 18:23:24 +0530

    cocouser issue resolved

--- deoanalytics/views.py
+++ deoanalytics/views.py
@@ -55,7 +55,8 @@ def deodatasetter(request):
 
     start_date = datetime.datetime.strptime(sdate, "%Y-%m-%d")
     end_date = datetime.datetime.strptime(edate, "%Y-%m-%d")
-
+    end_date+= datetime.timedelta(days=1)
+    
     Screening_objects = Screening.objects.filter(user_created_id=selecteddeo, time_created__gte=start_date, time_created__lte=end_date)
     screenings_entrydate = Screening_objects.values_list('time_created', flat=True)
     list_of_screening_entrydates = []
