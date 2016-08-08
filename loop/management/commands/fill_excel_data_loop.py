8decbb80c3603eb9755842c851592c29a6f62561
Abhishek Lodha <alodha21051992@gmail.com>
2016-08-08 17:15:00 +0530

    added all data in csv file, and script completed for CT

--- loop/management/commands/fill_excel_data_loop.py
+++ loop/management/commands/fill_excel_data_loop.py
@@ -1,6 +1,6 @@
 import os
-import unicodecsv as csv
-
+import csv
+import ast
 from xlrd import *
 from loop.models import *
 import datetime
@@ -10,45 +10,62 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        filename = 'loop/excel_data/Aggregator_Data.xlsx'
+        filename = 'loop/excel_data/ranjit_ct.csv'
         filepath = os.path.abspath(filename)
-        excel_workbook = open_workbook(filepath)
-        worksheet = excel_workbook.sheet_by_index(1)
+        excel_workbook = csv.reader(open(filepath))
+        # excel_workbook = open_workbook(filepath)
+        # worksheet = excel_workbook.sheet_by_index(1)
 
         error_file = 'loop/management/add_loop_data_error.csv'
         error_file_path = os.path.abspath(error_file)
         c = csv.writer(open(error_file_path, 'w'),
                        quoting=csv.QUOTE_ALL)
-        timestamp_ct = 1470383019
 
-        for r in range(1, 10):
-            date = worksheet.cell(r, 0).value
-            farmer_id = worksheet.cell(r, 2).value
-            village_id = worksheet.cell(r, 4).value
-            crop_id = worksheet.cell(r, 6).value
-            quantity = worksheet.cell(r, 7).value
-            price = worksheet.cell(r, 8).value
-            amount = quantity * price
-            mandi_id = worksheet.cell(r, 11).value
-            aggregator_id = worksheet.cell(r, 13).value
+        timestamp_ct = 1470638795
 
-            crop = Crop.objects.get(id=crop_id)
-            village = Village.objects.get(id=village_id)
-            farmer = Farmer.objects.get(id=farmer_id)
-            mandi = Mandi.objects.get(id=mandi_id)
-            user = LoopUser.objects.get(user_id=aggregator_id)
-            gaddidar = Gaddidar.objects.get(id=1)
+        excel_workbook.next()
+
+        for r in excel_workbook:
+            date = r[0]
+            farmer_id = r[2]
+            village_id = r[4]
+            crop_id = r[6]
+            quantity = r[7]
+            price = r[8]
+            amount = float(quantity) * float(price)
+            mandi_id = r[11]
+            aggregator_id = r[13]
+            gaddidar_id = r[14]
+
+            crop = Crop.objects.get(id=int(crop_id))
+            village = Village.objects.get(id=int(village_id))
+            print farmer_id
+            if(farmer_id != '#N/A'):
+                farmer = Farmer.objects.get(id=int(farmer_id))
+            else:
+                farmer = None
+                print 'Farmer Id with N/A'
+            mandi = Mandi.objects.get(id=int(mandi_id))
+            gaddidar = Gaddidar.objects.get(id=int(gaddidar_id))
             status = 1
-            print int(date), quantity, price, farmer, crop, village, mandi, user
-            print datetime.datetime(int(date))
-            # print datetime.datetime.strptime(date, '%d-%m-%Y')
-            # try:
-            #     # if(farmer!=null && crop!=null && village!=null && mandi!=null && user!=null){
-            #         transaction=CombinedTransaction(farmer=farmer,crop=crop,mandi=mandi,date=date,quantity=quantity,price=price,amount=amount,status=status,payment_date=date,timestamp=timestamp_ct)
-            #         transaction.save()
-            #         timestamp_ct+=1
-            #         print "CT added successfully"
-            #         # }
-            # except Exception as e:
-            #     print "CT : ",e
-            #     c.writerows(['CT :-', e])
+
+            try:
+                if(farmer is not None and crop is not None and village is not None and mandi is not None and aggregator_id is not None):
+                    transaction_date = datetime.datetime.strptime(
+                        date, '%d-%m-%y').date()
+                    # print transaction_date
+                    prev_transaction = CombinedTransaction.objects.get(
+                        user_created_id=aggregator_id, date=transaction_date, quantity=quantity, price=price, farmer=farmer, crop=crop, mandi=mandi, gaddidar=gaddidar)
+                    if prev_transaction is None:
+                        transaction = CombinedTransaction(user_created_id=aggregator_id, farmer=farmer, crop=crop, mandi=mandi, date=transaction_date,
+                                                          quantity=quantity, price=price, amount=amount, status=status, payment_date=transaction_date, timestamp=timestamp_ct, gaddidar=gaddidar)
+                        transaction.save()
+                        timestamp_ct += 1
+                        print "CT added successfully"
+                    else:
+                        print "Duplicate Found for CT"
+                        c.writerows(['CT :- Duplicate :', prev_transaction])
+
+            except Exception as e:
+                print "CT  ", e
+                c.writerows(['CT - Exception '])
