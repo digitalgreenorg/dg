d76dc4d15ca63d1f801242440ae3183a2e719b76
Abhishek Lodha <alodha21051992@gmail.com>
2016-09-28 12:25:43 +0530

    script initaiated

--- activities/management/commands/adoption_mediator_assignment.py
+++ activities/management/commands/adoption_mediator_assignment.py
@@ -6,12 +6,18 @@ from videos.models import Video
 from coco.base_models import ADOPTION_VERIFICATION, SCREENING_OBSERVATION, SCREENING_GRADE, VERIFIED_BY
 from activities.models import PersonAdoptPractice,Screening
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 
 class Command(BaseCommand):
     def handle(self,*args,**options):
-        pap_query=PersonAdoptPractice.objects.filter(animator_id__isnull=True)
-        print pap_query.count()
-        for pap in pap_query:
-            print pap['video']
-            screening = Screening.objects.filter(date__lte=pap['date_of_adoption'],videoes_screened__contains=pap['video']).order_by('-date')[0]
-            print screening['animator']
+        pap_query=Paginator(PersonAdoptPractice.objects.filter(animator_id__isnull=True),1000)
+        print pap_query.num_pages
+        for page in range(1, pap_query.num_pages + 1):
+            # print "----------------------------------------------------------------------------------------------------"
+            for row in pap_query.page(page).object_list:
+                print row.video
+                try:
+                    screening = Screening.objects.filter(date__lte=row.date_of_adoption,videoes_screened__in=[row.video]).order_by('-date')[0]
+                    print screening.animator
+                except Exception as e:
+                    print '##################################################################################################'
