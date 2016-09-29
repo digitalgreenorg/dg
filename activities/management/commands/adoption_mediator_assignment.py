4eb9f8b7bd56c4101fd2db9121b53a8d6cc8c4b2
Lokesh Garg <lokeshgarg92@gmail.com>
2016-09-30 00:17:13 +0530

    trying bulk_update

--- activities/management/commands/adoption_mediator_assignment.py
+++ activities/management/commands/adoption_mediator_assignment.py
@@ -2,6 +2,7 @@ from activities.models import PersonAdoptPractice,Screening, PersonMeetingAttend
 from django.core.management.base import BaseCommand
 from django.core.paginator import Paginator
 from datetime import datetime
+from bulk_update.helper import bulk_update
 
 class Command(BaseCommand):
     def handle(self,*args,**options):
@@ -10,7 +11,7 @@ class Command(BaseCommand):
 #        print pap_query.num_pages
         for page in range(1, 2):
             count = 0
-            print len(pap_query.page(page+10).object_list)
+            print len(pap_query.page(page+11).object_list)
 #        for page in range(1, pap_query.num_pages + 1):
             # print "----------------------------------------------------------------------------------------------------"
             for row in pap_query.page(page+10).object_list:
@@ -25,10 +26,11 @@ class Command(BaseCommand):
 #                  print str(screening.animator) + str(screening.date) + str(screening.village)
                     row.animator = screening.animator
 #                    print str(row.id) + '##' + str(row.person.id) + "##" + str(row.person) + str(row.date_of_adoption)
-                    row.save()
+#                    row.save()
                 except Exception as e:
                     count+=1
                     print "Exception" + str(e)
+            bulk_update(pap_query)
 #                    print '##################################################################################################'
             print "Done Successful"
             print "Failed : "+str(count)
