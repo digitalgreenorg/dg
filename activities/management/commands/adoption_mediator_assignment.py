a821571dc90d417be520408f0349a74399af5409
Lokesh Garg <lokeshgarg92@gmail.com>
2016-10-05 11:45:31 +0530

    mediator assignment to adoption working script and writing exception cases in a file

--- activities/management/commands/adoption_mediator_assignment.py
+++ activities/management/commands/adoption_mediator_assignment.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.core.paginator import Paginator
 from datetime import datetime
 from bulk_update.helper import bulk_update
-import csv
+import unicodecsv as csv
 from dg.base_settings import STATIC_URL
 from dg.settings import MEDIA_ROOT
 
@@ -11,12 +11,13 @@ from dg.settings import MEDIA_ROOT
 class Command(BaseCommand):
     def handle(self,*args,**options):
         print datetime.now()
-        pap_query=Paginator(PersonAdoptPractice.objects.filter(animator_id__isnull=True),5000)
+        pap_query=Paginator(PersonAdoptPractice.objects.filter(animator_id__isnull=True),100000)
 #        print pap_query.num_pages
         filename = 'C:/Users/Lokesh/Documents/dg_code/activities/management/exception.csv'
+        filename = '/home/ubuntu/code/dg_test/activities/management/exceptions.csv'
         for page in range(1, 2):
             count = 0
-            adoption_list = pap_query.page(page+15).object_list
+            adoption_list = pap_query.page(page).object_list
             print len(adoption_list)
 #        for page in range(1, pap_query.num_pages + 1):
             # print "----------------------------------------------------------------------------------------------------"
@@ -29,7 +30,7 @@ class Command(BaseCommand):
                         if len(screening) == 0:
                             with open(filename,'ab') as csvfile:
                                 fileWrite = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
-                                fileWrite.writerow(row.id)
+                                fileWrite.writerow(['ID',row.id])
                                 count+=1
                         else:
                             screening = screening[0]
