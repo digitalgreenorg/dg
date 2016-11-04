bc29cb7c6e347a4eda7f74bf8bcf52232083667a
sainivikas <vikassaini480@gmail.com>
2016-11-04 13:50:08 +0530

    single script added for run jslps data integration commands

--- activities/management/commands/jslps_data_integration.py
+++ activities/management/commands/jslps_data_integration.py
@@ -1,4 +1,26 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 class Command(BaseCommand):
-	print "Hello"
+	def handle(self, *args, **options):	
+		print "Geography,jslps_geo"
+		call_command('jslps_geo')
+
+		print "Groups,jslps_groups"
+		call_command('jslps_groups')
+
+		print "People,jslps_people"
+		call_command('jslps_people')
+
+		print "Mediators,jslps_mediators"
+		call_command('jslps_mediators')
+
+		print "Videos and Non negotiable,jslps_videos_nn"
+		call_command('jslps_videos_nn')
+
+		print "Screening and pma,jslps_screening_pma"
+		call_command('jslps_screening_pma')
+		
+		print "Adoption,jslps_adoptions"
+		call_command('jslps_adoptions')
+	
