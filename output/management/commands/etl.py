c8eac9b54a3b2146ee44a2876d9e363aa7a5d51b
Abhishek Chandran <abhishekchandran91@gmail.com>
2016-11-09 18:05:10 +0530

    etl file rollback

--- output/management/commands/etl.py
+++ output/management/commands/etl.py
@@ -39,93 +39,93 @@ class AnalyticsSync():
         #Create schema
         database = DATABASES['default']['NAME']
         print database
-        # ret_val = subprocess.call("mysql -u%s -p%s %s < %s" % (self.db_root_user, self.db_root_pass, database, os.path.join(DIR_PATH,'create_schema.sql')), shell=True)
-        # if ret_val != 0:
-        #     raise Exception("Could not recreate schema")
-        # print "Recreated schema"
+        ret_val = subprocess.call("mysql -u%s -p%s %s < %s" % (self.db_root_user, self.db_root_pass, database, os.path.join(DIR_PATH,'create_schema.sql')), shell=True)
+        if ret_val != 0:
+            raise Exception("Could not recreate schema")
+        print "Recreated schema"
         
         #Fill Data
         try:
-            # #village_partner_myisam
-            # self.db_cursor.execute("""INSERT INTO village_partner_myisam (partner_id,village_id,block_id,district_id,state_id,country_id)
-            #                             SELECT distinct pp.partner_id, gv.id ,gb.id ,gd.id ,gs.id ,gc.id
-            #                             FROM people_person pp INNER JOIN programs_partner ppa ON pp.partner_id = ppa.id INNER JOIN geographies_village gv ON pp.village_id = gv.id INNER JOIN geographies_block gb on gv.block_id = gb.id INNER JOIN geographies_district gd on gb.district_id=gd.id INNER JOIN geographies_state gs on gd.state_id =  gs.id INNER JOIN geographies_country gc on gs.country_id=gc.id""")
-            # print "Finished insert into village_partner_myisam"
+            #village_partner_myisam
+            self.db_cursor.execute("""INSERT INTO village_partner_myisam (partner_id,village_id,block_id,district_id,state_id,country_id)
+                                        SELECT distinct pp.partner_id, gv.id ,gb.id ,gd.id ,gs.id ,gc.id
+                                        FROM people_person pp INNER JOIN programs_partner ppa ON pp.partner_id = ppa.id INNER JOIN geographies_village gv ON pp.village_id = gv.id INNER JOIN geographies_block gb on gv.block_id = gb.id INNER JOIN geographies_district gd on gb.district_id=gd.id INNER JOIN geographies_state gs on gd.state_id =  gs.id INNER JOIN geographies_country gc on gs.country_id=gc.id""")
+            print "Finished insert into village_partner_myisam"
 
-            # #screening_myisam
-            # self.db_cursor.execute("""INSERT INTO screening_myisam (screening_id, date, video_id, practice_id, group_id,
-            #                             village_id, block_id, district_id, state_id, country_id, partner_id)
-            #                             SELECT sc.id, date, svs.video_id, vid.related_practice_id, persongroup_id, sc.village_id, block_id,
-            #                             district_id, state_id, country_id, sc.partner_id
-            #                             FROM activities_screening sc
-            #                             JOIN activities_screening_videoes_screened svs on svs.screening_id = sc.id
-            #                             JOIN activities_screening_farmer_groups_targeted sfgt on sfgt.screening_id = sc.id
-            #                             JOIN videos_video vid on vid.id = svs.video_id
-            #                             JOIN geographies_village v on v.id = sc.village_id
-            #                             JOIN geographies_block b on b.id = v.block_id
-            #                             JOIN geographies_district d on d.id = b.district_id
-            #                             JOIN geographies_state s on s.id = d.state_id""")
-            # print "Finished insert into screening_myisam"
-            # #video_myisam
-            # self.db_cursor.execute("""INSERT INTO video_myisam (video_id, video_production_date, practice_id, video_type,
-            #                             language_id, village_id, block_id, district_id, state_id, country_id, partner_id)
-            #                             select vid.id, production_date, related_practice_id, video_type, 
-            #                             language_id, vid.village_id, block_id, district_id,
-            #                             state_id, country_id, vid.partner_id
-            #                             FROM videos_video vid
-            #                             JOIN geographies_village v on v.id = vid.village_id
-            #                             JOIN geographies_block b on b.id = v.block_id
-            #                             JOIN geographies_district d on d.id = b.district_id
-            #                             JOIN geographies_state s on s.id = d.state_id
-            #                             WHERE vid.video_type = 1""")
-            # print "Finished insert into video_myisam"
+            #screening_myisam
+            self.db_cursor.execute("""INSERT INTO screening_myisam (screening_id, date, video_id, practice_id, group_id,
+                                        village_id, block_id, district_id, state_id, country_id, partner_id)
+                                        SELECT sc.id, date, svs.video_id, vid.related_practice_id, persongroup_id, sc.village_id, block_id,
+                                        district_id, state_id, country_id, sc.partner_id
+                                        FROM activities_screening sc
+                                        JOIN activities_screening_videoes_screened svs on svs.screening_id = sc.id
+                                        JOIN activities_screening_farmer_groups_targeted sfgt on sfgt.screening_id = sc.id
+                                        JOIN videos_video vid on vid.id = svs.video_id
+                                        JOIN geographies_village v on v.id = sc.village_id
+                                        JOIN geographies_block b on b.id = v.block_id
+                                        JOIN geographies_district d on d.id = b.district_id
+                                        JOIN geographies_state s on s.id = d.state_id""")
+            print "Finished insert into screening_myisam"
+            #video_myisam
+            self.db_cursor.execute("""INSERT INTO video_myisam (video_id, video_production_date, practice_id, video_type,
+                                        language_id, village_id, block_id, district_id, state_id, country_id, partner_id)
+                                        select vid.id, production_date, related_practice_id, video_type, 
+                                        language_id, vid.village_id, block_id, district_id,
+                                        state_id, country_id, vid.partner_id
+                                        FROM videos_video vid
+                                        JOIN geographies_village v on v.id = vid.village_id
+                                        JOIN geographies_block b on b.id = v.block_id
+                                        JOIN geographies_district d on d.id = b.district_id
+                                        JOIN geographies_state s on s.id = d.state_id
+                                        WHERE vid.video_type = 1""")
+            print "Finished insert into video_myisam"
                                           
-            # #person_meeting_attendance_myisam
-            # self.db_cursor.execute("""INSERT INTO person_meeting_attendance_myisam (pma_id, person_id, screening_id, gender, date, 
-            #                             village_id, block_id, district_id, state_id, country_id, partner_id)
-            #                             SELECT pma.id, pma.person_id, sc.id, GENDER, date, sc.village_id, block_id,
-            #                             district_id, state_id, country_id, sc.partner_id
-            #                             FROM activities_personmeetingattendance pma 
-            #                             JOIN activities_screening sc on sc.id = pma.screening_id
-            #                             JOIN people_person p on p.id = pma.person_id
-            #                             JOIN geographies_village v on v.id = sc.village_id
-            #                             JOIN geographies_block b on b.id = v.block_id
-            #                             JOIN geographies_district d on d.id = b.district_id
-            #                             JOIN geographies_state s on s.id = d.state_id""")
-            # print "Finished insert into person_meeting_attendance_myisam"
+            #person_meeting_attendance_myisam
+            self.db_cursor.execute("""INSERT INTO person_meeting_attendance_myisam (pma_id, person_id, screening_id, gender, date, 
+                                        village_id, block_id, district_id, state_id, country_id, partner_id)
+                                        SELECT pma.id, pma.person_id, sc.id, GENDER, date, sc.village_id, block_id,
+                                        district_id, state_id, country_id, sc.partner_id
+                                        FROM activities_personmeetingattendance pma 
+                                        JOIN activities_screening sc on sc.id = pma.screening_id
+                                        JOIN people_person p on p.id = pma.person_id
+                                        JOIN geographies_village v on v.id = sc.village_id
+                                        JOIN geographies_block b on b.id = v.block_id
+                                        JOIN geographies_district d on d.id = b.district_id
+                                        JOIN geographies_state s on s.id = d.state_id""")
+            print "Finished insert into person_meeting_attendance_myisam"
                                           
-            # #person_adopt_practice_myisam
-            # self.db_cursor.execute("""INSERT INTO person_adopt_practice_myisam (adoption_id, person_id, video_id, gender, date_of_adoption, 
-            #                             village_id, block_id, district_id, state_id, country_id, partner_id)
-            #                             SELECT pap.id, pap.person_id, video_id, GENDER, date_of_adoption, p.village_id, block_id,
-            #                             district_id, state_id, country_id, pap.partner_id
-            #                             FROM activities_personadoptpractice pap 
-            #                             JOIN people_person p on p.id = pap.person_id
-            #                             JOIN geographies_village v on v.id = p.village_id
-            #                             JOIN geographies_block b on b.id = v.block_id
-            #                             JOIN geographies_district d on d.id = b.district_id
-            #                             JOIN geographies_state s on s.id = d.state_id""")
-            # print "Finished insert into person_adopt_practice_myisam"
+            #person_adopt_practice_myisam
+            self.db_cursor.execute("""INSERT INTO person_adopt_practice_myisam (adoption_id, person_id, video_id, gender, date_of_adoption, 
+                                        village_id, block_id, district_id, state_id, country_id, partner_id)
+                                        SELECT pap.id, pap.person_id, video_id, GENDER, date_of_adoption, p.village_id, block_id,
+                                        district_id, state_id, country_id, pap.partner_id
+                                        FROM activities_personadoptpractice pap 
+                                        JOIN people_person p on p.id = pap.person_id
+                                        JOIN geographies_village v on v.id = p.village_id
+                                        JOIN geographies_block b on b.id = v.block_id
+                                        JOIN geographies_district d on d.id = b.district_id
+                                        JOIN geographies_state s on s.id = d.state_id""")
+            print "Finished insert into person_adopt_practice_myisam"
 
-            # #activities_screeningwisedata
-            # self.db_cursor.execute("""INSERT INTO activities_screeningwisedata (user_created_id, time_created, user_modified_id, time_modified,
-            #                             screening_id, old_coco_id, screening_date, start_time, location, village_id, animator_id, 
-            #                             partner_id, video_id, video_title, persongroup_id,video_youtubeid) 
-            #                             SELECT  A.user_created_id, A.time_created, A.user_modified_id, A.time_modified,  A.id, 
-            #                             A.old_coco_id, A.date, A.start_time, A.location, A.village_id, A.animator_id, A.partner_id, B.video_id, D.title, C.PERSONGROUP_ID,D.youtubeid
-            #                             from activities_screening A join activities_screening_videoes_screened B on B.screening_id=A.id join videos_video D on B.video_id=D.id 
-            #                             join activities_screening_farmer_groups_targeted C on C.SCREENING_ID = A.id""")
-            # print "Finished insert into activities_screeningwisedata"
+            #activities_screeningwisedata
+            self.db_cursor.execute("""INSERT INTO activities_screeningwisedata (user_created_id, time_created, user_modified_id, time_modified,
+                                        screening_id, old_coco_id, screening_date, start_time, location, village_id, animator_id, 
+                                        partner_id, video_id, video_title, persongroup_id,video_youtubeid) 
+                                        SELECT  A.user_created_id, A.time_created, A.user_modified_id, A.time_modified,  A.id, 
+                                        A.old_coco_id, A.date, A.start_time, A.location, A.village_id, A.animator_id, A.partner_id, B.video_id, D.title, C.PERSONGROUP_ID,D.youtubeid
+                                        from activities_screening A join activities_screening_videoes_screened B on B.screening_id=A.id join videos_video D on B.video_id=D.id 
+                                        join activities_screening_farmer_groups_targeted C on C.SCREENING_ID = A.id""")
+            print "Finished insert into activities_screeningwisedata"
 
-            # #people_animatorwisedata
-            # self.db_cursor.execute("""INSERT INTO people_animatorwisedata (user_created_id, time_created, user_modified_id, time_modified, 
-            #                             animator_id, old_coco_id, animator_name, gender, phone_no, partner_id, district_id, total_adoptions, 
-            #                             assignedvillage_id, start_date )
-            #                             SELECT A.user_created_id, A.time_created, A.user_modified_id, A.time_modified, A.id, A.old_coco_id, A.name,
-            #                             A.gender, A.phone_no, A.partner_id, A.district_id, A.total_adoptions, B.village_id, B.start_date 
-            #                             from people_animator A 
-            #                             join people_animatorassignedvillage B on A.id=B.animator_id""")
-            # print "Finished insert into people_animatorwisedata"
+            #people_animatorwisedata
+            self.db_cursor.execute("""INSERT INTO people_animatorwisedata (user_created_id, time_created, user_modified_id, time_modified, 
+                                        animator_id, old_coco_id, animator_name, gender, phone_no, partner_id, district_id, total_adoptions, 
+                                        assignedvillage_id, start_date )
+                                        SELECT A.user_created_id, A.time_created, A.user_modified_id, A.time_modified, A.id, A.old_coco_id, A.name,
+                                        A.gender, A.phone_no, A.partner_id, A.district_id, A.total_adoptions, B.village_id, B.start_date 
+                                        from people_animator A 
+                                        join people_animatorassignedvillage B on A.id=B.animator_id""")
+            print "Finished insert into people_animatorwisedata"
             
             
 
@@ -135,23 +135,21 @@ class AnalyticsSync():
                 tot_exp_att=0, tot_ques=0, tot_adopted_att=0, tot_active=0, tot_ado_by_act=0,
                 tot_active_vid_seen=0))))
             
-            print "1"
             sixty_days = datetime.timedelta(days=60)
-            print "2"
+ 
             person_village_qs = Person.objects.values_list('id','village','partner')
             person_village = {}
             person_partner ={}
             for id, village, partner in person_village_qs:
                 person_village[id] = village
                 person_partner[id] = partner
-            print "3"
+            
             pmas = PersonMeetingAttendance.objects.values('id', 'person','screening__date', 'person__gender', 'screening__questions_asked', 
             'screening__village__id', 'screening__partner__id').order_by('person', 'screening__date')
             person_att_dict = defaultdict(list) #Stores the active period of farmers in tuples (from_date, to_date)
             person_video_seen_date_dict = defaultdict(list) # For calculating total videos seen
             max_date = min_date = cur_person = prev_pma_id = None
-            print "4"
-            for pma in pmas.iteritems():
+            for pma in pmas:
                 per = pma['person']
                 dt = pma['screening__date']
                 person_video_seen_date_dict[per].append(dt)
@@ -179,14 +177,14 @@ class AnalyticsSync():
                     counts['tot_male_att'] = counts['tot_male_att'] + 1
                 else:
                     counts['tot_fem_att'] = counts['tot_fem_att'] + 1
-            print "5"
+
             scr = Screening.objects.values('questions_asked')
             for s in scr:
                 counts['tot_ques'] = counts['tot_ques'] + 1
-            print "6"        
+                     
             if min_date and max_date and cur_person:
                 person_att_dict[cur_person].append((min_date, max_date))
-            print "7"     
+                 
             del pmas #Free memory
             del scr
 
