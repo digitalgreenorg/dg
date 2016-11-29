435f3b5625e831aa05240fafef87b3ad64ae9713
Abhishek Chandran <abhishekchandran91@gmail.com>
2016-11-29 18:04:44 +0530

    removed unwanted comments

--- output/management/commands/delete_myisam_tables.sql
+++ output/management/commands/delete_myisam_tables.sql
@@ -1,3 +1,4 @@
+/* Deleting data from past one year from all myisam tables*/
 DELETE 
 FROM 
 	`screening_myisam` 
@@ -13,27 +14,20 @@ FROM
 	`person_meeting_attendance_myisam`
 WHERE `date` >= 20161001;
 
- /* DELETE  last 1 year Data from person_adopt_practice_myisam*/
-
 DELETE
 FROM
 	`person_adopt_practice_myisam`
 WHERE  `date_of_adoption` >= 20161001;
-/*DELETE last 1 year Data from activities_screeningwisedata*/
 
 DELETE
 FROM
 	`activities_screeningwisedata`
 WHERE `screening_date`  >= 20161001;
 
--- DELETE last 1 year Data from people_animatorwisedata
-
 DELETE
 FROM
 	`people_animatorwisedata`
 WHERE `time_created`  >= 20161001;
-
--- DELETE last 1 year Data from village_precalculation_copy
 
 DELETE
 FROM
