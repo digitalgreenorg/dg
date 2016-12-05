f367e5b8e8c8d30bf14c559164c78040a6b52c35
sujit22993 <sujit22993@gmail.com>
2016-12-05 17:01:52 +0530

    DB scheduler updated to drop schema if already exist

--- scripts/db_import.py
+++ scripts/db_import.py
@@ -10,13 +10,18 @@ sys.path.append(os.path.abspath(os.path.realpath('..')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'dg.settings'
 from dg.settings import DATABASES
 
+user_name = DATABASES['default']['USER']
+password = DATABASES['default']['PASSWORD']
+
 #script to download db from S3
 execfile("download_from_s3.py")
 
 dbName = glob.glob("*.gz")
-# delete previous db leaving one
-for i in range(len(dbName) - 1) :
-	os.remove(dbName[i])
+
+# buffer size showing delete all except one.
+# buffer_size = 1
+# for i in range(len(dbName) - buffer_size) :
+# 	os.remove(dbName[i])
 
 db_name = dbName[len(dbName) - 1].split('_')
 # Unrar gz file
@@ -38,8 +43,10 @@ else :
 	custom_db_name = 'dg_' + prev_day
 
 # Mysql Connection
-db = MySQLdb.connect("localhost","root","root")
+db = MySQLdb.connect("localhost", user_name, password)
 cursor = db.cursor()
+drop_database_sql = "DROP DATABASE IF EXISTS " + custom_db_name
+cursor.execute(drop_database_sql)
 sql = '''create schema ''' + custom_db_name
 cursor.execute(sql)
 db.close()
@@ -47,11 +54,9 @@ db.close()
 dbName = glob.glob("*.sql")
 
 # delete previous sql leaving one
-#for i in range(len(dbName) - 1) :
-#	os.remove(dbName[i])
+# for i in range(len(dbName) - buffer_size) :
+# 	os.remove(dbName[i])
 
-user_name = DATABASES['default']['USER']
-password = DATABASES['default']['PASSWORD']
 # To use current DB replace database = DATABASES['default']['NAME']
 database = custom_db_name
 
