f6e45d52223f07a2ad85af4f016d4bdb097429ce
djangobotpython <varma.nikhil22@gmail.com>
2016-12-20 12:01:50 +0530

    sending data in loop to avoid duplicate error from update

--- activities/management/commands/verification_date_to_be_adoption_date.py
+++ activities/management/commands/verification_date_to_be_adoption_date.py
@@ -9,10 +9,13 @@ class Command(BaseCommand):
 	help = 'Saves date_of_verification in date_of_adoption field under PersonAdoptPractice'
 
 	def handle(self, *args, **options):
-		try:
-			obj = PersonAdoptPractice.objects.filter(date_of_adoption__gte=datetime.datetime(2016, 07, 01)).exclude(date_of_verification__exact=None)\
+		obj = PersonAdoptPractice.objects.filter(date_of_adoption__gte=datetime.datetime(2016, 07, 01)).exclude(date_of_verification__exact=None)\
 					.filter(Q(date_of_adoption__isnull=False,
-							  date_of_verification__isnull=False) & ~Q(date_of_adoption=F('date_of_verification')))\
-					.update(date_of_adoption=F('date_of_verification'))
-		except IntegrityError as e:
-			print 'Error ::', e
+							  date_of_verification__isnull=False) & ~Q(date_of_adoption=F('date_of_verification')))
+		if len(obj):
+			try:
+				for iterable in obj:
+					iterable.date_of_adoption = iterable.date_of_verification
+					iterable.save()
+			except IntegrityError as e:
+				print 'Error ::', e
