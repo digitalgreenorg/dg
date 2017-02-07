bef1a2490b7aacbb5716f4645e0175245dfc5b94
sainivikas <vikassaini480@gmail.com>
2017-02-07 11:16:02 +0530

    sms body changed

--- loop/views.py
+++ loop/views.py
@@ -876,14 +876,14 @@ def helpline_call_response(request):
             if outgoing_obj:
                 farmer_number = outgoing_obj.to_number
                 #send sms to Notify User about Later Call
-                sms_body = "Hello,You will receive call later."
+                sms_body = "अभी आपसे संपर्क नही हो पा रहा है, हम आपको कुछ समय बाद संपर्क करेगे. कॉल करने के लिए धन्यवाद."
                 send_helpline_sms(EXOTEL_HELPLINE_NUMBER,farmer_number,sms_body)
             else:
                 call_detail = get_info_through_api(outgoing_call_id)
                 if call_detail != '':
                     farmer_number = call_detail[2]
                     #send sms to Notify User about Later Call
-                    sms_body = "Hello,You will receive call later."
+                    sms_body = "अभी आपसे संपर्क नही हो पा रहा है, हम आपको कुछ समय बाद संपर्क करेगे. कॉल करने के लिए धन्यवाद."
                     send_helpline_sms(EXOTEL_HELPLINE_NUMBER,farmer_number,sms_body)
         elif status == 'no-answer' or status == 'busy':  
             call_status = get_status(outgoing_call_id)
@@ -895,7 +895,7 @@ def helpline_call_response(request):
                     else:
                         farmer_number = call_status['to']                    
                     #send sms to Notify User about Later Call
-                    sms_body = "Hello,You will receive call later."
+                    sms_body = "अभी आपसे संपर्क नही हो पा रहा है, हम आपको कुछ समय बाद संपर्क करेगे. कॉल करने के लिए धन्यवाद."
                     send_helpline_sms(EXOTEL_HELPLINE_NUMBER,farmer_number,sms_body)
                     return HttpResponse(status=200)
             make_call = 0
