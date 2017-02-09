caf3db756d31ce2033ff230baa23b641a0de7986
Abhishek Lodha <alodha21051992@gmail.com>
2017-02-09 14:37:33 +0530

    calcualting gaddidar share amount

--- loop/management/commands/loop_etl.py
+++ loop/management/commands/loop_etl.py
@@ -64,6 +64,44 @@ class LoopStatistics():
             print result.head()
             print result.shape
 
+            start_time = time.time()
+            gc_queryset = GaddidarCommission.objects.all()
+            gso_queryset = GaddidarShareOutliers.objects.all()
+            combined_ct_queryset = CombinedTransaction.objects.values(
+                'date', 'user_created_id', 'gaddidar', 'mandi', 'gaddidar__discount_criteria').order_by('-date').annotate(
+                Sum('quantity'), Sum('amount'))
+            gaddidar_share_result = []
+            # gso_list = [gso.date for gso in gso_queryset.filter(gaddidar=CT['gaddidar'], aggregator=user.id)]
+            for CT in combined_ct_queryset:
+                amount_sum = 0
+                user = LoopUser.objects.get(user_id=CT['user_created_id'])
+                if CT['date'] not in [gso.date for gso in gso_queryset.filter(gaddidar=CT['gaddidar'], aggregator=user.id)]:
+                    try:
+                        gc_list_set = gc_queryset.filter(start_date__lte=CT['date'], gaddidar=CT[
+                            'gaddidar']).order_by('-start_date')
+                        if CT['gaddidar__discount_criteria'] == 0 and gc_list_set.count() > 0:
+                            amount_sum += CT['quantity__sum'] * \
+                                   gc_list_set[0].discount_percent
+                        elif gc_list_set.count() > 0:
+                            amount_sum += CT['amount__sum'] * gc_list_set[0].discount_percent
+                    except GaddidarCommission.DoesNotExist:
+                        pass
+                else:
+                    try:
+                        gso_gaddidar_date_aggregator = gso_queryset.filter(
+                            date=CT['date'], aggregator=user.id, gaddidar=CT['gaddidar']).values_list('amount', flat=True)
+                        if gso_gaddidar_date_aggregator.count():
+                            amount_sum += gso_gaddidar_date_aggregator[0]
+                    except GaddidarShareOutliers.DoesNotExist:
+                        pass
+                gaddidar_share_result.append({'date': CT['date'], 'user_created__id': CT['user_created_id'], 'gaddidar__id': CT[
+                    'gaddidar'], 'mandi__id': CT['mandi'], 'gaddidar_share_amount': amount_sum, 'quantity__sum': CT['quantity__sum']})
+
+            gaddidar_share = pd.DataFrame(gaddidar_share_result)
+            # end_time = time.time()
+            # print "total_time = %d" %(end_time-start_time)
+            print gaddidar_share.head()
+
 
 
 
