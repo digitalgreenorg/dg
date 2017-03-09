768b905bdec41f963620b7ba5917c426cb1609cd
Lokesh Garg <lokeshgarg92@gmail.com>
2017-03-09 12:54:55 +0530

    migration sorted out to avoid conflict with ivr migrations

--- loop/migrations/0008_auto_20170309_1252.py
+++ loop/migrations/0008_auto_20170309_1252.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('loop', '0005_auto_20161202_1104'),
+        ('loop', '0007_auto_20170214_1040'),
     ]
 
     operations = [
@@ -18,7 +18,17 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='state',
+            name='crop_add',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='state',
             name='helpline_number',
             field=models.CharField(default=b'0', max_length=14),
+        ),
+        migrations.AlterField(
+            model_name='aggregatorincentive',
+            name='model_type',
+            field=models.IntegerField(choices=[(0, b'Direct'), (1, b'Tax Based'), (2, b'Slab Based'), (3, b'Daily Pay')]),
         ),
     ]
