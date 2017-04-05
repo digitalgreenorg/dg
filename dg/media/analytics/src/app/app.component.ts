5a2015b2fcd395041ea4fcf348ba3d4d6b7572c4
Abhishek Chandran <abhishekchandran91@gmail.com>
2017-04-05 06:23:55 +0530

    dummy graph rendered

--- dg/media/analytics/src/app/app.component.ts
+++ dg/media/analytics/src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let d3:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  title = 'Graphs';
+  options;
+  data;
+  ngOnInit(){
+        this.options = {
+      chart: {
+        type: 'discreteBarChart',
+        height: 450,
+        margin : {
+          top: 20,
+          right: 20,
+          bottom: 50,
+          left: 55
+        },
+        x: (d) => {return d.label;},
+        y: (d) => {return d.value;},
+        showValues: true,
+        valueFormat: function(d){
+          return d3.format(',.4f')(d);
+        },
+        duration: 500,
+        xAxis: {
+          axisLabel: 'X Axis'
+        },
+        yAxis: {
+          axisLabel: 'Y Axis',
+          axisLabelDistance: -10
+        }
+      }
+    }
+    this.data = [
+      {
+        key: "Cumulative Return",
+        values: [
+          {
+            "label" : "A" ,
+            "value" : -29.765957771107
+          } ,
+          {
+            "label" : "B" ,
+            "value" : 0
+          } ,
+          {
+            "label" : "C" ,
+            "value" : 32.807804682612
+          } ,
+          {
+            "label" : "D" ,
+            "value" : 196.45946739256
+          } ,
+          {
+            "label" : "E" ,
+            "value" : 0.19434030906893
+          } ,
+          {
+            "label" : "F" ,
+            "value" : -98.079782601442
+          } ,
+          {
+            "label" : "G" ,
+            "value" : -13.925743130903
+          } ,
+          {
+            "label" : "H" ,
+            "value" : -5.1387322875705
+          }
+        ]
+      }
+    ];
+  }
 }
--- dg/media/analytics/src/app/app.module.ts
+++ dg/media/analytics/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NvD3Module } from 'angular2-nvd3';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NvD3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
