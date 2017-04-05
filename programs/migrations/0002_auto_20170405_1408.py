2cf7cd75f0b9ff82e058cb05c6ce602fbd32ec28
sainivikas <vikassaini480@gmail.com>
2017-04-05 14:08:53 +0530

    programs migration combined

--- programs/migrations/0002_auto_20170405_1408.py
+++ programs/migrations/0002_auto_20170405_1408.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('programs', '0002_partner_full_partner_name'),
+        ('programs', '0001_initial'),
     ]
 
     operations = [
@@ -22,12 +22,29 @@ class Migration(migrations.Migration):
                 ('project_name', models.CharField(unique=True, max_length=100)),
                 ('start_date', models.DateField(null=True, blank=True)),
                 ('end_date', models.DateField(null=True, blank=True)),
-                ('associate_partner', models.ManyToManyField(to='programs.Partner', blank=True)),
-                ('user_created', models.ForeignKey(related_name='programs_project_created', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
-                ('user_modified', models.ForeignKey(related_name='programs_project_related_modified', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
                 'abstract': False,
             },
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='full_partner_name',
+            field=models.CharField(default=b'', max_length=200, blank=True),
+        ),
+        migrations.AddField(
+            model_name='project',
+            name='associate_partner',
+            field=models.ManyToManyField(to='programs.Partner', blank=True),
+        ),
+        migrations.AddField(
+            model_name='project',
+            name='user_created',
+            field=models.ForeignKey(related_name='programs_project_created', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AddField(
+            model_name='project',
+            name='user_modified',
+            field=models.ForeignKey(related_name='programs_project_related_modified', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True),
         ),
     ]
