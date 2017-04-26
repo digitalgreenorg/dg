7bec7c6d7fd994c62a6ef6812d8e67e515443d0a
sainivikas <vikassaini480@gmail.com>
2017-04-26 16:02:56 +0530

    title field added in Broadcast form

--- loop/forms.py
+++ loop/forms.py
@@ -2,10 +2,9 @@ from django import forms
 from loop.models import Broadcast, BroadcastUser, LoopUser
 
 class BroadcastTestForm(forms.Form):
-	to_number = forms.CharField(max_length=20, required=True)
+	to_number = forms.CharField(max_length=20)
 	audio_file = forms.FileField(label='Select a WAV Audio file',
-                               help_text='Max 5MB',
-                               required=True,
+                               help_text='Upload .wav, 8Khz Mono format audio file with bit depth must be 16 bit (Max. Size 5MB)'
                                )
 
 	def clean_to_number(self):
@@ -17,30 +16,28 @@ class BroadcastTestForm(forms.Form):
 		return to_number
 
 	def clean_audio_file(self):
-		print "In clean audio"
 		audio_file = self.cleaned_data.get('audio_file')
 		if audio_file.content_type != 'audio/wav':
 			raise forms.ValidationError("Please upload a WAV Audio file.")
-		# .size return size in bytes
+		# .size returns size in bytes
 		if audio_file.size/(1024*1024.0) > 5:
 			 raise forms.ValidationError("Please upload a WAV Audio file less than 5 MB")
 		return audio_file
 
 
 class BroadcastForm(forms.Form):
+	title = forms.CharField(label='Broadcast Title',widget=forms.TextInput(attrs={'placeholder': 'Broadcast Title'}),max_length=Broadcast._meta.get_field('title').max_length)
 	cluster_choice = [('','Select a Cluster')] + [(cluster['id'],'%s (%s)'%(cluster['name'],cluster['village__village_name'])) for cluster in LoopUser.objects.filter(role=2).values('id', 'name', 'village__village_name')]
 	cluster = forms.ChoiceField(label='Select Cluster',choices=cluster_choice)
 	audio_file = forms.FileField(label='Select a .WAV Audio file',
-                               help_text='Upload .wav, 8Khz Mono format audio file with bit depth must be 16 bit (Max. Size 5MB)',
-                               required=True,
+                               help_text='Upload .WAV, 8Khz Mono format audio file with bit depth must be 16 bit (Max. Size 5MB)'
                                )
 
 	def clean_audio_file(self):
-		print "In clean audio"
 		audio_file = self.cleaned_data.get('audio_file')
 		if audio_file.content_type != 'audio/wav':
 			raise forms.ValidationError("Please upload a WAV Audio file.")
-		# .size return size in bytes
+		# .size returns size in bytes
 		if audio_file.size/(1024*1024.0) > 5:
 			 raise forms.ValidationError("Please upload a WAV Audio file less than 5 MB")
 		return audio_file
