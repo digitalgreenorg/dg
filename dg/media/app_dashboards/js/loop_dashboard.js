71bf4ed2583350013caf0981bd86e94f3db08937
sourabhsingh29 <sourabh@digitalgreen.org>
2017-05-08 13:48:20 +0530

    changed

--- dg/media/app_dashboards/js/loop_dashboard.js
+++ dg/media/app_dashboards/js/loop_dashboard.js
@@ -3482,7 +3482,7 @@ function aggregator_payment_sheet(data_json, aggregator, agg_id, aggregator_name
                                 }
                                 rows_table4 = [];
                                 get_payments_data();
-                                delay = 3000;
+                                delay = 6000;
                                 setTimeout(function() {
                                     $("#aggregator_payments").val(aggregator).change();
                                     //$("#aggregator_payment_tab :input").val(aggregator_name_input);
