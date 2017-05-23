de3c8bc7a4bf28fd1407e4a6cc52894d03c7ed63
Abhishek Lodha <alodha21051992@gmail.com>
2017-05-23 15:54:48 +0530

    added state and trainer filter to all sql queries

--- training/management/databases/get_sql_queries.py
+++ training/management/databases/get_sql_queries.py
@@ -35,10 +35,10 @@ def get_training_data_sql(**Kwargs):
         sql_ds['where'].append('tt.date between \'' + start_date + '\' and \'' + end_date + '\'')
 
     sql_q = join_sql_ds(sql_ds)
-    args_dict['query_tag'] = 'No. of Trainings'
+    # args_dict['query_tag'] = 'No. of Trainings'
     args_dict['component'] = 'recentBar'
     args_dict['query_string'] = sql_q
-    args_dict['apply_filter'] = True
+    # args_dict['apply_filter'] = True
     args_list.append(args_dict.copy())
 
     return args_list
@@ -53,27 +53,33 @@ def get_mediators_data_sql(**Kwargs):
     sql_ds = get_init_sql_ds()
     sql_ds['select'].append('count(distinct ts.participant_id)')
     sql_ds['from'].append('training_score ts')
-    # sql_ds['join'].append(['training_training_participants ttps', 'ts.training_id = ttps.training_id'])
     sql_ds['where'].append('ts.score in (0, 1)')
     sql_q = join_sql_ds(sql_ds)
     args_dict['query_tag'] = 'No. of Mediators'
     args_dict['component'] = 'overallBar'
     args_dict['query_string'] = sql_q
     args_dict['apply_filter'] = apply_filter
-    if(args_dict['apply_filter'] is False) :
+    if args_dict['apply_filter'] is False :
         args_list.append(args_dict.copy())
 
-    args_dict = {}
-    sql_ds = get_init_sql_ds()
-    sql_ds['select'].append('count(distinct ts.participant_id)')
-    sql_ds['from'].append('training_score ts')
-    sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id  and ' + 'tt.date between \'' + start_date + '\' and \'' + end_date + '\''])
-    sql_ds['where'].append('ts.score in (0, 1)')
+
+    if apply_filter:
+        sql_ds['where'].append('tt.date between \'' + start_date + '\' and \'' + end_date + '\'')
+        sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id'])
+        if len(trainers_list) > 0:
+            sql_ds['join'].append(['training_training_trainer ttt','ttt.training_id = tt.id'])
+            sql_ds['where'].append('ttt.trainer_id in (' + ",".join(trainers_list) + ")")
+        if len(states_list) > 0:
+            sql_ds['join'].append(['people_animator pa', 'pa.id = ts.participant_id'])
+            sql_ds['join'].append(['geographies_district gd','pa.district_id = gd.id'])
+            sql_ds['join'].append(['geographies_state gs','gs.id = gd.state_id'])
+            sql_ds['where'].append('gs.id in (' + ",".join(states_list) + ')')
+
     sql_q = join_sql_ds(sql_ds)
-    args_dict['query_tag'] = 'No. of Mediators'
+    # args_dict['query_tag'] = 'No. of Mediators'
     args_dict['component'] = 'recentBar'
     args_dict['query_string'] = sql_q
-    args_dict['apply_filter'] = True
+    # args_dict['apply_filter'] = True
     args_list.append(args_dict)
 
     return args_list
@@ -86,41 +92,47 @@ def get_pass_perc_data_sql(**Kwargs):
 
     # Pass_percentage
     args_dict = {}
-    sql_ds = get_init_sql_ds()
-    sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
-    sql_ds['from'].append('training_score ts')
-    sql_ds['where'].append('ts.score in (0, 1)')
-    sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
-    sql_q = join_sql_ds(sql_ds)
+    # Nested Query
+    inner_sql_ds = get_init_sql_ds()
+    inner_sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
+    inner_sql_ds['from'].append('training_score ts')
+    inner_sql_ds['where'].append('ts.score in (0, 1)')
+    inner_sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
+    inner_sql_q = join_sql_ds(inner_sql_ds)
 
     sql_ds = get_init_sql_ds()
     sql_ds['select'].append('cast(round((COUNT(CASE WHEN (T.sum_score / T.score_count) >= 0.7 then 1 ELSE NULL END) / count(*))*100, 2) as char(10))')
-    sql_ds['from'].append('(' + sql_q + ') T')
+    sql_ds['from'].append('(' + inner_sql_q + ') T')
     sql_q = join_sql_ds(sql_ds)
     args_dict['query_tag'] = 'Pass Percentage'
     args_dict['component'] = 'overallBar'
     args_dict['query_string'] = sql_q
     args_dict['apply_filter'] = apply_filter
-    if(args_dict['apply_filter'] is False) :
-        args_list.append(args_dict)
+    if args_dict['apply_filter'] is False :
+        args_list.append(args_dict.copy())
 
-    args_dict = {}
-    sql_ds = get_init_sql_ds()
-    sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
-    sql_ds['from'].append('training_score ts')
-    sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id  and ' + 'tt.date between \'' + start_date + '\' and \'' + end_date + '\''])
-    sql_ds['where'].append('ts.score in (0, 1)')
-    sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
-    sql_q = join_sql_ds(sql_ds)
+    if apply_filter:
+        inner_sql_ds['where'].append('tt.date between \'' + start_date + '\' and \'' + end_date + '\'')
+        inner_sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id'])
+        if len(trainers_list) > 0:
+            inner_sql_ds['join'].append(['training_training_trainer ttt','ttt.training_id = tt.id'])
+            inner_sql_ds['where'].append('ttt.trainer_id in (' + ",".join(trainers_list) + ")")
+        if len(states_list) > 0:
+            inner_sql_ds['join'].append(['people_animator pa', 'pa.id = ts.participant_id'])
+            inner_sql_ds['join'].append(['geographies_district gd','pa.district_id = gd.id'])
+            inner_sql_ds['join'].append(['geographies_state gs','gs.id = gd.state_id'])
+            inner_sql_ds['where'].append('gs.id in (' + ",".join(states_list) + ')')
+
+    inner_sql_q = join_sql_ds(inner_sql_ds)
 
     sql_ds = get_init_sql_ds()
     sql_ds['select'].append('cast(round((COUNT(CASE WHEN (T.sum_score / T.score_count) >= 0.7 then 1 ELSE NULL END) / count(*))*100, 2) as char(10))')
-    sql_ds['from'].append('(' + sql_q + ') T')
+    sql_ds['from'].append('(' + inner_sql_q + ') T')
     sql_q = join_sql_ds(sql_ds)
-    args_dict['query_tag'] = 'Pass Percentage'
+    # args_dict['query_tag'] = 'Pass Percentage'
     args_dict['component'] = 'recentBar'
     args_dict['query_string'] = sql_q
-    args_dict['apply_filter'] = True
+    # args_dict['apply_filter'] = True
     args_list.append(args_dict)
 
     return args_list
@@ -130,43 +142,47 @@ def get_avg_score_data_sql(**Kwargs):
     sql_query_list = []
     args_list = []
 
-        # Avg Score
+    # Avg Score
     args_dict = {}
-    sql_ds = get_init_sql_ds()
-    sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
-    sql_ds['from'].append('training_score ts')
-    sql_ds['where'].append('ts.score in (0, 1)')
-    sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
-    sql_q = join_sql_ds(sql_ds)
+    inner_sql_ds = get_init_sql_ds()
+    inner_sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
+    inner_sql_ds['from'].append('training_score ts')
+    inner_sql_ds['where'].append('ts.score in (0, 1)')
+    inner_sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
+    inner_sql_q = join_sql_ds(inner_sql_ds)
 
     sql_ds = get_init_sql_ds()
     sql_ds['select'].append('cast(round(avg(T.sum_score), 2) as char(10))')
-    sql_ds['from'].append('(' + sql_q + ') T')
+    sql_ds['from'].append('(' + inner_sql_q + ') T')
     sql_q = join_sql_ds(sql_ds)
     args_dict['query_tag'] = 'Avg Score'
     args_dict['component'] = 'overallBar'
     args_dict['query_string'] = sql_q
     args_dict['apply_filter'] = apply_filter
-    if(args_dict['apply_filter'] is False) :
-        args_list.append(args_dict)
+    if args_dict['apply_filter'] is False :
+        args_list.append(args_dict.copy())
 
-    args_dict = {}
-    sql_ds = get_init_sql_ds()
-    sql_ds['select'].append('ts.participant_id, (sum(ts.score)) sum_score,count(ts.score) score_count')
-    sql_ds['from'].append('training_score ts')
-    sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id  and ' + 'tt.date between \'' + start_date + '\' and \'' + end_date + '\''])
-    sql_ds['where'].append('ts.score in (0, 1)')
-    sql_ds['group by'].append('ts.participant_id') #check group by training_id is required or not
-    sql_q = join_sql_ds(sql_ds)
+    if apply_filter:
+        inner_sql_ds['where'].append('tt.date between \'' + start_date + '\' and \'' + end_date + '\'')
+        inner_sql_ds['join'].append(['training_training tt', 'ts.training_id = tt.id'])
+        if len(trainers_list) > 0:
+            inner_sql_ds['join'].append(['training_training_trainer ttt','ttt.training_id = tt.id'])
+            inner_sql_ds['where'].append('ttt.trainer_id in (' + ",".join(trainers_list) + ")")
+        if len(states_list) > 0:
+            inner_sql_ds['join'].append(['people_animator pa', 'pa.id = ts.participant_id'])
+            inner_sql_ds['join'].append(['geographies_district gd','pa.district_id = gd.id'])
+            inner_sql_ds['join'].append(['geographies_state gs','gs.id = gd.state_id'])
+            inner_sql_ds['where'].append('gs.id in (' + ",".join(states_list) + ')')
+    inner_sql_q = join_sql_ds(inner_sql_ds)
 
     sql_ds = get_init_sql_ds()
     sql_ds['select'].append('cast(round(avg(T.sum_score), 2) as char(10))')
-    sql_ds['from'].append('(' + sql_q + ') T')
+    sql_ds['from'].append('(' + inner_sql_q + ') T')
     sql_q = join_sql_ds(sql_ds)
-    args_dict['query_tag'] = 'Avg Score'
+    # args_dict['query_tag'] = 'Avg Score'
     args_dict['component'] = 'recentBar'
     args_dict['query_string'] = sql_q
-    args_dict['apply_filter'] = True
+    # args_dict['apply_filter'] = True
     args_list.append(args_dict)
 
     return args_list
