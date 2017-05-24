36e3caf1cf1e0dcf791d5aace19d1be312acc183
Abhishek Lodha <alodha21051992@gmail.com>
2017-05-24 14:35:36 +0530

    added trainings graph in tab 3

--- dg/media/analytics/src/app/graphs/graphs.service.ts
+++ dg/media/analytics/src/app/graphs/graphs.service.ts
@@ -5,19 +5,19 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class GraphsService {
-  
-    private graphURL = 'http://localhost:8000/training/graph_data';
-    
-    constructor(private http: Http) { }
 
-    getData(chartType, chartName): Promise<any> {
-        return this.http.get(this.graphURL, {'params':{'chartType':chartType,'chartName':chartName}})
-            .toPromise()
-            .then(response => response.json())
-            .catch(this.handleError);
-    }
-    private handleError(error: any): any {
-        console.error('An error occurred', error);
-        return Promise.reject(error.message || error);
-    }
+  private graphURL = 'http://localhost:8000/training/graph_data';
+
+  constructor(private http: Http) { }
+
+  getData(chartType, chartName): Promise<any> {
+    return this.http.get(this.graphURL, { 'params': { 'chartType': chartType, 'chartName': chartName } })
+      .toPromise()
+      .then(response => response.json())
+      .catch(this.handleError);
+  }
+  private handleError(error: any): any {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
