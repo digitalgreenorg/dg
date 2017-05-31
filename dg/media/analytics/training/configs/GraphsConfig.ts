62f12748b7ade209b76463ed02f8f29195d3721c
Abhishek Chandran <abhishekchandran91@gmail.com>
2017-05-31 11:34:23 +0530

    congfigs made to dict to improve readability

--- dg/media/analytics/training/configs/GraphsConfig.ts
+++ dg/media/analytics/training/configs/GraphsConfig.ts
@@ -1,183 +1,143 @@
-export const chartsConfig = [
-  {
-    chart: {
-      type: 'column',
-      renderTo: 'column_tab_3',
-      tab: {
-          'id': 'tab1',
-          'class':'col-sm-6'
-      },
-      drillDown: true
-    },
-    credits: {
-        enabled: false
-    },
-    chartName: 'state__#trainings',
-    title: {
-      text: 'Trainings Conducted'
-    },
-    xAxis: { type: 'category' },
-    yAxis: {
-      title: {
-        text: 'Number of Trainings'
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      column: {
-        grouping: false,
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true
-        }
-      }
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
-    },
-    series: [],
-    drilldown: {
-      allowPointDrilldown: false,
-      series: []
-    }
+export const chartsConfig = {
+  'state__#trainings' : {
+                          chart: {
+                                  type: 'column',
+                                  renderTo: 'column_tab_3',
+                                  tab: {
+                                        'id': 'tab1',
+                                        'class':'col-sm-6'
+                                  },
+                                  drillDown: true
+                          },
+                          credits:{ enabled: false },
+                          //chartName: 'state__#trainings',
+                          title: { text: 'Trainings Conducted'},
+                          xAxis: { type: 'category' },
+                          yAxis: { title: { text: 'Number of Trainings' } },
+                          legend: { enabled: false },
+                          plotOptions: {
+                                        column: {
+                                                grouping: false,
+                                                borderWidth: 0,
+                                                dataLabels: {
+                                                             enabled: true
+                                                }
+                                        }
+                          },
+                          tooltip: {
+                                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
+                          },
+                          series: [],
+                          drilldown: {
+                                      allowPointDrilldown: false,
+                                      series: []
+                          }
   },
-  {
-    chart: {
-      type: 'column',
-      renderTo: 'column',
-      tab: {
-          'id': 'tab1',
-          'class':'col-sm-6'
-      },
-      drillDown: true
-    },
-    credits: {
-        enabled: false
-    },
-    chartName: 'state_district__#trainings',
-    title: {
-      text: 'Mediators trained '
-    },
-    subtitle: {
-      text: 'Click the columns to view state wise trainer figures.'
-    },
-    xAxis: { type: 'category' },
-    yAxis: {
-      title: {
-        text: 'Number of Mediators'
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      column: {
-        grouping: false,
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true
-        }
-      }
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
-    },
-    series: [],
-    drilldown: {
-      allowPointDrilldown: false,
-      series: []
-    }
+  'state_district__#trainings':{
+                                chart: {
+                                        type: 'column',
+                                        renderTo: 'column',
+                                        tab: {
+                                            'id': 'tab1',
+                                            'class':'col-sm-6'
+                                        },
+                                        drillDown: true
+                                },
+                                credits: { enabled: false },
+                                //chartName: 'state_district__#trainings',
+                                title: { text: 'Mediators trained'},
+                                subtitle: { text: 'Click the columns to view state wise trainer figures.'},
+                                xAxis: { type: 'category' },
+                                yAxis: { title: { text: 'Number of Mediators' } },
+                                legend: { enabled: false },
+                                plotOptions: {
+                                              column: {
+                                                      grouping: false,
+                                                      borderWidth: 0,
+                                                      dataLabels: {
+                                                        enabled: true
+                                                      }
+                                              }
+                                },
+                                tooltip: {
+                                          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                                          pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
+                                },
+                                series: [],
+                                drilldown: {
+                                            allowPointDrilldown: false,
+                                            series: []
+                                }
   },
-  {
-    chart: {
-      type: 'column',
-      renderTo: 'graph_3',
-      tab: {
-          'id': 'tab2',
-          'class':'col-sm-12'
-      },
-      drillDown: false
-    },
-    credits: {
-        enabled: false
-    },
-    chartName: 'question_wise_data',
-    title: {
-      text: 'Questions Answered Correctly'
-    },
-    xAxis: { type: 'category' },
-    yAxis: {
-      title: {
-        text: 'Percentage Answered'
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      column: {
-        grouping: false,
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true
-        }
-      }
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
-    },
-    series: [],
-    drilldown: {}
+  'question_wise_data':{
+                        chart: {
+                                type: 'column',
+                                renderTo: 'graph_3',
+                                tab: {
+                                    'id': 'tab2',
+                                    'class':'col-sm-12'
+                                },
+                                drillDown: false
+                        },
+                        credits: { enabled: false },
+                        //chartName: 'question_wise_data',
+                        title: {
+                                text: 'Questions Answered Correctly'
+                        },
+                        xAxis: { type: 'category' },
+                        yAxis: { title: { text: 'Percentage Answered'} },
+                        legend: { enabled: false },
+                        plotOptions: {
+                                      column: {
+                                              grouping: false,
+                                              borderWidth: 0,
+                                              dataLabels: {
+                                                enabled: true
+                                              }
+                                      }
+                        },
+                        tooltip: {
+                                  headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                                  pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
+                        },
+                        series: [],
+                        drilldown: {}
   },
-  {
-    chart: {
-      type: 'column',
-      renderTo: 'graph_4',
-      tab: {
-          'id': 'tab3',
-          'class':'col-sm-12'
-      },
-      drillDown: true
-    },
-    credits: {
-        enabled: false
-    },
-    chartName: 'year_month_wise_data',
-    title: {
-      text: 'Periodical Trainings Conducted'
-    },
-    xAxis: { type: 'category' },
-    yAxis: {
-      title: {
-        text: 'Number of Trainings'
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      column: {
-        grouping: false,
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true
-        }
-      }
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
-    },
-    series: [],
-    drilldown: {
-      allowPointDrilldown: false,
-      series: []
-    }
-  },
+  'year_month_wise_data':{
+                          chart: {
+                                  type: 'column',
+                                  renderTo: 'graph_4',
+                                  tab: {
+                                      'id': 'tab3',
+                                      'class':'col-sm-12'
+                                  },
+                                  drillDown: true
+                          },
+                          credits: { enabled: false },
+                          //chartName: 'year_month_wise_data',
+                          title: { text: 'Periodical Trainings Conducted' },
+                          xAxis: { type: 'category' },
+                          yAxis: { title: { text: 'Number of Trainings'} },
+                          legend: { enabled: false },
+                          plotOptions: {
+                                        column: {
+                                                grouping: false,
+                                                borderWidth: 0,
+                                                dataLabels: {
+                                                  enabled: true
+                                                }
+                                        }
+                          },
+                          tooltip: {
+                                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
+                          },
+                          series: [],
+                          drilldown: {
+                            allowPointDrilldown: false,
+                            series: []
+                          }
+                        },
   
-]
+}
