92aa7f98824b5495d517234484a8c926e9d02569
sainivikas <vikassaini480@gmail.com>
2017-06-05 11:59:03 +0530

    bug fixed

--- loop/management/commands/helpline_summary.py
+++ loop/management/commands/helpline_summary.py
@@ -163,7 +163,7 @@ Total number of repeat caller: %s<br/>Total Calls from repeat callers: %s<br/>\
             elif from_date > to_date:
                 print 'From date is greater than current date'
                 return
-            email_subject = 'Loop helpline Summary from %s to %s'%(from_date.strftime("%Y-%m-%d"),to_date.strftime("%Y-%m-%d"))
+            email_subject = 'Loop helpline Summary from %s to %s'%(from_date,to_date)
             summary_data = self.helpline_summary(from_date,to_date,1)
             self.send_mail(summary_data,email_subject)
         else:
