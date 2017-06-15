db4a5eebdb7fef096cc35079d5588d81e7a3952b
Abhishek Chandran <abhishekchandran91@gmail.com>
2017-06-15 17:22:53 +0530

    sub navs render fixed

--- dg/media/analytics/src/app/navs/navs.component.ts
+++ dg/media/analytics/src/app/navs/navs.component.ts
@@ -9,34 +9,32 @@ import { environment } from '../../environments/environment.loop';
 
 export class NavsComponent implements OnInit {
   public isCollapsed:boolean = false;
-  navsFirst = [];
+  navsConfig = environment.navsConfig;
+  toggleNav = {};
   navsSecond = [];
   overall : false;
   recent : false;
-  navsConfig = environment.navsConfig;
-  constructor() { }
+  
+  constructor() {}
 
   ngOnInit() {
     Object.keys(this.navsConfig).forEach(navOne => {
-      this.navsFirst.push({
-        'name': navOne,
-        'data-target': '#'+navOne
-      });
-      let subNav = []
-      Object.keys(this.navsConfig[navOne]).forEach(navTwo => {
-        subNav.push(navTwo);
-      });
-      this.navsSecond.push({
-          'id': navOne,
-          'name':subNav
-        })
+      let tempDict = {}
+      tempDict['status'] = false;
+      tempDict['subNav'] = Object.keys(this.navsConfig[navOne])
+      this.toggleNav[navOne] = tempDict;
+    })
+  }
+
+  toggleNavKeys() {
+    return Object.keys(this.toggleNav)
+  }
+
+  setNav(selectedItem : string) {
+    Object.keys(this.toggleNav).forEach(nav => {
+      this.toggleNav[nav].status = false
     });
+    this.toggleNav[selectedItem].status = true;
+    this.navsSecond = this.toggleNav[selectedItem].subNav;
   }
-
-  ngAfterViewInit() {
-    console.log(this.navsFirst);
-    console.log(this.navsSecond);
-  }
-  
-
 }
