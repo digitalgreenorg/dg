84520f6ca2032e241c2f36279e0c2358bbd565e7
sainivikas <vikassaini480@gmail.com>
2017-06-19 15:01:22 +0530

    module seprated

--- loop_ivr/helper_function.py
+++ loop_ivr/helper_function.py
@@ -1,6 +1,8 @@
 __author__ = 'Vikas Saini'
 
 from django.db import connection
+from django.db.models import get_model
+from loop.models import Crop, Mandi
 from loop.utils.ivr_helpline.helpline_data import HELPLINE_LOG_FILE
 from loop.helpline_view import send_helpline_sms
 
@@ -25,5 +27,30 @@ def send_info(to_number, content):
         send_helpline_sms(from_number, to_number, content[index,index+1998])
         index += 1998
 
-def get_price_info(crop_id,mandi_id):
-    pass
+def get_price_info(from_number, crop_list, mandi_list):
+    price_info_list = []
+    crop_map = dict()
+    mandi_map = dict()
+    all_crop = Crop.objects.filter(id__in=crop_list).values('id', 'crop_name')
+    all_mandi = Mandi.objects.filter(id__in=mandi_list).values('id', 'mandi_name')
+    for crop in all_crop:
+        crop_map[crop['id']] = crop['crop_name']
+    for mandi in all_mandi:
+        mandi_map[mandi['id']] = mandi['mandi_name']
+    last_three_trans = raw_sql.last_three_trans%(','.join(map(str,crop_list)),','.join(map(str,mandi_list)))
+    query_result = run_query(last_three_trans)
+    pre_mandi = -1
+    pre_crop = -1
+    for row in query_results:
+        if (pre_crop != row[0]) or (pre_mandi != row[1]):
+            temp_str = ('\n%s,%s\n')%(crop_map[row[0]],mandi_map[row[1]])
+            price_info_list.append(temp_str)
+            pre_crop, pre_mandi = row[0], row[1]
+        date, min_price, max_price, mean = row[2], row[3], row[4], row[5]
+        if max_price-min_price >= 2:
+            min_price = mean-1
+            max_price = mean+1
+        temp_str = ('%s: %s to %s\n')%(date.strftime('%d-%m-%Y'),str(min_price),str(max_price))
+        price_info_list.append(temp_str)
+    final_result = ''.join(price_info_list)
+    send_info(from_number, final_result)
--- loop_ivr/views.py
+++ loop_ivr/views.py
@@ -3,9 +3,12 @@ __author__ = 'Vikas Saini'
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from threading import Thread
+
 from loop_ivr.models import PriceInfoIncoming, PriceInfoLog
 from loop_ivr.utils.marketinfo import raw_sql
-from loop_ivr.helper_function import get_valid_list, run_query, send_info
+from loop_ivr.helper_function import get_valid_list, send_info, get_price_info
+
 from loop.utils.ivr_helpline.helpline_data import HELPLINE_LOG_FILE
 from loop.helpline_view import fetch_info_of_incoming_call
 
@@ -49,36 +52,14 @@ def mandi_info(request):
         mandi_list = get_valid_list('loop', 'mandi', mandis_info)
         if not crop_list:
             final_result = 'Please correct crop list'
-            send_info(from_number, final_result)
+            print "crop nhi mili"
+            #send_info(from_number, final_result)
             return HttpResponse(status=200)
         if not mandi_list:
             final_result = 'Please correct mandi list'
+            print "mandi nhi mili"
+            #send_info(from_number, final_result)
             return HttpResponse(status=200)
-        price_info_list = []
-        crop_map = dict()
-        mandi_map = dict()
-        all_crop = Crop.objects.filter(id__in=crop_list).values('id', 'crop_name')
-        all_mandi = Mandi.objects.filter(id__in=mandi_list).values('id', 'mandi_name')
-        for crop in all_crop:
-            crop_map[crop['id']] = crop['crop_name']
-        for mandi in all_mandi:
-            mandi_map[mandi['id']] = mandi['mandi_name']
-        last_three_trans = raw_sql.last_three_trans%(','.join(map(str,crop_list)),','.join(map(str,mandi_list)))
-        query_result = run_query(last_three_trans)
-        pre_mandi = -1
-        pre_crop = -1
-        for row in query_results:
-        	if (pre_crop != row[0]) or (pre_mandi != row[1]):
-        		temp_str = ('\n%s,%s\n')%(crop_map[row[0]],mandi_map[row[1]])
-        		price_info_list.append(temp_str)
-        		pre_crop, pre_mandi = row[0], row[1]
-        	date, min_price, max_price, mean = row[2], row[3], row[4], row[5]
-        	if max_price-min_price >= 2:
-        		min_price = mean-1
-        		max_price = mean+1
-        	temp_str = ('%s: %s to %s\n')%(date.strftime('%d-%m-%Y'),str(min_price),str(max_price))
-        	price_info_list.append(temp_str)
-        final_result = ''.join(price_info_list)
-        send_info(from_number, final_result)
+        #Thread(target=get_price_info, args=[from_number, crop_list, mandi_list]).start()
         return HttpResponse(status=200)
     return HttpResponse(status=403)
