5fe765d1914a87f630720c04ed68a393eb612677
sainivikas <vikassaini480@gmail.com>
2017-06-19 14:23:03 +0530

    admin added

--- loop_ivr/admin.py
+++ loop_ivr/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin import SimpleListFilter
 
-# Register your models here.
+from loop_ivr.models import PriceInfoIncoming, PriceInfoLog
+
+class Loop_ivrAdmin(AdminSite):
+
+    def has_permission(self, request):
+        return request.user.is_active
+
+class PriceInfoIncomingAdmin(admin.ModelAdmin):
+    list_filter = ['info_status']
+
+loop_ivr_admin = Loop_ivrAdmin(name='loop_ivr_admin')
+loop_ivr_admin.register(PriceInfoIncoming, PriceInfoIncomingAdmin)
+loop_ivr_admin.register(PriceInfoLog)
