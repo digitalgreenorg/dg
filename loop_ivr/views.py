175f4ec623fcb59ceed1d100ded881893f2a65ff
sainivikas <vikassaini480@gmail.com>
2017-06-20 15:50:35 +0530

    all crops and all mandi are not allowed together

--- loop_ivr/views.py
+++ loop_ivr/views.py
@@ -52,6 +52,12 @@ def mandi_info(request):
         crop_list, all_crop_flag = get_valid_list('loop', 'crop', crops_info)
         mandi_list, all_mandi_flag = get_valid_list('loop', 'mandi', mandis_info)
         price_info_incoming_obj.query_for_mandi = mandis_info
+        if all_crop_flag and all_mandi_flag:
+            final_result = 'कृपया सब्जी और मंडी की सूची ठीक करे.'
+            price_info_incoming_obj.info_status = 2
+            price_info_incoming_obj.save()
+            send_info(from_number, final_result)
+            return HttpResponse(status=200)
         if not crop_list:
             final_result = 'कृपया सब्जी की सूची ठीक करे.'
             price_info_incoming_obj.info_status = 2
