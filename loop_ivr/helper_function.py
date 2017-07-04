52e07c348db6786b3378e864d5831e77c2d5b80e
sainivikas <vikassaini480@gmail.com>
2017-07-04 10:09:31 +0530

    seprate static messages

--- loop_ivr/helper_function.py
+++ loop_ivr/helper_function.py
@@ -14,7 +14,7 @@ from loop.models import Crop, Mandi, CropLanguage
 from loop.utils.ivr_helpline.helpline_data import SMS_REQUEST_URL
 
 from loop_ivr.utils.marketinfo import raw_sql
-from loop_ivr.utils.data import LOG_FILE, AGGREGATOR_SMS_NO
+from loop_ivr.utils.data import LOG_FILE, AGGREGATOR_SMS_NO, agg_sms_initial_line
 from loop_ivr.models import PriceInfoLog
 
 
@@ -67,12 +67,12 @@ def get_price_info(from_number, crop_list, mandi_list, price_info_incoming_obj, 
     for crop in crop_in_hindi:
         crop_in_hindi_map[crop['crop_id']] = crop['crop_name']
     # Fetching price from DB
-    price_info_list.append('लूप मंडी रेट\n')
+    price_info_list.append(agg_sms_initial_line)
     today_date = datetime.now()
     raw_query = raw_sql.last_three_trans.format('(%s)'%(crop_list[0],) if len(crop_list) == 1 else crop_list, '(%s)'%(mandi_list[0],) if len(mandi_list) == 1 else mandi_list, tuple((today_date-timedelta(days=day)).strftime('%Y-%m-%d') for day in range(0,3)))
     query_result = run_query(raw_query)
     if not query_result:
-        price_info_list.append("इस मंडी और फसल के लिए पिछले तीन दिन के रेट उपलब्ध नही है.")
+        price_info_list.append(agg_sms_no_price_available)
     else:
         crop_mandi_comb = []
         prev_crop, prev_mandi, crop_name, mandi_name = -1, -1, '', ''
@@ -84,15 +84,15 @@ def get_price_info(from_number, crop_list, mandi_list, price_info_incoming_obj, 
                 prev_crop, prev_mandi = crop, mandi
                 crop_name = crop_in_hindi_map.get(crop).encode("utf-8") if crop_in_hindi_map.get(crop) else crop_map[crop].encode("utf-8")
                 mandi_name = mandi_map[mandi].encode("utf-8")
-                temp_str = ('\n%s,%s मंडी\n')%(crop_name,mandi_name.rstrip('मंडी'))
+                temp_str = ('\n%s,%s %s\n')%(crop_name,mandi_name.rstrip(mandi_hi).rstrip(),mandi_hi)
                 price_info_list.append(temp_str)
             if max_price-min_price >= 2:
                 min_price = mean-1
                 max_price = mean+1
             if min_price != max_price:
-                temp_str = ('%s: रु %s-%s\n')%(date.strftime('%d-%m-%Y'),str(min_price),str(max_price))
+                temp_str = ('%s: %s %s-%s\n')%(date.strftime('%d-%m-%Y'),indian_rupee,str(min_price),str(max_price))
             else:
-                temp_str = ('%s: रु %s\n')%(date.strftime('%d-%m-%Y'),str(max_price))
+                temp_str = ('%s: %s %s\n')%(date.strftime('%d-%m-%Y'),indian_rupee,str(max_price))
             price_info_list.append(temp_str)
         if not all_crop_flag and not all_mandi_flag:
             for crop in crop_list:
@@ -100,9 +100,9 @@ def get_price_info(from_number, crop_list, mandi_list, price_info_incoming_obj, 
                     if (crop,mandi) not in crop_mandi_comb:
                         crop_name = crop_in_hindi_map.get(crop).encode("utf-8") if crop_in_hindi_map.get(crop) else crop_map[crop].encode("utf-8")
                         mandi_name = mandi_map[mandi].encode("utf-8")
-                        temp_str = ('\n%s,%s मंडी\n')%(crop_name,mandi_name.rstrip('मंडी'))
+                        temp_str = ('\n%s,%s %s\n')%(crop_name,mandi_name.rstrip(mandi_hi).rstrip(),mandi_hi)
                         price_info_list.append(temp_str)
-                        price_info_list.append('रेट उपलब्ध नही है\n')
+                        price_info_list.append(agg_sms_no_price_for_combination)
     final_result = ''.join(price_info_list)
     price_info_incoming_obj.price_result = final_result
     if len(final_result) >= 2000:
