d0f244e8175ff566c9f89039f8f4d4a6aa4302a3
Abhishek Lodha <alodha21051992@gmail.com>
2017-07-04 17:40:46 +0530

    added graph for drop down in config for time series

--- dg/media/analytics/loop/configs/NavsConfig.ts
+++ dg/media/analytics/loop/configs/NavsConfig.ts
@@ -1,103 +1,107 @@
 export const navsConfig = {
-    'navs':{
-        'Home':{
-            'active':true,
-            'import':{
-                'overall':false,
-                'recent':false,
-            },
-            'containers':{
-                'container1':{
-                    'Volume and Famers Cummulative Count':{
-                        'addDivs':['cummulativeCount']
-                    },
-                }
-            } 
-        },
-        'Analytics':{
-            'subNavs':{
-                'Aggregators':{
-                    'active':true,
-                    'containers': {
-                        'container1':{
-                            'Volume':{
-                                'addDivs':['aggrvol']
-                            },
-                            'Visits':{
-                                'addDivs':['aggrvisit']
-                            }
-                        },
-                        'container2':{
-                            'SPK/CPk':{
-                                'addDivs':['aggrspkcpk']
-                            },
-                            'Recovered/Total':{
-                                'addDivs':['aggrrecoveredtotal']
-                            }
-                        },
-                        'container3':{
-                            'Farmer Count':{
-                                'addDivs':['aggrfarmercount']
-                            }
-                        }
-                    }
-                },
-                'Mandi':{
-                    'containers':{
-                        'container1':{
-                            'Volume':{
-                                'addDivs':['mandivolume']
-                            },
-                            'Visits':{
-                                'addDivs':['mandivisit']
-                            }
-                        },
-                        'container2':{
-                            'SPK/CPk':{
-                                'addDivs':['mandispkcpk']
-                            },
-                            'Recovered/Total':{
-                                'addDivs':['mandirecoveredtotal']
-                            }
-                        },
-                        'container3':{
-                            'Farmer Count':{
-                                'addDivs':['mandifarmercount']
-                            }
-                        }
-                    }
-                },
-                'Crop':{
-                    'containers':{
-                        'container1':{
-                            'Volume':{
-                                'addDivs':['cropvolume']
-                            },
-                            'Amount':{
-                                'addDivs':['cropamount']
-                            }
-                        },
-                        'container2':{
-                            'Crop Prices':{
-                                'addDivs':['cropprices']
-                            }
-                        },
-                        'container3':{
-                            'Farmer Count':{
-                                'addDivs':['cropfarmercount']
-                            }
-                        }
-                    }
-                },
-            }
-        },
-        'Time Series': {
-            'containers':{
-                'container1':{
-                    'test3':{},
-                    'test4':{}
-                }
-            }   
+  'navs': {
+    'Home': {
+      'active': true,
+      'import': {
+        'overall': false,
+        'recent': false,
+      },
+      'containers': {
+        'container1': {
+          'Volume and Famers Cummulative Count': {
+            'addDivs': ['cummulativeCount']
+          },
         }
+      }
     },
+    'Analytics': {
+      'subNavs': {
+        'Aggregators': {
+          'active': true,
+          'containers': {
+            'container1': {
+              'Volume': {
+                'addDivs': ['aggrvol']
+              },
+              'Visits': {
+                'addDivs': ['aggrvisit']
+              }
+            },
+            'container2': {
+              'SPK/CPk': {
+                'addDivs': ['aggrspkcpk']
+              },
+              'Recovered/Total': {
+                'addDivs': ['aggrrecoveredtotal']
+              }
+            },
+            'container3': {
+              'Farmer Count': {
+                'addDivs': ['aggrfarmercount']
+              }
+            }
+          }
+        },
+        'Mandi': {
+          'containers': {
+            'container1': {
+              'Volume': {
+                'addDivs': ['mandivolume']
+              },
+              'Visits': {
+                'addDivs': ['mandivisit']
+              }
+            },
+            'container2': {
+              'SPK/CPk': {
+                'addDivs': ['mandispkcpk']
+              },
+              'Recovered/Total': {
+                'addDivs': ['mandirecoveredtotal']
+              }
+            },
+            'container3': {
+              'Farmer Count': {
+                'addDivs': ['mandifarmercount']
+              }
+            }
+          }
+        },
+        'Crop': {
+          'containers': {
+            'container1': {
+              'Volume': {
+                'addDivs': ['cropvolume']
+              },
+              'Amount': {
+                'addDivs': ['cropamount']
+              }
+            },
+            'container2': {
+              'Crop Prices': {
+                'addDivs': ['cropprices']
+              }
+            },
+            'container3': {
+              'Farmer Count': {
+                'addDivs': ['cropfarmercount']
+              }
+            }
+          }
+        },
+      }
+    },
+    'Time Series': {
+      'DropDownGraph': {
+        'container1': {
+          'Volume Farmer': {
+            'addDivs': ['volFarmerTS']
+          },
+          'SPK CPK': {
+            'addDivs': ['spkcpkTS']
+          }
+        },
+      }
+    }
+  },
 }
