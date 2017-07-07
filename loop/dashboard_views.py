66b0c8fa379af29c1f0c9daa69336c925ad3b373
Abhishek Lodha <alodha21051992@gmail.com>
2017-07-07 15:49:56 +0530

    added dashboard views file to return data for loop dashboard

---
+++ loop/dashboard_views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Count, Min, Sum, Avg, Max, F
+
+import pandas as pd
+from loop.models import CombinedTransaction, Farmer, Crop, Mandi, Gaddidar, LoopUser
+from loop.utils.loop_etl.group_myisam_data import *
+
+def volume_aggregator(request):
+    volume_per_aggregator = get_volume_aggregator(1)
+    chart_dict = { "volume_per_aggregator": volume_per_aggregator }
+    return JsonResponse(chart_dict)
