2792bf1f300f2aec272b611fe0a06622fceb47f3
Abhishek Lodha <alodha21051992@gmail.com>
2017-07-13 16:47:16 +0530

    sending data for time series vol_amoutn graph

--- loop/dashboard_views.py
+++ loop/dashboard_views.py
@@ -10,3 +10,7 @@ from loop.utils.loop_etl.group_myisam_data import *
 def volume_aggregator(request):
     volume_per_aggregator = get_volume_aggregator(1)
     return JsonResponse(volume_per_aggregator)
+
+def vol_amount_farmer(request):
+    v_a_f_ts = volume_amount_farmers_ts(1,'20170401','20170601')
+    return HttpResponse(v_a_f_ts)
