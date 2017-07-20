5c2bb428c80df508c1cbbca52050b83e6cbf89ae
Abhishek Lodha <alodha21051992@gmail.com>
2017-07-20 14:14:48 +0530

    applying class from graph config

--- dg/media/analytics/loop/configs/GraphsConfig.ts
+++ dg/media/analytics/loop/configs/GraphsConfig.ts
@@ -3,7 +3,10 @@ export const chartsConfig = {
     chart: {
       type: 'column',
       renderTo: 'cummulativeCount',
-      drillDown: true
+      drillDown: true,
+      tab: {
+        'class': 'col-sm-12'
+      },
     },
     credits: { enabled: false },
     title: { text: '' },
@@ -30,18 +33,18 @@ export const chartsConfig = {
       name: 'Installation',
       data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
     }, {
-      name: 'Manufacturing',
-      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-      name: 'Sales & Distribution',
-      data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-      name: 'Project Development',
-      data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-      name: 'Other',
-      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }],
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+      }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+      }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+      }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+      }],
     drilldown: {}
   },
   'volFarmerTS': {
@@ -49,6 +52,9 @@ export const chartsConfig = {
       type: "areaspline",
       renderTo: 'volFarmerTS',
       drilldown: false,
+      tab: {
+        'class': 'col-sm-6'
+      },
     },
     rangeSelector: {
       selected: 0
@@ -79,6 +85,9 @@ export const chartsConfig = {
       type: "areaspline",
       renderTo: 'cpkSpkTS',
       drilldown: false,
+      tab: {
+        'class': 'col-sm-6'
+      },
     },
     rangeSelector: {
       selected: 0
