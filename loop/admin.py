e2422aed2f97912cc2e320a76cc0276eff009692
sainivikas <vikassaini480@gmail.com>
2017-07-31 10:21:08 +0530

    changes in  admin of combined transactions

--- loop/admin.py
+++ loop/admin.py
@@ -69,7 +69,7 @@ class FarmerAdmin(admin.ModelAdmin):
 class CombinedTransactionAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', '__mandi__','__gaddidar__', '__aggregator__', '__farmer__', '__crop__', 'price',
                     'quantity', 'amount', 'status')
-    search_fields = ['farmer__name', 'farmer__village__village_name',
+    search_fields = ['farmer__name', 'farmer__village__village_name', 'gaddidar__gaddidar_name',
                      'user_created__username', 'crop__crop_name', 'mandi__mandi_name', 'status']
     list_filter = (UserListFilter,'status',
                    'crop__crop_name', 'mandi__mandi_name','gaddidar__gaddidar_name', 'farmer__village__village_name', 'mandi__district__state__country')
