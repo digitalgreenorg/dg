ab91c43c6ed6e79a677af68f43f8cbb3be92d510
sainivikas <vikassaini480@gmail.com>
2017-08-03 13:41:14 +0530

    migration added for loop_ivr

--- loop_ivr/migrations/0002_auto_20170803_1340.py
+++ loop_ivr/migrations/0002_auto_20170803_1340.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('loop_ivr', '0002_auto_20170803_1019'),
+        ('loop_ivr', '0001_initial'),
     ]
 
     operations = [
@@ -20,5 +20,10 @@ class Migration(migrations.Migration):
             model_name='priceinfoincoming',
             name='prev_query_code',
             field=models.CharField(max_length=120, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='priceinfoincoming',
+            name='info_status',
+            field=models.IntegerField(default=0, db_index=True, choices=[(0, b'Pending'), (1, b'Done'), (2, b'Wrong Query'), (3, b'No Input')]),
         ),
     ]
