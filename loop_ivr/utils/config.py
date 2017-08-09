7d0c84aa49a9a376ed9f88200f5c9b11c766281e
sainivikas <vikassaini480@gmail.com>
2017-08-09 11:09:06 +0530

    MARKET_INFO_CALL_RESPONSE_URL and MARKET_INFO_APP changed for production

--- loop_ivr/utils/config.py
+++ loop_ivr/utils/config.py
@@ -10,5 +10,5 @@ agg_sms_initial_line = 'लूप मंडी रेट\n'
 agg_sms_no_price_for_combination = 'रेट उपलब्ध नही है\n'
 agg_sms_no_price_available = 'इस मंडी और फसल के लिए पिछले तीन दिन के रेट उपलब्ध नही है.'
 
-MARKET_INFO_CALL_RESPONSE_URL = 'http://dgtest.org:2345/loopivr/market_info_response/'
-MARKET_INFO_APP = '140902'
+MARKET_INFO_CALL_RESPONSE_URL = 'http://www.digitalgreen.org/loopivr/market_info_response/'
+MARKET_INFO_APP = '141649'
