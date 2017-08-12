3cae001635738ee749099e2b4ee04097a9a54aa9
sainivikas <vikassaini480@gmail.com>
2017-08-12 18:47:46 +0530

    team contact excluded from matrices

--- loop_ivr/management/commands/push_sms_email.py
+++ loop_ivr/management/commands/push_sms_email.py
@@ -11,7 +11,7 @@ from loop_ivr.models import PriceInfoIncoming, SubscriptionLog
 
 from loop_ivr.utils.config import agg_sms_no_price_available
 
-from dg.settings import EMAIL_HOST_USER
+from dg.settings import EMAIL_HOST_USER, team_contact
 
 class Command(BaseCommand):
 
@@ -35,13 +35,13 @@ class Command(BaseCommand):
         today_date = datetime.now().date()
         yesterday_date = datetime.now().date()-timedelta(days=1)
         fifteen_day_back_date = datetime.now().date()-timedelta(days=16)
-        yesterday_call_count = PriceInfoIncoming.objects.filter(incoming_time__gte=yesterday_date,incoming_time__lt=today_date).count()
+        yesterday_call_count = PriceInfoIncoming.objects.filter(incoming_time__gte=yesterday_date,incoming_time__lt=today_date).exclude(from_number__in=team_contact).count()
         # excluding yesterday, because we are counting matrices for yeseterday.
-        last_fifteen_day_caller_no = list(PriceInfoIncoming.objects.filter(incoming_time__gte=fifteen_day_back_date,incoming_time__lt=yesterday_date).values_list('from_number',flat=True))
+        last_fifteen_day_caller_no = list(PriceInfoIncoming.objects.filter(incoming_time__gte=fifteen_day_back_date,incoming_time__lt=yesterday_date).exclude(from_number__in=team_contact).values_list('from_number',flat=True))
         # Active callers are who called in last fifteen days.
-        active_caller_count = PriceInfoIncoming.objects.filter(incoming_time__gte=yesterday_date,incoming_time__lt=today_date,from_number__in=last_fifteen_day_caller_no).count()
+        active_caller_count = PriceInfoIncoming.objects.filter(incoming_time__gte=yesterday_date,incoming_time__lt=today_date,from_number__in=last_fifteen_day_caller_no).exclude(from_number__in=team_contact).count()
         yesterday_call_count_with_market_rate = PriceInfoIncoming.objects.filter(~Q(price_result__contains=agg_sms_no_price_available),incoming_time__gte=yesterday_date,
-                                        incoming_time__lt=today_date,info_status=1).count()
+                                        incoming_time__lt=today_date,info_status=1).exclude(from_number__in=team_contact).count()
 
         if yesterday_call_count > 0:
             percent_calls_with_market_rate = round((yesterday_call_count_with_market_rate*100.0) / yesterday_call_count,2)
@@ -49,7 +49,7 @@ class Command(BaseCommand):
             percent_calls_with_market_rate = 0
         subscriber_no = list(SubscriptionLog.objects.filter(status=1).values_list('subscription__subscriber__phone_no',flat=True))
         yeseterday_non_subscriber_caller = PriceInfoIncoming.objects.filter(incoming_time__gte=yesterday_date,
-                                            incoming_time__lt=today_date).exclude(from_number__in=subscriber_no).count()
+                                            incoming_time__lt=today_date).exclude(from_number__in=subscriber_no).exclude(from_number__in=team_contact).count()
         email_subject = 'Loop Market Information Summary for %s'%(yesterday_date.strftime("%Y-%m-%d"),)
         self.send_mail(email_subject, yesterday_call_count, active_caller_count,
                         percent_calls_with_market_rate, yeseterday_non_subscriber_caller)
