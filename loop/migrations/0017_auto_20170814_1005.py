fd10379f284c119387b72cfded11229aff5ad6ed
sourabhsingh <sourabhsingh29@github.com>
2017-08-14 15:37:30 +0530

    migrations

--- loop/migrations/0017_auto_20170814_1005.py
+++ loop/migrations/0017_auto_20170814_1005.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('loop', '0022_auto_20170727_0543'),
+        ('loop', '0016_auto_20170628_0555'),
     ]
 
     operations = [
@@ -37,10 +37,75 @@ class Migration(migrations.Migration):
                 ('model_id', models.IntegerField(null=True)),
             ],
         ),
+        migrations.CreateModel(
+            name='VehicleLanguage',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('vehicle_name', models.CharField(max_length=30)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='adminuser',
+            name='village',
+            field=models.ForeignKey(default=None, to='loop.Village', null=True),
+        ),
+        migrations.AddField(
+            model_name='croplanguage',
+            name='measuring_unit',
+            field=models.CharField(default=b'kg', max_length=20),
+        ),
+        migrations.AddField(
+            model_name='gaddidar',
+            name='is_prime',
+            field=models.BooleanField(default=False),
+        ),
         migrations.AddField(
             model_name='language',
             name='notation',
             field=models.CharField(max_length=3, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='state',
+            name='phone_digit',
+            field=models.CharField(default=10, max_length=2, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='state',
+            name='phone_start',
+            field=models.CharField(default=789, max_length=15, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='adminuser',
+            name='assigned_loopusers',
+            field=models.ManyToManyField(to='loop.LoopUser', through='loop.AdminAssignedLoopUser'),
+        ),
+        migrations.RemoveField(
+            model_name='broadcast',
+            name='cluster',
+        ),
+        migrations.AddField(
+            model_name='broadcast',
+            name='cluster',
+            field=models.ManyToManyField(to='loop.LoopUser'),
+        ),
+        migrations.AlterField(
+            model_name='croplanguage',
+            name='crop',
+            field=models.ForeignKey(related_name='crops', to='loop.Crop'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='gaddidar',
+            unique_together=set([('gaddidar_phone', 'gaddidar_name', 'mandi')]),
+        ),
+        migrations.AddField(
+            model_name='vehiclelanguage',
+            name='language',
+            field=models.ForeignKey(to='loop.Language', null=True),
+        ),
+        migrations.AddField(
+            model_name='vehiclelanguage',
+            name='vehicle',
+            field=models.ForeignKey(related_name='vehicles', to='loop.Vehicle'),
         ),
         migrations.AddField(
             model_name='adminlog',
@@ -71,11 +136,5 @@ class Migration(migrations.Migration):
             model_name='adminassignedloopuser',
             name='user_modified',
             field=models.ForeignKey(related_name='loop_adminassignedloopuser_related_modified', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True),
-        ),
-        
-        migrations.AlterField(
-            model_name='adminuser',
-            name='assigned_loopusers',
-            field=models.ManyToManyField(to='loop.LoopUser', through='loop.AdminAssignedLoopUser'),
         ),
     ]
