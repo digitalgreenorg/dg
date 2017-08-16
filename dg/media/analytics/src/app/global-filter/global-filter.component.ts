b06732c9f65553b8286c2972d159f5ed90313593
sujit22993 <sujit22993@gmail.com>
2017-08-16 11:24:37 +0530

    country filter by-default india selected

--- dg/media/analytics/src/app/global-filter/global-filter.component.ts
+++ dg/media/analytics/src/app/global-filter/global-filter.component.ts
@@ -13,7 +13,7 @@ export class GlobalFilterComponent implements OnInit {
   Dropdownitems: GlobalFilter[] = [];
   constructor(private _globalfilter: GlobalFilterService, private _sharedService: SharedService,
   private _globalfiltersharedService : GlobalFilterSharedService) { }
-  private country:string = 'Country';
+  private country:string = '';
 
   ngOnInit() {
     this._globalfilter.getData().subscribe(data => {
@@ -21,8 +21,12 @@ export class GlobalFilterComponent implements OnInit {
         this.Dropdownitems.push(element);
         // console.log('global filter', Object.keys(global_filter).length);
 
-        if('country_id' in global_filter && element.id == global_filter['country_id']) {
-          this.country = element.value;
+        if('country_id' in global_filter) {
+          if(element.id == global_filter['country_id']) {
+            this.country = element.value;
+          }
+        } else {
+          this.country = this.Dropdownitems[0].value;
         }
       });
     });
