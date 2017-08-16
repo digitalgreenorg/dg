55814e37c115ded35e8dcc07fadda3bc3773861f
sainivikas <vikassaini480@gmail.com>
2017-08-16 11:41:23 +0530

    sms response changed when there is no rate available for query

--- loop_ivr/utils/config.py
+++ loop_ivr/utils/config.py
@@ -10,7 +10,11 @@ helpline_hi = 'हैल्पलाइन'
 agg_sms_initial_line = 'लूप मंडी रेट\n'
 agg_sms_crop_line = 'फसल'
 agg_sms_no_price_for_combination = 'रेट उपलब्ध नही है\n'
-agg_sms_no_price_available = 'इस मंडी और फसल के लिए पिछले तीन दिन के रेट उपलब्ध नही है.'
+agg_sms_no_price_available = 'इस मंडी और फसल के लिए पिछले तीन दिनो के रेट उपलब्ध नही है.'
+agg_sms_no_price_all_mandi = '%s का पिछले तीन दिनो के रेट उपलब्ध नही है.'
+agg_sms_no_price_crop_mandi = '%s का %s मंडी मे पिछले तीन दिनो के रेट उपलब्ध नही है.'
+# This is common string in no price messages.
+string_for_no_rate_query = 'पिछले तीन दिनो के रेट उपलब्ध नही है'
 MONTH_NAMES = {1:'जनवरी', 2:'फरवरी', 3:'मार्च', 4:'अप्रैल', 5:'मई', 6:'जून', 7:'जुलाई', 8:'अगस्त',
 				9:'सितंबर', 10:'अक्टूबर', 11:'नवंबर', 12:'दिसंबर'}
 
