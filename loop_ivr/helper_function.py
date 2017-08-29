b179c6450224b4a0b43f4d87d545ea1b2cbb5766
sainivikas <vikassaini480@gmail.com>
2017-08-29 16:48:06 +0530

    if first time caller then send crop code to them

--- loop_ivr/helper_function.py
+++ loop_ivr/helper_function.py
@@ -189,4 +189,7 @@ def get_price_info(from_number, crop_list, mandi_list, price_info_incoming_obj, 
         send_info(from_number, final_result)
     else:
         price_info_incoming_obj.save()
+    # If caller is calls first time then send crop code to them.
+    if not PriceInfoIncoming.objects.filter(from_number=from_number).exists():
+        send_sms(AGGREGATOR_SMS_NO, from_number, crop_and_code)
     PriceInfoLog.objects.bulk_create(price_info_log_list)
