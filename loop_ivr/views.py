1de93e45363d545eaf8f204d8cb443381b2ce6cb
sainivikas <vikassaini480@gmail.com>
2017-08-31 12:09:14 +0530

    variable imported from settings.py

--- loop_ivr/views.py
+++ loop_ivr/views.py
@@ -9,9 +9,11 @@ from django.views.decorators.csrf import csrf_exempt
 from threading import Thread
 import time
 
+from dg.settings import EXOTEL_HELPLINE_NUMBER
+
 from loop_ivr.models import PriceInfoIncoming, PriceInfoLog, SubscriptionLog
 from loop_ivr.helper_function import get_valid_list, send_info, get_price_info, make_market_info_call
-from loop_ivr.utils.config import LOG_FILE, call_failed_sms, crop_and_code, helpline_hi, EXOTEL_HELPLINE_NUMBER
+from loop_ivr.utils.config import LOG_FILE, call_failed_sms, crop_and_code, helpline_hi
 
 from loop.helpline_view import fetch_info_of_incoming_call, write_log
 
