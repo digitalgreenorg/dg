211875df22369032ea46c8f16b322cb145120999
Abhishek Lodha <alodha21051992@gmail.com>
2017-09-08 18:34:43 +0530

    using context processor to redirect to main website domain using setting variable

---
+++ dg/context_processors.py
@@ -0,0 +1,5 @@
+from django.conf import settings # import the settings file
+
+def admin_media(request):
+    # return the value you want as a dictionnary. you may add multiple values in there.
+    return {'WEBSITE_DOMAIN': settings.WEBSITE_DOMAIN}
