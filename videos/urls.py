11e4d1bef04043693a8c32b64ce3437898ebc743
sainivikas <vikassaini480@gmail.com>
2017-09-11 16:00:41 +0530

    URL of /analytics/video_search changed to /videos/search

--- videos/urls.py
+++ videos/urls.py
@@ -5,6 +5,8 @@ from django.views.generic.base import RedirectView
 from social_website.views import collection_view, video_view, search_view
 from social_website.urls import DirectTemplateView
 
+from output.views import video_analytics
+
 from dg.base_settings import VIDEOS_PAGE
 
 import videokheti.urls
@@ -17,5 +19,5 @@ urlpatterns = patterns('',
     url(r'^library/?$', search_view, name='search'),
     url(r'^library/$', DirectTemplateView.as_view(template_name='collections.html', extra_context={'header': {'jsController':'Collections', 'currentPage':'Discover', 'loggedIn':False}}), name='discover'),
     (r'^videokheti/', include(videokheti.urls)),
-
+    (r'^search/?$',video_analytics.video_search),
 )
