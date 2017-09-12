41a09f0d57bfa8ac526de7779f65da41902dd796
sainivikas <vikassaini480@gmail.com>
2017-09-12 11:33:25 +0530

    bug fixed dimagi admin URL

--- dimagi/urls.py
+++ dimagi/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from views import save_submission
 
-from mcoco_admin import mcoco_admin
+from dg.mcoco_admin import mcoco_admin
 
 urlpatterns = patterns('',
     url(r'^submission/$', save_submission),
