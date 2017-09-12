70813bd5796dc334e9b4e5ef80882c6b906a8351
sainivikas <vikassaini480@gmail.com>
2017-09-12 11:39:00 +0530

    redirect dimagi/ to product page of website

--- dimagi/urls.py
+++ dimagi/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url, include
 from views import save_submission
 
 from dg.mcoco_admin import mcoco_admin
+from dg.base_settings import PRODUCT_PAGE
 
 urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url=PRODUCT_PAGE)),
     url(r'^submission/$', save_submission),
     (r'^admin/', include(mcoco_admin.urls)),
 )
