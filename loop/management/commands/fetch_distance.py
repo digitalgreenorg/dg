362d9c793c0aa3340548cfc15b2d45851e1006e8
Abhishek Lodha <alodha21051992@gmail.com>
2017-10-24 15:36:47 +0530

    made panads dataframe for cross product of all amndis

--- loop/management/commands/fetch_distance.py
+++ loop/management/commands/fetch_distance.py
@@ -1,22 +1,35 @@
 import os
 import sys
 from django.core.management.base import BaseCommand, CommandError
-from loop.models import Mandi, Village
+import pandas as pd
 from libs.distance_matrix import DistanceMatrix
+from loop.models import Mandi, Village
 
 class Command(BaseCommand):
     help = '''This command fetches distance between source and destination. '''
 
     def handle(self,*args,**options):
-        mandi_list = Mandi.objects.prefetch_related()
-        source = str(mandi_list[0].latitude) + "," + str(mandi_list[0].longitude)
-        destination = str(mandi_list[1].latitude) + "," + str(mandi_list[1].longitude)
-        self.get_distance(source=source,destination=destination,mode='driving')
+        mandi_matrix = pd.DataFrame(list(Mandi.objects.values('id','mandi_name_en','latitude','longitude').filter(district__state=1)))
+        mandi_matrix['tmp']=1
+        mandi_matrix = mandi_matrix.merge(mandi_matrix,on='tmp', suffixes=('_src','_dest')).drop('tmp', axis=1)
+        # print mandi_matrix.head()
+        # mandi_matrix = mandi_matrix.head(n=10)
+        for index, item in mandi_matrix.iterrows():
+            print item['mandi_name_en_src'] + "  TO  " + item['mandi_name_en_dest'] + " : "
+            source = str(item['latitude_src']) + "," + str(item['longitude_src'])
+            destination = str(item['latitude_dest']) + "," + str(item['longitude_dest'])
+            mandi_matrix.loc[index,'distance'] = self.get_distance(source=source, destination=destination, mode='driving')
+        mandi_matrix.to_csv('distance.csv')
+        print mandi_matrix
+        # mandi_list = Mandi.objects.prefetch_related()
+        # source = str(mandi_list[0].latitude) + "," + str(mandi_list[0].longitude)
+        # destination = str(mandi_list[1].latitude) + "," + str(mandi_list[1].longitude)
+        # self.get_distance(source=source,destination=destination,mode='driving')
 
     def get_distance(self,source=None,destination=None, mode=None):
         distanceMatrix = DistanceMatrix()
         if distanceMatrix.distance(source=source, destination=destination, mode=mode):
             try:
-                print distanceMatrix.getDistance()
+                return distanceMatrix.getDistance()
             except Exception as e:
                 print e
