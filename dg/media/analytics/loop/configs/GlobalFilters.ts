8167e27da5a5975789618029dbc6d11e9e2a707c
sujit22993 <sujit22993@gmail.com>
2017-12-08 17:21:01 +0530

    config added for filter

---
+++ dg/media/analytics/loop/configs/GlobalFilters.ts
@@ -0,0 +1,12 @@
+export const globalFiltersConfig = {
+    'filter0': {
+        name: 'Geography',
+        show: true,
+        child: true
+    },
+    'filter1': {
+        name: 'Partner',
+        show: true,
+        child: true
+    },
+}
