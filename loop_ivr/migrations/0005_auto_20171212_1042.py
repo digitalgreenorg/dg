8103f8265625e35a0d5d6e55a47e1d81ea2252ca
Abhishek Lodha <alodha21051992@gmail.com>
2017-12-12 16:13:37 +0530

    migrations merged for loop ivr

--- loop_ivr/migrations/0005_auto_20171212_1042.py
+++ loop_ivr/migrations/0005_auto_20171212_1042.py
@@ -16,4 +16,14 @@ class Migration(migrations.Migration):
             name='call_source',
             field=models.IntegerField(default=1, choices=[(1, b'Exotel Call'), (2, b'Textlocal Call'), (3, b'Textlocal SMS')]),
         ),
+        migrations.AlterField(
+            model_name='priceinfoincoming',
+            name='call_id',
+            field=models.CharField(max_length=100, verbose_name=b'Call / Message Id', db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='priceinfoincoming',
+            name='textlocal_sms_id',
+            field=models.TextField(null=True, blank=True),
+        ),
     ]
