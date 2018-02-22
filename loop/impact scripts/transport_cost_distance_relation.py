ee0801a47d7097b7f288ea0a0ac24fd0ea09d727
Abhishek Lodha <alodha21051992@gmail.com>
2018-02-22 14:03:36 +0530

    plotting cost distance graph after adding predicted costs

--- loop/impact scripts/transport_cost_distance_relation.py
+++ loop/impact scripts/transport_cost_distance_relation.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import Tables
 import Transport_Cost
-
+import matplotlib.pyplot as plt
 
 def get_cost_distance():
     # List of state-wise frequently used vehicles
@@ -26,9 +26,21 @@ def get_cost_distance():
     aggregator_market_vehicle_cost_detailed = Transport_Cost.find_transport_cost_amv(daily_vehicle_single_data, vehicle_quantity_limits)
 
     aggregator_market_vehicle_cost = aggregator_market_vehicle_cost_detailed.drop(
-        ['Quantity_Min', 'Quantity_Limit'], axis=1) #'Vehicle_Name',
+        ['Quantity_Min', 'Quantity_Limit', 'Vehicle_Name'], axis=1) #'Vehicle_Name',
 
     # print aggregator_market_vehicle_cost.head(n=10)
+    aggregator_market_vehicle_cost_na = Transport_Cost.find_predicted_transport_cost_amv(aggregator_market_vehicle_cost, vehicles_to_fill_na)
+    aggregator_market_vehicle_cost = aggregator_market_vehicle_cost.append(aggregator_market_vehicle_cost_na)
+
+    aggregator_market_motorcycle_list = aggregator_market_vehicle_cost[aggregator_market_vehicle_cost['Vehicle_ID'] == 0]
+    aggregator_market_motorcycle_list['Motorcycle Cost'] = 0.4*aggregator_market_motorcycle_list['Predicted_TC']
+    aggregator_market_motorcycle_list['Vehicle_ID'] = 1
+    # aggregator_market_motorcycle_list['Vehicle_Name']= 'Motorcycle'
+    aggregator_market_motorcycle_list['Predicted_TC'] = aggregator_market_motorcycle_list['Motorcycle Cost']
+    aggregator_market_motorcycle_list = aggregator_market_motorcycle_list.drop(['Motorcycle Cost'], axis = 1)
+
+    aggregator_market_vehicle_cost = aggregator_market_vehicle_cost.append(aggregator_market_motorcycle_list)
+
 
     df_distance = pd.read_csv('mandi_village_mapping.csv')
     df_distance.drop(['Unnamed: 0','loop_user__id','mandi__latitude','mandi__longitude','village__latitude','village__longitude', 'village__village_name_en', 'village__id'], axis = 1,inplace = True)
@@ -41,6 +53,9 @@ def get_cost_distance():
     cost_distance_vehicle['cost_per_km'] = cost_distance_vehicle['Predicted_TC'] / cost_distance_vehicle['distance_km']
     print cost_distance_vehicle.head(n=25)
 
+    cost_distance_vehicle = cost_distance_vehicle[cost_distance_vehicle['Vehicle_ID']==2]
+    plot_cost_distance = cost_distance_vehicle.plot(use_index=False,kind='scatter',x='distance_km',y='cost_per_km',figsize=(18,8), color='Red')
+    plt.show()
 
 
 
