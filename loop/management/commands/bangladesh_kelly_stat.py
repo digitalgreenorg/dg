f57fa18f56c04d94f845acc8e8dbc6c04f8d900c
Lokesh Garg <lokeshgarg92@gmail.com>
2018-02-23 15:46:09 +0530

    and to in

--- loop/management/commands/bangladesh_kelly_stat.py
+++ loop/management/commands/bangladesh_kelly_stat.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         from_email = dg.settings.EMAIL_HOST_USER
         to_email = ['kellystenhoff@digitalgreen.org', 'tech@digitalgreen.org']
         body = ['Dear Team,\n\nBelow is the genderwise counting of farmers who have used the LOOP platform at least once in Bangladesh.\n\n',
-                'Common Active farmer who participated and YEAR 1 as well as YEAR 2: %s\n\tNo of Female Farmers: %s\n\tNo of Male Farmers: %s\n\n'%(gender_wise_farmer['F'][0]+ gender_wise_farmer['M'][0],gender_wise_farmer['F'][0], gender_wise_farmer['M'][0]),
+                'Common Active farmer who participated in YEAR 1 as well as YEAR 2: %s\n\tNo of Female Farmers: %s\n\tNo of Male Farmers: %s\n\n'%(gender_wise_farmer['F'][0]+ gender_wise_farmer['M'][0],gender_wise_farmer['F'][0], gender_wise_farmer['M'][0]),
                 'New Farmer who participated in YEAR 2 but not in YEAR 1: %s\n\tNo of Female Farmers: %s\n\tNo of Male Farmers: %s\n\n'%(gender_wise_farmer['F'][1]+gender_wise_farmer['M'][1],gender_wise_farmer['F'][1],gender_wise_farmer['M'][1]),
                 'Please contact system@digitalgreen.org for any question or clarification.\n\nThank you.']
         body = ''.join(body)
