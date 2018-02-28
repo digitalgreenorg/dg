f1643056915240db98d4c6b0ccf44edc7f8c3a65
Nikhil Verma <djangobotpython@gmail.com>
2018-02-28 12:30:35 +0530

    correcting the Email text

--- activities/management/commands/ap_data_integration.py
+++ activities/management/commands/ap_data_integration.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         from_email = dg.settings.EMAIL_HOST_USER
         to_email = ['vivek@digitalgreen.org', 'kamalakar@digitalgreen.org', 'nikhil@digitalgreen.org']
         # to_email = ['jharkhand@digitalgreen.org', 'system@digitalgreen.org', 'charu@digitalgreen.org']
-        body = "Dear Team,\n\n" + "AP data has been successfully updated in COCO.\n\n" + "Here are the details of this update:\n\n" + "Total entries sent by JSLPS (A): " + str(new_count+duplicate_count+other_error_count) + "\nEntries with errors (B): " + str(other_error_count) + "  *e.g. missing video category, missing video in screening, etc. error files are attached.\nEntries we already had (C): " + str(duplicate_count) + "\nEntries successfully updated in COCO (A-(B+C)): " + str(new_count) + "\n\nPlease contact system@digitalgreen.org for any clarification.\n\nThank you.\n"
+        body = "Dear Team,\n\n" + "AP data has been successfully updated in COCO.\n\n" + "Here are the details of this update:\n\n" + "Total entries sent by AP (A): " + str(new_count+duplicate_count+other_error_count) + "\nEntries with errors (B): " + str(other_error_count) + "  *e.g. missing video category, missing video in screening, etc. error files are attached.\nEntries we already had (C): " + str(duplicate_count) + "\nEntries successfully updated in COCO (A-(B+C)): " + str(new_count) + "\n\nPlease contact system@digitalgreen.org for any clarification.\n\nThank you.\n"
         msg = EmailMultiAlternatives(subject, body, from_email, to_email)
         for files in glob.glob("ap/*"):
             if files.endswith('.csv'):
