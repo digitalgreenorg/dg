b35a056de11eba98fa5b783f157cb8b905c23b41
Nikhil Verma <djangobotpython@gmail.com>
2018-03-13 14:24:36 +0530

    Bringing the original file

--- activities/management/commands/jslps_data_integration.py
+++ activities/management/commands/jslps_data_integration.py
@@ -13,8 +13,7 @@ class Command(BaseCommand):
     def send_mail(self):
         subject = "JSLPS data entry status"
         from_email = dg.settings.EMAIL_HOST_USER
-        to_email = ['varma.nikhil22@gmail.com']
-        # to_email = ['jharkhand@digitalgreen.org', 'system@digitalgreen.org', 'charu@digitalgreen.org']
+        to_email = ['jharkhand@digitalgreen.org', 'system@digitalgreen.org', 'charu@digitalgreen.org']
         body = "Dear Team,\n\n" + "JSLPS data has been successfully updated in COCO.\n\n" + "Here are the details of this update:\n\n" + "Total entries sent by JSLPS (A): " + str(new_count+duplicate_count+other_error_count) + "\nEntries with errors (B): " + str(other_error_count) + "  *e.g. missing video category, missing video in screening, etc. error files are attached.\nEntries we already had (C): " + str(duplicate_count) + "\nEntries successfully updated in COCO (A-(B+C)): " + str(new_count) + "\n\nPlease contact system@digitalgreen.org for any clarification.\n\nThank you.\n"
         msg = EmailMultiAlternatives(subject, body, from_email, to_email)
         for files in glob.glob("jslps_data_integration_files/*"):
@@ -24,109 +23,109 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options): 
         
-        # try:
-        #     print "Geography,jslps_geo"
-        #     call_command('jslps_geo')
-        # except:
-        #     pass
+        try:
+            print "Geography,jslps_geo"
+            call_command('jslps_geo')
+        except:
+            pass
 
-        # try:
-        #     print "Geography,jslps_geo"
-        #     call_command('mksp_jslps_geo')
-        # except:
-        #     pass
+        try:
+            print "Geography,jslps_geo"
+            call_command('mksp_jslps_geo')
+        except:
+            pass
 
-        # try:
-        #     print "Groups,jslps_groups"
-        #     call_command('jslps_groups')
-        # except:
-        #     pass
+        try:
+            print "Groups,jslps_groups"
+            call_command('jslps_groups')
+        except:
+            pass
 
-        # try:
-        #     print "Groups,jslps_groups"
-        #     call_command('mksp_jslps_groups')
-        # except:
-        #     pass
+        try:
+            print "Groups,jslps_groups"
+            call_command('mksp_jslps_groups')
+        except:
+            pass
         
-        # try:
-        #     print "People,jslps_people"
-        #     call_command('jslps_people_new')
-        # except:
-        #     pass
+        try:
+            print "People,jslps_people"
+            call_command('jslps_people_new')
+        except:
+            pass
 
-        # try:
-        #     print "People,jslps_people"
-        #     call_command('jslps_people_latest')
-        # except:
-        #     pass
+        try:
+            print "People,jslps_people"
+            call_command('jslps_people_latest')
+        except:
+            pass
 
-        # try:
-        #     print "People,jslps_people"
-        #     call_command('mksp_jslps_person_new')
-        # except:
-        #     pass
+        try:
+            print "People,jslps_people"
+            call_command('mksp_jslps_person_new')
+        except:
+            pass
 
-        # try:
-        #     print "Mediators,jslps_mediators"
-        #     call_command('jslps_mediator_new')
-        # except:
-        #     pass
+        try:
+            print "Mediators,jslps_mediators"
+            call_command('jslps_mediator_new')
+        except:
+            pass
 
-        # try:
-        #     print "Mediators,jslps_mediators"
-        #     call_command('mksp_jslps_mediator_new')
-        # except:
-        #     pass
+        try:
+            print "Mediators,jslps_mediators"
+            call_command('mksp_jslps_mediator_new')
+        except:
+            pass
 
-        # try:
-        #     print "Videos and Non negotiable,jslps_videos_nn"
-        #     call_command('jslps_videos_nn_new')
-        # except:
-        #     pass
+        try:
+            print "Videos and Non negotiable,jslps_videos_nn"
+            call_command('jslps_videos_nn_new')
+        except:
+            pass
 
-        # try:
-        #     print "Videos and Non negotiable,jslps_videos_nn"
-        #     call_command('jslps_videogoatry')
-        # except:
-        #     pass
+        try:
+            print "Videos and Non negotiable,jslps_videos_nn"
+            call_command('jslps_videogoatry')
+        except:
+            pass
         
-        # try:
-        #     print "Screening and pma,jslps_screening_pma"
-        #     call_command('jslps_screening_pma_new')
-        # except:
-        #     pass
+        try:
+            print "Screening and pma,jslps_screening_pma"
+            call_command('jslps_screening_pma_new')
+        except:
+            pass
 
-        # try:
-        #     print "MKSP Screening and pma, mksp_jslps_screening_pma"
-        #     call_command('mksp_jslps_screening_pma')
-        # except:
-        #     pass
+        try:
+            print "MKSP Screening and pma, mksp_jslps_screening_pma"
+            call_command('mksp_jslps_screening_pma')
+        except:
+            pass
 
-        # try:
-        #     print "Gotary Screeing"
-        #     call_command('gotary_screening_pma_new')
-        # except:
-        #     pass
+        try:
+            print "Gotary Screeing"
+            call_command('gotary_screening_pma_new')
+        except:
+            pass
         
-        # try:
-        #     print "Adoption,jslps_adoptions"
-        #     call_command('jslps_adoptions_new')
-        # except:
-        #     pass
-
-        
-        # try:
-        #     print "Health Person"
-        #     call_command('jslps_hnn_person')
-        # except:
-        #     pass
+        try:
+            print "Adoption,jslps_adoptions"
+            call_command('jslps_adoptions_new')
+        except:
+            pass
 
         
-        # try:
-        #     print "Health Person New"
-        #     call_command('jslps_hnn_person_new')
-        # except:
-        #     pass
+        try:
+            print "Health Person"
+            call_command('jslps_hnn_person')
+        except:
+            pass
+
+        
+        try:
+            print "Health Person New"
+            call_command('jslps_hnn_person_new')
+        except:
+            pass
 
         
         try:
