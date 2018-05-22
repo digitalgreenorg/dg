bd78f6c245afd331a3d0b0a2580b803cf6881d87
Amandeep Singh <amandeep1401@iiitd.ac.in>
2018-05-22 15:42:56 +0530

    beneficiary data state wise

--- dataexport/views.py
+++ dataexport/views.py
@@ -73,20 +73,32 @@ class ExportView(FormView):
                         for _dict in cat:
                             if int(_dict.get('id')) not in person_cat_map.get(str(item.get('person_id'))):
                                 person_cat_map[str(item.get('person_id'))].append(int(_dict.get('id')))
-                                state_beneficiary_count_map.get(str(item.get('screening__village__block__district__state__state_name'))).get(str(_dict.get('id'))) += 1
+                                state_beneficiary_count_map.get(str(item.get('screening__village__block__district__state__state_name')))[str(_dict.get('id'))] += 1
 
                     else:
                         for _id in cat:
                             if int(_id) not in person_cat_map.get(str(item.get('person_id'))):
                                person_cat_map[str(item.get('person_id'))].append(int(_id))
-                               state_beneficiary_count_map.get(str(item.get('screening__village__block__district__state__state_name'))).get(str(_id)) += 1
+                               state_beneficiary_count_map.get(str(item.get('screening__village__block__district__state__state_name')))[str(_id)]+= 1
 
                 else:
                     pass
         except Exception as e:
             print e
 
-        return data_list
+        state_beneficiary_count_list = []
+        for key, value in state_beneficiary_count_map.items():
+            obj = {}
+            obj['State'] = key
+            obj['Woman of reproductive age (15-49 years)'] = value.get('1')
+            obj['Adolescent girl (10-19 years)'] = value.get('2')
+            obj['Mother of a child 2 to 5 years'] = value.get('3')
+            obj['Mother of a child 6 months to 2 years'] = value.get('4')
+            obj['Mother of a child up to 6 months'] = value.get('5')
+            obj['Pregnant woman'] = value.get('6')
+            state_beneficiary_count_list.append(obj)
+
+        return data_list, state_beneficiary_count_list
 
 
     # def get_adoption_data(self, date_range, data):
@@ -100,12 +112,14 @@ class ExportView(FormView):
         data = int(cd.get('data'))
         data_category = list(cd.get('data_category'))
         if data == 1:
-            data_list = self.get_screening_data(date_range, data_category)
+            data_list, state_beneficiary_count_list = self.get_screening_data(date_range, data_category)
         elif data == 2:
             data_list = self.get_adoption_data(date_range, data_category)
         if len(data_list):
 
             data = pd.DataFrame(data_list)
+            beneficiary_data = pd.DataFrame(state_beneficiary_count_list)
+
             data = data[['village__block__district__state__country_id', 'village__block__district__state__country__country_name',\
                           'village__block__district__state_id', 'village__block__district__state__state_name',\
                           'village__block__district_id', 'village__block__district__district_name',\
@@ -117,9 +131,22 @@ class ExportView(FormView):
                             'Partner ID', 'PartnerName', 'Video Id', 'Video Title', 'Date', 'Category ID', 'Category Name',
                             'Screening#ID',
                             'Viewers Count']
-            data = data.to_html()
 
-        context = {'data_list': data, 'start_date': cd.get('start_date'), 'end_date': cd.get('end_date')}
+            beneficiary_data = beneficiary_data[['State', 'Woman of reproductive age (15-49 years)',\
+                          'Adolescent girl (10-19 years)', 'Mother of a child 2 to 5 years',\
+                          'Mother of a child 6 months to 2 years', 'Mother of a child up to 6 months',\
+                          'Pregnant woman']]
+
+            beneficiary_data.columns = ['State', 'Woman of reproductive age (15-49 years)',\
+                          'Adolescent girl (10-19 years)', 'Mother of a child 2 to 5 years',\
+                          'Mother of a child 6 months to 2 years', 'Mother of a child up to 6 months',\
+                          'Pregnant woman']
+
+
+            data = data.to_html()
+            beneficiary_data = beneficiary_data.to_html()
+
+        context = {'data_list': data, 'beneficiary_data_list': beneficiary_data, 'start_date': cd.get('start_date'), 'end_date': cd.get('end_date')}
         template = "dataexport/table-data.html"
         return render(self.request, template, context)
 
