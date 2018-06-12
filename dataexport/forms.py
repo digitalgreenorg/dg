a0ce65b6473aac0d77f36be4c5dae97b1e229599
Nikhil Verma <varma.nikhil24@gmail.com>
2018-06-12 17:45:57 +0530

    Remove comments. Code Review.

--- dataexport/forms.py
+++ dataexport/forms.py
@@ -20,7 +20,6 @@ DATA = [
 class PageView(forms.Form):
     date_period = forms.CharField(max_length=100, required=True,
                                   widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    # end_date = forms.DateField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     data_category = forms.ChoiceField(choices=DATA_CATEGORY, required=True, widget=forms.Select(attrs={'class' : 'form-control'}))
     data = forms.ChoiceField(choices=DATA, required=True, widget=forms.Select(attrs={'class' : 'form-control'}))
     country = forms.ModelChoiceField(queryset=Country.objects.all(),
