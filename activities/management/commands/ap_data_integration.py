e23f34c0831dacbdf6e846d73e2bf95ca0220721
Amandeep Singh <amandeep1401@iiitd.ac.in>
2018-08-20 14:28:28 +0530

    ap APi call updated

--- activities/management/commands/ap_data_integration.py
+++ activities/management/commands/ap_data_integration.py
@@ -58,6 +58,6 @@ class Command(BaseCommand):
         # call_command('bluefrog_adoptions')
 
         print "Migrating Bluefrog data to COCO"
-        call_command(os.path.join(PROJECT_PATH, '../geographies/management/commands/migrate_ap_data')
+        call_command('migrate_ap_data')
 
         self.send_mail()
