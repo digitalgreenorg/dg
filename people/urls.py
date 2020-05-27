848893afc9824d4cba908455c1269607d801d4f6
vermastuti <stutiverma.developer@gmail.com>
2020-05-27 16:23:07 +0530

    match-people-phone-numbers-api

--- people/urls.py
+++ people/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
     url(r'^api/default', views.DefaultView.as_view()), 
     url(r'^api/farmers', views.FarmersJsonAPIView.as_view()),
     url(r'^api/csv', views.FarmersCsvAPIView.as_view()), # r'^$' is used for regex of exact match as mentioned
-    # url(r'^farmers-list', views.FarmerViewSet.as_view({'get': 'list'})),
+    url(r'^api/match/phone', views.FarmersMatchAPIView.as_view()),
 ]
