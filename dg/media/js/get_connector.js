90ef7197e5efc98d2d8bc30386898479c4526ff6
ugeshdg <ugesh@digitalgreen.org>
2022-01-24 10:19:53 +0530

    video library changes

---
+++ dg/media/js/get_connector.js
@@ -0,0 +1,24 @@
+function getConnector() {
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  let email = $('#recipient-name').attr('value')
+         console.log(email)
+        if (filter.test(email))  {
+                url = window.location.href
+                url = url+ '/get_data?email=' + email;
+                $.get(url, function(data, status){
+                    console.log(data)
+                    console.log(status)
+                    window.location = data['data']
+
+    });
+   }
+   $('#recipient-name').css('border', 'solid 3px red');
+}
+$('.get-connector').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-body input').val(recipient)
+})
---
+++ django_env/lib/python3.10/site-packages/yolk/__init__.py
@@ -0,0 +1,15 @@
+
+"""
+
+__init__.py
+
+Author: Rob Cakebread <gentoodev@gmail.com>
+
+License  : GNU General Public License Version 2
+
+"""
+
+__docformat__ = 'restructuredtext'
+__version__ = '0.4.1'
+
+
