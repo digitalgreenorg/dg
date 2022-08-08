e92aeb82dab40d3d30cfe3ddd0fc7fb35752ace2
Samuel <samuelabate2@gmail.com>
2022-08-08 10:27:38 +0300

    Modified qacoco_monthly_reporting to send results from Ethiopia only and fixed a bug in dissemination_quality report

--- qacoco/management/commands/qacoco_monthly_data_reporting.py
+++ qacoco/management/commands/qacoco_monthly_data_reporting.py
@@ -8,26 +8,28 @@ import dg.settings
 import csv
 import datetime
 
+
 class Command(BaseCommand):
 
-    def send_mail(self,attached_files):
+    def send_mail(self, attached_files):
         till_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        subject = "QA COCO: Data as of %s"%(till_date)
+        subject = "QA COCO: Data as of %s" % (till_date)
         from_email = dg.settings.EMAIL_HOST_USER
         to_email = ['eth_qacoco_monthly_report_recipients@digitalgreen.org']
-        body = "Dear Team,\n\nPlease find the attached QA COCO data entered as of %s.\nYou may reach out to system@digitalgreen.org for any question or clarification.\n\nThank you."%(till_date)
+        body = "Dear Team,\n\nPlease find the attached QA COCO data entered as of %s.\nYou may reach out to system@digitalgreen.org for any question or clarification.\n\nThank you." % (
+            till_date)
         msg = EmailMessage(subject, body, from_email, to_email)
         for file in attached_files:
-            msg.attach_file(file, 'text/csv' )
+            msg.attach_file(file, 'text/csv')
         msg.send()
 
-    def run_query(self,query):
-        mysql_cn = MySQLdb.connect(host=dg.settings.DATABASES['default']['HOST'], port=dg.settings.DATABASES['default']['PORT'], 
-            user=dg.settings.DATABASES['default']['USER'] ,
-            passwd=dg.settings.DATABASES['default']['PASSWORD'],
-            db=dg.settings.DATABASES['default']['NAME'],
-            charset = 'utf8',
-            use_unicode = True)
+    def run_query(self, query):
+        mysql_cn = MySQLdb.connect(host=dg.settings.DATABASES['default']['HOST'], port=dg.settings.DATABASES['default']['PORT'],
+                                   user=dg.settings.DATABASES['default']['USER'],
+                                   passwd=dg.settings.DATABASES['default']['PASSWORD'],
+                                   db=dg.settings.DATABASES['default']['NAME'],
+                                   charset='utf8',
+                                   use_unicode=True)
         temp_df = psql.read_sql(query, con=mysql_cn)
         mysql_cn.close()
         return temp_df
@@ -92,13 +94,15 @@ class Command(BaseCommand):
                 qacoco_qareviewername vn ON vn.id = qr.qareviewername_id
                     JOIN
                 auth_user au ON qr.user_created_id = au.id
+            WHERE
+                gc.id = 2
             ORDER BY qr.date DESC
         '''
 
         dissemination_quality_query = '''
             SELECT 
-                YEAR(qd.date) Year,
-                MONTHNAME(qd.date) Month,
+                YEAR(dq.date) Year,
+                MONTHNAME(dq.date) Month,
                 gc.country_name Country,
                 gs.state_name State,
                 gd.DISTRICT_NAME District,
@@ -106,52 +110,55 @@ class Command(BaseCommand):
                 gv.id 'Village Id',
                 gv.VILLAGE_NAME Village,
                 pg.group_name 'Group Name',
-                qd.date Date,
+                dq.date Date,
                 vv.id 'Video ID',
                 vv.title 'Video Title',
                 pa.id AS 'Mediator Id',
                 pa.name AS 'Mediator',
-                CASE qd.pico
+                CASE dq.pico
                     WHEN '1' THEN 'Working'
                     WHEN '0' THEN 'Not Working'
+                    ELSE 'N/A'
                 END 'Pico',
-                CASE qd.speaker
+                CASE dq.speaker
                     WHEN '1' THEN 'Working'
                     WHEN '0' THEN 'Not Working'
+                    ELSE 'N/A'
                 END 'Speaker',
-                CASE qd.remote
+                CASE dq.remote
                     WHEN '1' THEN 'Working'
                     WHEN '0' THEN 'Not Working'
+                    ELSE 'N/A'
                 END 'Remote',
-                qd.equipments_setup_handling 'Equipment Setup',
-                qd.maintained_ideal_darkness 'Maintained Ideal Darkness',
-                qd.maintained_ideal_screen_size 'Maintained Ideal Screen Size',
-                qd.maintained_ideal_av_quality 'Maintained Ideal AV Quality',
-                qd.established_logical_conn 'Established Logical Connection',
-                qd.introduce_topic 'Introduced Topic',
-                qd.paused_video 'Paused Video',
-                qd.encouraged_adoption 'Encouraged Adoption',
-                qd.summarized_video 'Summarized Video',
-                qd.subject_knowledge 'Subject Knowledge',
-                qd.filled_dissemination 'Filled Dissemination',
-                qd.total_score 'Total Score',
-                qd.video_grade 'Video Grade',
-                vn.id 'Reviewer Id',
-                vn.name AS 'Reviewed By',
-                qd.remark 'Remarks',
+                dq.equipments_setup_handling 'Equipment Setup',
+                dq.maintained_ideal_darkness 'Maintained Ideal Darkness',
+                dq.maintained_ideal_screen_size 'Maintained Ideal Screen Size',
+                dq.maintained_ideal_av_quality 'Maintained Ideal AV Quality',
+                dq.established_logical_conn 'Established Logical Connection',
+                dq.introduce_topic 'Introduced Topic',
+                dq.paused_video 'Paused Video',
+                dq.encouraged_adoption 'Encouraged Adoption',
+                dq.summarized_video 'Summarized Video',
+                dq.subject_knowledge 'Subject Knowledge',
+                dq.filled_dissemination 'Filled Dissemination',
+                dq.total_score 'Total Score',
+                dq.video_grade 'Video Grade',
+                rn.id 'Reviewer Id',
+                rn.name AS 'Reviewed By',
+                dq.remark 'Remarks',
                 au.username 'Created By'
             FROM
-                qacoco_disseminationquality qd
+                qacoco_disseminationquality dq
                     JOIN
-                videos_video vv ON vv.id = qd.video_id
+                videos_video vv ON vv.id = dq.video_id
                     JOIN
-                qacoco_qareviewername vn ON vn.id = qd.qareviewername_id
+                qacoco_qareviewername rn ON rn.id = dq.qareviewername_id
                     JOIN
-                people_animator pa ON pa.id = qd.mediator_id
+                people_animator pa ON pa.id = dq.mediator_id
+                    LEFT JOIN
+                people_persongroup pg ON pg.id = dq.group_id
                     JOIN
-                people_persongroup pg ON pg.id = qd.group_id
-                    JOIN
-                geographies_village gv ON gv.id = qd.village_id
+                geographies_village gv ON gv.id = dq.village_id
                     JOIN
                 geographies_block gb ON gb.id = gv.block_id
                     JOIN
@@ -161,8 +168,10 @@ class Command(BaseCommand):
                     JOIN
                 geographies_country gc ON gc.id = gs.country_id
                     JOIN
-                auth_user au ON qd.user_created_id = au.id
-            ORDER BY qd.date DESC
+                auth_user au ON dq.user_created_id = au.id
+            WHERE
+                gc.id = 2
+            ORDER BY dq.date DESC
         '''
 
         adoption_verification_query = '''
@@ -208,6 +217,8 @@ class Command(BaseCommand):
                 geographies_country gc ON gc.id = gs.country_id
                     JOIN
                 auth_user au ON au.id = af.user_created_id
+            WHERE
+                gc.id = 2
             ORDER BY YEAR(af.verification_date) DESC , MONTH(af.verification_date) DESC
         '''
 
@@ -226,16 +237,22 @@ class Command(BaseCommand):
         video_quality_df = self.run_query(video_quality_query)
         dissemination_quality_df = self.run_query(dissemination_quality_query)
         adoption_verification_df = self.run_query(adoption_verification_query)
-        adoption_verification_nonnego_df = self.run_query(adoption_verification_nonnego_query)
+        adoption_verification_nonnego_df = self.run_query(
+            adoption_verification_nonnego_query)
 
         # JOIN the adoption verification and adopted non negotiables
-        combine_df = pd.merge(adoption_verification_df, adoption_verification_nonnego_df, how='inner')
+        combine_df = pd.merge(adoption_verification_df,
+                              adoption_verification_nonnego_df, how='inner')
         combine_df = combine_df.drop('af_id', axis=1)
 
         # Save the results on a CSV file
-        video_quality_df.to_csv(file_save_path+"video_quality_review.csv", sep=',', encoding='utf-8', index=False)
-        dissemination_quality_df.to_csv(file_save_path+"dissemination_quality.csv", sep=',', encoding='utf-8', index=False)
-        combine_df.to_csv(file_save_path+"adoption_verification.csv", sep=',', encoding='utf-8', index=False)
+        video_quality_df.to_csv(
+            file_save_path+"video_quality_review.csv", sep=',', encoding='utf-8', index=False)
+        dissemination_quality_df.to_csv(
+            file_save_path+"dissemination_quality.csv", sep=',', encoding='utf-8', index=False)
+        combine_df.to_csv(file_save_path+"adoption_verification.csv",
+                          sep=',', encoding='utf-8', index=False)
 
         # Attach and send them via email
-        self.send_mail([file_save_path+"video_quality_review.csv",file_save_path+"dissemination_quality.csv",file_save_path+"adoption_verification.csv"])
+        self.send_mail([file_save_path+"video_quality_review.csv", file_save_path +
+                       "dissemination_quality.csv", file_save_path+"adoption_verification.csv"])
